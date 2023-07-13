1b34c14039f58b116bb46160ee6ae334621a95b6
Elad Venezianbashexitqexitgitk & <eladv@il.ibm.com>
2023-07-13 16:29:18 +0300

    Add tests

---
+++ tests/test_examples.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+class TestExamples(unittest.TestCase):
+    def test_example1(self):
+        import examples.example1
+        self.assertTrue(True)
+
+    def test_example1(self):
+        import examples.example1
+        self.assertTrue(True)
+
+    def test_example2(self):
+        import examples.example2
+        self.assertTrue(True)
+
+    def test_example3(self):
+        import examples.example3
+        self.assertTrue(True)
+
+    def test_example4(self):
+        import examples.example4
+        self.assertTrue(True)
+
+    def test_example5(self):
+        import examples.example5
+        self.assertTrue(True)
+
+    def test_example6(self):
+        import examples.example6
+        self.assertTrue(True)
+
+    def test_example7(self):
+        import examples.example7
+        self.assertTrue(True)
+
+    def test_example8(self):
+        import examples.example8
+        self.assertTrue(True)
+
+    def test_example9(self):
+        import examples.example9
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
