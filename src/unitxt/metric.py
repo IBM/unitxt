1f8c4791758aadbaa81e344413526d9807a136d2
Elad Venezianbashexitqexitgitk & <eladv@il.ibm.com>
2023-07-13 12:32:59 +0300

    Fix general cards

--- src/unitxt/metric.py
+++ src/unitxt/metric.py
@@ -96,7 +96,7 @@ class MetricRecipe(SequntialOperatorInitilizer):
         self.steps = [
             FromPredictionsAndOriginalData(),
             ApplyValueOperatorsField(
-                value_field="prediction", operators_field="processors", default_operators=["to_string"]
+                value_field="prediction", operators_field="processors", default_operators=["processors::to_string"]
             ),
             SplitByValue(["group"]),
             ApplyStreamOperatorsField(
