c515ef4d994b6c35bf9ddafca794e33035c7cdad
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-07-16 18:57:55 +0300

    ethos ready for tests

---
+++ prepare/splitters/missing_split.py
@@ -0,0 +1,10 @@
+add_to_catalog(SplitRandomMix({'train': 'train[95%]', 'validation': 'train[5%]', 'test': 'validation'}),
+               'splitters.small_no_test', overwrite=True)
+add_to_catalog(SliceSplit({'train': 'train[-500]', 'validation': 'train[500]', 'test': 'validation'}),
+               'splitters.large_no_test', overwrite=True)
+add_to_catalog(SplitRandomMix({'train': 'train[95%]', 'validation': 'train[5%]', 'test': 'test'}),
+               'splitters.small_no_dev', overwrite=True)
+add_to_catalog(SliceSplit({'train': 'train[-500]', 'validation': 'train[500]', 'test': 'test'}),
+               'splitters.large_no_dev', overwrite=True)
+add_to_catalog(SliceSplit({'train': 'train[0]', 'validation': 'train[0]', 'test': 'test'}),
+               'splitters.test_only', overwrite=True)
