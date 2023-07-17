d1191f1f8601bb6c1d75b5cd411aa5bdfcf00958
Elron Bandel <elron.bandel@ibm.com>
2023-07-17 08:29:22 -0400

    Add Test: add some tests for operators

--- src/unitxt/test_utils/operators.py
+++ src/unitxt/test_utils/operators.py
@@ -2,21 +2,24 @@ from ..operator import StreamingOperator
 from ..stream import MultiStream
 from ..type_utils import is_typing_type
 from typing import List
+import json
 
-def test_operator(operator:StreamingOperator, inputs: List[dict], outputs: List[dict]):
+def apply_operator(operator:StreamingOperator, inputs: List[dict]):
+    multi_stream = MultiStream({'test': inputs})
+    output_multi_stream = operator(multi_stream)
+    output_stream = output_multi_stream['test']
+    return list(output_stream)
+    
+def test_operator(operator:StreamingOperator, inputs: List[dict], targets: List[dict]):
     
     assert is_typing_type(operator, StreamingOperator), "operator must be an Operator"
     assert is_typing_type(inputs, List[dict]), "inputs must be a list of dicts"
     assert is_typing_type(outputs, List[dict]), "outputs must be a list of dicts"
     
-    multi_stream = MultiStream({'test': inputs})
-    output_multi_stream = operator(multi_stream)
-    output_stream = output_multi_stream['test']
+    outputs = apply_operator(operator, inputs)
     
-    for input, output in zip(output_stream, outputs):
-        for key in output:
-            assert key in input, f"key {key} not found in input"
-            assert input[key] == output[key], f"key {key} does not match"
+    for input, output in zip(outputs, targets):
+        assert json.dumps(input, sort_keys=True) == json.dumps(output, sort_keys=True), "input and output must be equal"
     
     return True
     
