dd0a76c22a5ccf347b6db6e27cc08c0969105d44
Elron Bandel <elron.bandel@ibm.com>
2023-07-17 05:02:53 -0400

    Add Tests: cards metric existance

--- src/unitxt/test_utils/card.py
+++ src/unitxt/test_utils/card.py
@@ -2,6 +2,7 @@ import os.path
 import tempfile
 
 from .. import add_to_catalog
+from ..artifact import fetch_artifact
 
 TEMP_NAME = 'tmp_name'
 
@@ -11,6 +12,9 @@ def test_adding_to_catalog(card):
         add_to_catalog(card, TEMP_NAME, overwrite=True, catalog_path=tmp_dir)
         assert os.path.exists(os.path.join(tmp_dir, TEMP_NAME + '.json')), 'Card was not added to catalog'
 
+def test_metrics_exist(card):
+    for metric_name in card.task.metrics:
+        metric, _ = fetch_artifact(metric_name)
 
 def test_loading_to_catalog():
     pass
@@ -26,6 +30,7 @@ def test_with_eval():
 
 def test_card(card):
     test_adding_to_catalog(card)
+    test_metrics_exist(card)
     test_loading_to_catalog()
     test_with_common_recipe()
     test_with_eval()
