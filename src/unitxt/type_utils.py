cf67cb28a1e4561edad8b24765a34b2c0bd36f05
Elron Bandel <elron.bandel@ibm.com>
2023-07-19 08:40:27 -0400

    Refactor is_styping_type -> isoftype
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/type_utils.py
+++ src/unitxt/type_utils.py
@@ -1,6 +1,9 @@
+import collections.abc
+import io
+import itertools
 import typing
 
-def is_typing_instance(object, type):
+def isoftype(object, type):
     """
     Checks if an object is of a certain typing type, including nested types.
     
@@ -16,13 +19,13 @@ def is_typing_instance(object, type):
         bool: True if the object is of the specified type, False otherwise.
 
     Examples:
-        >>> is_typing_type(1, int)
+        >>> isoftype(1, int)
         True
-        >>> is_typing_type([1, 2, 3], typing.List[int])
+        >>> isoftype([1, 2, 3], typing.List[int])
         True
-        >>> is_typing_type([1, 2, 3], typing.List[str])
+        >>> isoftype([1, 2, 3], typing.List[str])
         False
-        >>> is_typing_type([[1, 2], [3, 4]], typing.List[typing.List[int]])
+        >>> isoftype([[1, 2], [3, 4]], typing.List[typing.List[int]])
         True
     """
     
@@ -31,20 +34,17 @@ def is_typing_instance(object, type):
         type_args = typing.get_args(type)
 
         if origin is list or origin is set:
-            return all(is_typing_instance(element, type_args[0]) for element in object)
+            return all(isoftype(element, type_args[0]) for element in object)
         elif origin is dict:
-            return all(is_typing_instance(key, type_args[0]) and is_typing_instance(value, type_args[1]) for key, value in object.items())
+            return all(isoftype(key, type_args[0]) and isoftype(value, type_args[1]) for key, value in object.items())
         elif origin is tuple:
-            return all(is_typing_instance(element, type_arg) for element, type_arg in zip(object, type_args))
+            return all(isoftype(element, type_arg) for element, type_arg in zip(object, type_args))
     else:
         return isinstance(object, type)
     
 # copied from: https://github.com/bojiang/typing_utils/blob/main/typing_utils/__init__.py
 # liscened under Apache License 2.0
-import collections.abc
-import io
-import itertools
-import typing
+
 
 if hasattr(typing, "ForwardRef"):  # python3.8
     ForwardRef = getattr(typing, "ForwardRef")
