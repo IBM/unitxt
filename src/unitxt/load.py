11be0d84196adb1c1874b9acc7b460bbcc37f5e9
Elron Bandel <elron.bandel@ibm.com>
2023-07-19 05:17:12 -0400

    Bug Fix: fix bug in loading with actual artifact
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/load.py
+++ src/unitxt/load.py
@@ -7,5 +7,5 @@ from .operator import StreamSource
 def load_dataset(source: Union[StreamSource, str]) -> DatasetDict:
     assert isinstance(source, (StreamSource, str)), "source must be a StreamSource or a string"
     if isinstance(source, str):
-        artifact, _ = fetch_artifact(source)
-    return artifact().to_dataset()
+        source, _ = fetch_artifact(source)
+    return source().to_dataset()
