c6a40672e24afe7f6c5e36fb61c09c5ebddfde27
Elad Venezian <eladv@il.ibm.com>
2023-07-19 13:30:24 +0300

    Add Card: GLUE:RTE

---
+++ prepare/cards/rte.py
@@ -0,0 +1,46 @@
+from src.unitxt.blocks import (
+    LoadHF,
+    SplitRandomMix,
+    AddFields,
+    TaskCard,
+    NormalizeListFields,
+    FormTask,
+    TemplatesList,
+    InputOutputTemplate,
+    MapInstanceValues
+)
+from src.unitxt.test_utils.card import test_card
+
+from src.unitxt.catalog import add_to_catalog
+
+card = TaskCard(
+        loader=LoadHF(path='glue', name='rte'),
+        preprocess_steps=[
+            SplitRandomMix({'train': 'train[95%]', 'validation': 'train[5%]', 'test': 'validation'}),
+            MapInstanceValues(mappers={'label': {"0": 'entailment', "1": 'not_entailment'}}),
+            AddFields(
+            fields={
+                'choices': ['entailment', 'not_entailment'],
+            }
+            ),
+        ],
+        task=FormTask(
+            inputs=['choices', 'sentence1', 'sentence2'],
+            outputs=['label'],
+            metrics=['metrics.accuracy'],
+        ),
+        templates=TemplatesList([
+            InputOutputTemplate(
+                input_format="""
+                    Given this sentence: {sentence1}, classify if this sentence: {sentence2} is {choices}.
+                """.strip(),
+                output_format='{label}',
+            ),
+               ])
+    )
+
+test_card(card)
+add_to_catalog(card, 'cards.rte', overwrite=True)
+
+
+        
