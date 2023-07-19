b9850c4f2d99567cd392b9f510fb9c268e17f97d
Elron Bandel <elron.bandel@ibm.com>
2023-07-19 05:15:53 -0400

    Add links to dataset and metric files
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/__init__.py
+++ src/unitxt/__init__.py
@@ -1,8 +1,12 @@
 from .register import register_all_artifacts, register_local_catalog
 from .load import load_dataset
 from .catalog import add_to_catalog
+import os
 
 register_all_artifacts()
+
+dataset_file = os.path.join(os.path.dirname(__file__), "dataset.py")
+metric_file = os.path.join(os.path.dirname(__file__), "metric.py")
 
 dataset_url = "unitxt/data"
 metric_url = "unitxt/metric"
