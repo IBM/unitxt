669dc9904ffe00b7af8bc983b66035a8dd8968c6
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-07-19 18:07:38 +0300

    fix imports

--- prepare/cards/wnli.py
+++ prepare/cards/wnli.py
@@ -9,12 +9,11 @@ from src.unitxt.blocks import (
 from src.unitxt.catalog import add_to_catalog
 from src.unitxt.test_utils.card import test_card
 from src.unitxt.splitters import SplitRandomMix
-from unitxt.prepare_utils.card_types import create_2sentences_classification_card
+from src.unitxt.prepare_utils.card_types import create_2sentences_classification_card
 
 card = create_2sentences_classification_card(
     loader=LoadHF(path='glue', name='wnli'),
     preprocess_steps=
-    # [SplitRandomMix({'train': 'train[95%]', 'validation': 'train[5%]', 'test': 'validation'}), ],
     ['splitters.small_no_test', ],
     label_name="label",
     label2string={"0": 'entailment', "1": 'not entailment'},
@@ -30,7 +29,5 @@ card = create_2sentences_classification_card(
     ])
 )
 
-project_dir = Path(__file__).parent.parent.parent.absolute()
-catalog_dir = os.path.join(project_dir, 'fm_eval', 'catalogs', 'private')
 test_card(card)
-add_to_catalog(card, 'cards.wnli', overwrite=True, catalog_path=catalog_dir)
+add_to_catalog(card, 'cards.wnli', overwrite=True)
