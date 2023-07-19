79adec4cebe64931710369fb22e5b86300a3f7d8
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-07-19 12:24:47 +0300

    more informative wrong column specified

--- src/unitxt/task.py
+++ src/unitxt/task.py
@@ -13,8 +13,19 @@ class FormTask(Tasker, StreamInstanceOperator):
     metrics: List[str]
 
     def process(self, instance: Dict[str, Any], stream_name: str = None) -> Dict[str, Any]:
-        inputs = {key: instance[key] for key in self.inputs}
-        outputs = {key: instance[key] for key in self.outputs}
+        try:
+            inputs = {key: instance[key] for key in self.inputs}
+        except KeyError as e:
+            raise KeyError(
+                f"Unexpected input column names: {list(key for key in self.inputs if key not in instance)}"
+                f"\n available names:{list(instance.keys())}\n given input names:{self.inputs}")
+        try:
+            outputs = {key: instance[key] for key in self.outputs}
+        except KeyError as e:
+            raise KeyError(
+                f"Unexpected output column names: {list(key for key in self.inputs if key not in instance)}"
+                f" \n available names:{list(instance.keys())}\n given output names:{self.outputs}")
+
         return {
             "inputs": inputs,
             "outputs": outputs,
