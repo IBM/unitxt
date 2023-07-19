5f1ca8d1c2142f095fa09cbe6079165e7f531e7b
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-07-19 11:27:34 +0300

    fix imports ethos wnli wsc

--- prepare/cards/ethos.py
+++ prepare/cards/ethos.py
@@ -1,35 +1,40 @@
-from src.unitxt.blocks import (
+import os
+from pathlib import Path
+
+from unitxt.blocks import (
     LoadHF,
     SplitRandomMix,
     AddFields,
     TaskCard,
-    ClassificationCard,
     NormalizeListFields,
     FormTask,
     TemplatesList,
     InputOutputTemplate,
     MapInstanceValues
 )
+from unitxt.card import ClassificationCard
 
-from src.unitxt.catalog import add_to_catalog
+from unitxt.catalog import add_to_catalog
 
 from unitxt.instructions import TextualInstruction
 from unitxt.templates import outputs_inputs2templates, instructions2templates
 
 # TODO convert str to instructions
+from unitxt.test_utils.card import test_card
+
 classification_instructions = ["Predict the class of the following ({choices}):",
                                "What is the type of the following? Types:{choices}",
-                               "Which of the choices, best describes the following text:"]  # TODO How to share instructions?
-classification_instructions = [TextualInstruction(x) for x in classification_instructions]
+                               "Which of the choices, best describes the following text:"]  # TODO How to share
+# instructions?
+classification_instructions = [TextualInstruction(text=x) for x in classification_instructions]
 
-instructions = [classification_instructions]
+instructions = classification_instructions
 card = ClassificationCard(
     loader=LoadHF(path='ethos'),
-    label="label",
+    label_name="label",
     label2string={"0": 'hate speech', "1": 'not hate speech'},
     inputs=['text'],
-    outputs=['label'],
-    metrics=['accuracy'],
+    metrics=['metrics.accuracy'],
     preprocess_steps=[
         "splitter.single_split_test"
     ],
@@ -50,5 +55,8 @@ card = ClassificationCard(
                   outputs='{label}'
               )
 )
-add_to_catalog(card, 'ethos-test-only', 'cards', overwrite=True)
-load_dataset()
+
+project_dir = Path(__file__).parent.parent.parent.absolute()
+catalog_dir = os.path.join(project_dir, 'fm_eval', 'catalogs', 'private')
+test_card(card)
+add_to_catalog(card, 'cards.ethos', overwrite=True, catalog_path=catalog_dir)
