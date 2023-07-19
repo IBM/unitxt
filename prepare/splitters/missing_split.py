8bc02a01eacbbf5f19f062711527d9d87527f70d
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-07-19 18:07:38 +0300

    fix imports

--- prepare/splitters/missing_split.py
+++ prepare/splitters/missing_split.py
@@ -1,3 +1,6 @@
+from src.unitxt.splitters import SplitRandomMix, SliceSplit
+from src.unitxt import add_to_catalog
+
 add_to_catalog(SplitRandomMix({'train': 'train[95%]', 'validation': 'train[5%]', 'test': 'validation'}),
                'splitters.small_no_test', overwrite=True)
 add_to_catalog(SliceSplit({'train': 'train[-500]', 'validation': 'train[500]', 'test': 'validation'}),
