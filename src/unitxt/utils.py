24f29c30b9f392ff210f2b4bb639c940681d1f9f
Elron Bandel <elron.bandel@ibm.com>
2023-07-20 03:45:16 -0400

    Transfer flatten dict to general utils file

--- src/unitxt/utils.py
+++ src/unitxt/utils.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 
 class Singleton(type):
     _instances = {}
@@ -5,3 +6,13 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+    
+def flatten_dict(d: Dict[str, Any], parent_key: str = "", sep: str = "_") -> Dict[str, Any]:
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
