008d629678c437329af6df2f1739ec6debfe5b7f
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-07-20 12:33:28 +0300

    updated cards

--- prepare/cards/ethos.py
+++ prepare/cards/ethos.py
@@ -1,62 +1,76 @@
-import os
-from pathlib import Path
-
-from unitxt.blocks import (
+from src.unitxt.blocks import (
     LoadHF,
-    SplitRandomMix,
-    AddFields,
-    TaskCard,
-    NormalizeListFields,
-    FormTask,
     TemplatesList,
-    InputOutputTemplate,
-    MapInstanceValues
+    InputOutputTemplate
 )
-from unitxt.card import ClassificationCard
+from src.unitxt.catalog import add_to_catalog
+from src.unitxt.test_utils.card import test_card
+from src.unitxt.prepare_utils.card_types import create_sentence_classification_card
+from src.unitxt.templates import outputs_inputs2templates
+from src.unitxt.operators import RenameFields
+from src.unitxt.splitters import RenameSplits
 
-from unitxt.catalog import add_to_catalog
-
-from unitxt.instructions import TextualInstruction
-from unitxt.templates import outputs_inputs2templates, instructions2templates
-
-# TODO convert str to instructions
-from unitxt.test_utils.card import test_card
-
-classification_instructions = ["Predict the class of the following ({choices}):",
-                               "What is the type of the following? Types:{choices}",
-                               "Which of the choices, best describes the following text:"]  # TODO How to share
-# instructions?
-classification_instructions = [TextualInstruction(text=x) for x in classification_instructions]
-
-instructions = classification_instructions
-card = ClassificationCard(
-    loader=LoadHF(path='ethos'),
+card = create_sentence_classification_card(
+    loader=LoadHF(path='ethos', name='binary'),
     label_name="label",
     label2string={"0": 'hate speech', "1": 'not hate speech'},
     inputs=['text'],
     metrics=['metrics.accuracy'],
     preprocess_steps=[
-        "splitter.single_split_test"
+        RenameSplits({'train': 'test'}),
+        "splitters.test_only"
     ],
-    templates=instructions2templates(
-        instructions=instructions,
-        templates=[
-            ItesnputOutputTemplate(
-                input_format="""
-                    {instruction} Sentence: {text}. Choices {choices}
-                """.strip(),
-                output_format='{label}',
-            ),
-        ]
-    ) +
-              outputs_inputs2templates(inputs=[
-                  """Given this sentence: {text}. Classify if it contains hatespeech. Choices: {choices}.""",
-                  """Does the following sentence contains hatespeech? Answer by choosing one of the options {choices}. sentence: {text}."""],
-                  outputs='{label}'
-              )
+    templates=outputs_inputs2templates(inputs=[
+        """Given this sentence: {text}. Classify if it contains hatespeech. Choices: {choices}.""",
+        """Does the following sentence contains hatespeech? Answer by choosing one of the options {choices}. sentence: {text}."""],
+        outputs='{label}'
+    )
+    # +
+    # instructions2templates(
+    #     instructions=sentence_classification_instructions,
+    #     templates=[
+    #         InputOutputTemplate(
+    #             input_format="""
+    #                 {instruction} Sentence: {text}. Choices {choices}
+    #             """.strip(),
+    #             output_format='{label}',
+    #         ),
+    #     ]
+    # )
 )
 
-project_dir = Path(__file__).parent.parent.parent.absolute()
-catalog_dir = os.path.join(project_dir, 'fm_eval', 'catalogs', 'private')
 test_card(card)
-add_to_catalog(card, 'cards.ethos', overwrite=True, catalog_path=catalog_dir)
+add_to_catalog(card, 'cards.ethos_binary', overwrite=True)
+
+# # see example in https://huggingface.co/datasets/ethos/viewer/multilabel/train
+# card = create_sentence_multilabel_classification_card(
+#     loader=LoadHF(path='ethos', name='multilabel'),
+#     label_name="label",
+#     label2string={"0": 'hate speech', "1": 'not hate speech'},
+#     inputs=['text'],
+#     metrics=['metrics.accuracy'],
+#     preprocess_steps=[
+#         "splitters.test_only"
+#     ],
+#     templates=outputs_inputs2templates(inputs=[
+#                   """Given this sentence: {text}. Classify if it contains hatespeech. Choices: {choices}.""",
+#                   """Does the following sentence contains hatespeech? Answer by choosing one of the options {choices}. sentence: {text}."""],
+#                   outputs='{label}'
+#               )
+#     # +
+#     # instructions2templates(
+#     #     instructions=sentence_classification_instructions,
+#     #     templates=[
+#     #         InputOutputTemplate(
+#     #             input_format="""
+#     #                 {instruction} Sentence: {text}. Choices {choices}
+#     #             """.strip(),
+#     #             output_format='{label}',
+#     #         ),
+#     #     ]
+#     # )
+#
+# )
+#
+# test_card(card)
+# add_to_catalog(card, 'cards.ethos_multilabel', overwrite=True)
