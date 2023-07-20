6dcb444a0d00d618af9a26a0b0ed1f40da33e938
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-07-20 11:35:55 +0300

    classification block

--- src/unitxt/prepare_utils/card_types.py
+++ src/unitxt/prepare_utils/card_types.py
@@ -10,30 +10,34 @@ from ..task import FormTask
 from ..templates import TemplatesDict, TemplatesList
 
 
+def addClassificationChoices(label_name, label2string):
+    return [MapInstanceValues(mappers={label_name: label2string}),
+            AddFields(
+                fields={
+                    'choices': list(sorted(label2string.keys())),
+                }
+            ),
+            NormalizeListFields(
+                fields=['choices']
+            ), ]
+
+
 def create_2sentences_classification_card(loader: Loader, label_name: str, label2string: Dict, inputs: List[str],
                                           metrics: List[str] = tuple('accuracy'), task: FormTask = None,
                                           preprocess_steps: Optional[List[StreamingOperator]] = None,
                                           templates: Union[TemplatesList, TemplatesDict] = None,
                                           instructions: Union[InstructionsList, InstructionsDict] = None
                                           ) -> TaskCard:
-    assert len(inputs) == 2, f"expected only 2 columns as input but recieved {inputs}"
+    assert len(inputs) == 2, f"expected only 2 columns as input but received {inputs}"
     sentence1_col = "sentence1"
     sentence2_col = "sentence2"
     preprocess_steps += [
-        MapInstanceValues(mappers={label_name: label2string}),
-        AddFields(
-            fields={
-                'choices': list(sorted(label2string.keys())),
-            }
-        ),
-        NormalizeListFields(
-            fields=['choices']
-        ),
+        *addClassificationChoices(label_name, label2string),
         RenameFields({inputs[0]: sentence1_col, inputs[1]: sentence2_col})
     ]
     if task is None:
         task = FormTask(
-            inputs=['choices'] + inputs,
+            inputs=['choices'] + [sentence1_col, sentence2_col],
             outputs=[label_name],
             metrics=metrics
         )
@@ -64,20 +68,12 @@ def create_sentence_classification_card(loader: Loader, label_name: str, label2s
     assert len(inputs) == 1, f"expected only 1 column as input but recieved {inputs}"
     sentence_col = "sentence1"
     preprocess_steps += [
-        MapInstanceValues(mappers={label_name: label2string}),
-        AddFields(
-            fields={
-                'choices': list(sorted(label2string.keys())),
-            }
-        ),
-        NormalizeListFields(
-            fields=['choices']
-        ),
+        *addClassificationChoices(label_name, label2string),
         RenameFields({inputs[0]: sentence_col})
     ]
     if task is None:
         task = FormTask(
-            inputs=['choices'] + inputs,
+            inputs=['choices'] + [sentence_col],
             outputs=[label_name],
             metrics=metrics
         )
