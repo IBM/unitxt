683d5f7a796128712c817c443172ff55052e49bd
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-07-23 10:52:09 +0300

    temporarily change large tests to 1% until fixed size is supported

--- prepare/splitters/missing_split.py
+++ prepare/splitters/missing_split.py
@@ -3,11 +3,11 @@ from src.unitxt import add_to_catalog
 
 add_to_catalog(SplitRandomMix({'train': 'train[95%]', 'validation': 'train[5%]', 'test': 'validation'}),
                'splitters.small_no_test', overwrite=True)
-add_to_catalog(SliceSplit({'train': 'train[-500]', 'validation': 'train[500]', 'test': 'validation'}),
-               'splitters.large_no_test', overwrite=True)
+add_to_catalog(SplitRandomMix({'train': 'train[99%]', 'validation': 'train[1%]', 'test': 'validation'}),
+               'splitters.large_no_test', overwrite=True) # Note, this would be change in future version to a constant amount of examples (500) instead of 1% of the examples
+add_to_catalog(SplitRandomMix({'train': 'train[99%]', 'validation': 'train[1%]', 'test': 'test'}),
+               'splitters.large_no_dev', overwrite=True) # Note, this would be change in future version to a constant amount of examples (500) instead of 1% of the examples
 add_to_catalog(SplitRandomMix({'train': 'train[95%]', 'validation': 'train[5%]', 'test': 'test'}),
                'splitters.small_no_dev', overwrite=True)
-add_to_catalog(SliceSplit({'train': 'train[-500]', 'validation': 'train[500]', 'test': 'test'}),
-               'splitters.large_no_dev', overwrite=True)
-add_to_catalog(SliceSplit({'train': 'train[0]', 'validation': 'train[0]', 'test': 'test'}),
+add_to_catalog(SplitRandomMix({'train': 'test[0%]', 'validation': 'test[0%]', 'test': 'test'}),
                'splitters.test_only', overwrite=True)
