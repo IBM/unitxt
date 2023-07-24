5995791a8c87a244392d51e5ed5fbba0c3664d7b
Elron Bandel <elron.bandel@ibm.com>
2023-07-24 03:28:39 -0400

    Add tests for cards preprations

---
+++ tests/test_preperation.py
@@ -0,0 +1,20 @@
+import unittest
+import glob
+import os
+
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+glob_query = os.path.join(project_dir, 'prepare', '**', '*.py')
+all_prepration_files = glob.glob(glob_query, recursive=True)
+
+class TestExamples(unittest.TestCase):
+    
+    def test_preprations(self):
+        print(glob_query)
+        print(f"Testing prepration files: {all_prepration_files}")
+        for file in all_prepration_files:
+            with self.subTest(file=file):
+                print(f"Testing prepration file: {file}")
+                with open(file, 'r') as f:
+                    exec(f.read())
+                print(f"Testing prepration file: {file} passed")
+                self.assertTrue(True)
