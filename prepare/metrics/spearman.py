8264fdf41fca35110dab8fb877706dc6adfb6354
Elron Bandel <elron.bandel@ibm.com>
2023-07-24 01:57:28 -0400

    Add stsb with minimal code

---
+++ prepare/metrics/spearman.py
@@ -0,0 +1,42 @@
+
+
+from src.unitxt.metrics import HFSpearman, MetricPipeline, HuggingfaceMetric
+from src.unitxt.test_utils.metrics import test_metric
+from src.unitxt import add_to_catalog
+from src.unitxt.blocks import CastFields, CopyPasteFields
+import numpy as np
+
+metric = MetricPipeline(
+    main_score='spearmanr',
+    preprocess_steps=[
+        CopyPasteFields(mapping=[('references/0', 'references')], use_dpath=True),
+        CastFields(
+            fields={'prediction': 'float', 'references': 'float'}, 
+            failure_defaults={'prediction': 0.0}, 
+            use_dpath=True,
+        ),
+    ],
+    metric=HuggingfaceMetric(
+        metric_name='spearmanr',
+        main_score='spearmanr',
+    )
+)
+
+predictions = ['1.0',' 2.0', '1.0']
+references = [['-1.0'], ['1.0'], ['0.0']]
+
+instance_targets = [{'spearmanr': np.nan, 'score': np.nan},
+                    {'spearmanr': np.nan, 'score': np.nan},
+                    {'spearmanr': np.nan, 'score': np.nan}]
+
+global_target = {'spearmanr': 0.87, 'score': 0.87}
+
+outputs = test_metric(
+    metric=metric, 
+    predictions=predictions, 
+    references=references, 
+    instance_targets=instance_targets, 
+    global_target=global_target
+)
+
+add_to_catalog(metric, 'metrics.spearman', overwrite=True)
