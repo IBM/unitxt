4ba7b99c7326ff3d9f63d0918ebc4c1d63649383
Yoav Katz <katz@il.ibm.com>
2023-07-24 16:18:17 +0300

    Added LoadFromIBMCloud loader
    Allows downloading of files from IBM Cloud Object Storage.  Requires ACCESS_KEY_ID, SECRET_ACCESS_KEY and ENDPOINT_URL credentials.
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- src/unitxt/loaders.py
+++ src/unitxt/loaders.py
@@ -1,9 +1,17 @@
 from typing import Mapping, Optional, Sequence, Union
-
 from datasets import load_dataset as hf_load_dataset
-
+from tempfile import TemporaryDirectory
+from tqdm import tqdm
+import os
 from .operator import SourceOperator
 from .stream import MultiStream
+
+try:
+    import ibm_boto3
+    from ibm_botocore.client import ClientError
+    ibm_boto3_available = True
+except ImportError:
+    ibm_boto3_available = False
 
 
 class Loader(SourceOperator):
@@ -21,5 +29,54 @@ class LoadHF(Loader):
         dataset = hf_load_dataset(
             self.path, name=self.name, data_dir=self.data_dir, data_files=self.data_files, streaming=True
         )
+
+        return MultiStream.from_iterables(dataset)
+
+
+class LoadFromIBMCloud(Loader):
+    endpoint_url_env: str
+    aws_access_key_id_env: str
+    aws_secret_access_key_env: str
+    bucket_name : str
+    data_dir: str
+    data_files: Sequence[str]
+
+    def _download_from_cos(self,cos, bucket_name, item_name, local_file):
+        print(f"Downloading {item_name} from {bucket_name} COS to {local_file}")
+        response = cos.Object(bucket_name, item_name).get()
+        size = response['ContentLength']
+        progress_bar = tqdm(total=size, unit='iB', unit_scale=True)
+
+        def upload_progress(chunk):
+            progress_bar.update(chunk)
+        try:
+            cos.Bucket(bucket_name).download_file(item_name, local_file, Callback=upload_progress)
+            print("\nDownload Successful")
+        except Exception as e:
+            raise Exception(f"Unabled to access {item_name} in {bucket_name}",e)
+    
+
+    def prepare(self):
+        super().prepare()
+        self.endpoint_url=os.getenv(self.endpoint_url_env)
+        self.aws_access_key_id=os.getenv(self.aws_access_key_id_env)
+        self.aws_secret_access_key=os.getenv(self.aws_secret_access_key_env) 
+
+    def verify(self):
+        super().verify()
+        assert ibm_boto3_available,"fPlease install ibm_boto3 using pip install ibm... in order to use LoadHFFromIBMCOS"
+        assert self.endpoint_url is not None, f"Please set the {self.endpoint_url_env} environmental variable"
+        assert self.aws_access_key_id is not None, f"Please set {self.aws_access_key_id_env} environmental variable"
+        assert self.aws_secret_access_key is not None, f"Please set {self.aws_secret_access_key_env} environmental variable"
+
+    def process(self):                
+        cos = ibm_boto3.resource('s3',aws_access_key_id=self.aws_access_key_id,
+                                      aws_secret_access_key=self.aws_secret_access_key,
+                                      endpoint_url=self.endpoint_url)
+        
+        with TemporaryDirectory() as temp_directory:            
+            for data_file in self.data_files:
+                self._download_from_cos(cos,self.bucket_name,self.data_dir + '/' + data_file,temp_directory+"/" + data_file)
+            dataset = hf_load_dataset(temp_directory, streaming=False)
 
         return MultiStream.from_iterables(dataset)
