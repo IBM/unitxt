718aa8b80e348be97d3ddb864a9b8193b605811f
Elron Bandel <elron.bandel@ibm.com>
2023-07-24 01:48:01 -0400

    Improve Tests: for operators and metrics

--- src/unitxt/test_utils/metrics.py
+++ src/unitxt/test_utils/metrics.py
@@ -4,6 +4,20 @@ from ..type_utils import isoftype
 from typing import List
 import json
 
+def round_floats(obj, precision=2, recursive=True):
+    if isinstance(obj, float):
+        return round(obj, precision)
+    if isinstance(obj, dict) and recursive:
+        return {k: round_floats(v) for k, v in obj.items()}
+    elif isinstance(obj, (list, tuple)) and recursive:
+        return [round_floats(x) for x in obj]
+    else:
+        return obj
+
+def dict_equal(dict1, dict2):
+    return json.dumps(dict1, sort_keys=True) == json.dumps(dict2, sort_keys=True)
+
+
 def apply_metric(metric:Metric, predictions: List[str], references: List[List[str]]):
     
     assert isoftype(metric, Metric), "operator must be an Operator"
@@ -16,16 +30,27 @@ def apply_metric(metric:Metric, predictions: List[str], references: List[List[st
     output_stream = output_multi_stream['test']
     return list(output_stream)
     
-def test_metric(metric:Metric, inputs: List[dict], targets: List[dict]):
+def test_metric(metric:Metric, predictions: List[str], references: List[List[str]], instance_targets: List[dict], global_target: dict):
     
     assert isoftype(metric, Metric), "operator must be an Operator"
-    assert isoftype(inputs, List[dict]), "inputs must be a list of dicts"
-    assert isoftype(outputs, List[dict]), "outputs must be a list of dicts"
+    assert isoftype(predictions, List[str]), "predictions must be a list of strings"
+    assert isoftype(references, List[List[str]]), "references must be a list of lists of strings"
     
-    outputs = apply_metric(metric, inputs)
+    outputs = apply_metric(metric, predictions, references)
     
-    for input, output in zip(outputs, targets):
-        assert json.dumps(input, sort_keys=True) == json.dumps(output, sort_keys=True), "input and output must be equal"
+    errors = []
+    global_score = round_floats(outputs[0]['score']['global'])
+    if not dict_equal(global_score, global_target):
+        errors.append(f"global score must be equal, got <{global_score}> =/= <{global_target}>")
+        
+    for output, instance_target in zip(outputs, instance_targets):
+        instance_score = round_floats(output['score']['instance'])
+        if not dict_equal(instance_score, instance_target):
+            errors.append(f"instance score must be equal, got <{instance_score}> =/= <{instance_target}>")
     
+    if len(errors) > 0:
+        raise AssertionError("\n".join(errors))
+    
+    print('Metric tested successfully!')
     return True
     
