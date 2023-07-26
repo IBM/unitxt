6f44a968e6b0468a0a2ddbfd9d18ae4889a3a58d
Yoav Katz <katz@il.ibm.com>
2023-07-26 18:05:46 +0300

    Updated the F1 code and improved testing
    1. Corrected the code when there are predictions that are not part of the allowed classes
    2. Added the F1 of each of the classes when calculating macro f1
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -40,49 +40,43 @@ class TestMetrics(unittest.TestCase):
             self.assertEqual(output['score']['instance'], target)
 
 
-    def test_f1(self):
-        # metric = F1()
-        # predictions = ['1976', 'Beyonce', 'climate change']
-        # references = [['1976'], ['Beyoncé and Bruno Mars'], ['climate change']]
-        # instance_targets = [{'f1': 1.0,'score': 1.0},
-        #                     {'f1': 0.0,'score': 0.0},
-        #                     {'f1': 1.0,'score': 1.0}]
-        # global_target =  2 /3
-        # outputs = apply_metric(metric=metric, predictions=predictions, references=references)
-
-        # self.assertAlmostEqual(global_target, outputs[0]['score']['global']['score'])
-        # for output, target in zip(outputs, instance_targets):
-        #     self.assertEqual(output['score']['instance'], target)
-
-        metric = F1()
-        references = [['0'], ['1'], ['0'], ['1'], ['0']]
-        predictions = ['0', '0', '1', '1', '0']
-        global_target = 0.5
-        outputs = apply_metric(metric=metric, predictions=predictions, references=references)
-
-        self.assertAlmostEqual(global_target, outputs[0]['score']['global']['score'])
-
-
     def test_f1_micro(self):
         metric = F1Micro()
-        predictions = ['1976', 'Beyonce', 'climate change']
-        references = [['1976'], ['Beyoncé and Bruno Mars'], ['climate change']]
-        instance_targets = [{'f1': 1.0,'score': 1.0},
-                            {'f1': 0.0,'score': 0.0},
-                            {'f1': 1.0,'score': 1.0}]
-        global_target =  2 /3
+        references =  [['cat'], ['dog'], ['dog'], ['dog'], ['cat'] , ['cat']]
+        predictions = [ 'cat'  , 'cat'  , 'dog',   'dog',   'cat'  ,  'cat' ]
+        # F1 micro is equal to accuracy in multi class setting  (5/6)
+        global_target = 0.8333333
         outputs = apply_metric(metric=metric, predictions=predictions, references=references)
+        self.assertAlmostEqual(global_target, outputs[0]['score']['global']['score'])
 
     def test_f1_macro(self):
         metric = F1Macro()
-        predictions = ['0', '2', '1', '0', '0', '1']
-        references = [['0'], ['1'], ['2'], ['0'], ['1'], ['2']]
-        # recall class 0 -> 2/2 = 1.0    precision class 0 -> 2/3 = 0.667       f1 class 0 = 0.8 
-        # recall class 1 -> 0/2 = 0      precision class 1 -> 0/2 = 0           f1 class 1 = 0 
-        # recall class 2 -> 0/2 = 0      precision class 2 -> 0/0 = 0           f1 class 2 = 0
-        #
-        # macro f1 = 0.8 / 3 = 0.266..
-        global_target = 0.26666666666
-        outputs = apply_metric(metric=metric, predictions=predictions, references=references)
+        references =  [['cat'], ['dog'], ['dog'], ['dog'], ['cat'] , ['cat']]
+        predictions = [ 'cat'  , 'cat'  , 'dog',   'dog',   'cat'  ,  'cat' ]
+        # recall class 'dog'  = 2/3  = 0.666        precision= 2/2 = 1    f1 = 0.8
+        # recall class 'cat'  = 3/3  = 1            precision= 3/4 = 0.75 f1  =0.857142857143
+        # macro f1 = (0.8+0.847)/2
+        global_target = 0.82857142
+        global_target_dog = 0.8
+        global_target_cat = 0.857142857143
 
+        outputs = apply_metric(metric=metric, predictions=predictions, references=references)
+        self.assertAlmostEqual(global_target, outputs[0]['score']['global']['score'])
+        self.assertAlmostEqual(global_target_dog, outputs[0]['score']['global']['f1_dog'])
+        self.assertAlmostEqual(global_target_cat, outputs[0]['score']['global']['f1_cat'])
+
+    def test_f1_macro_with_ood_predictions(self):
+        metric = F1Macro()
+        references =  [['cat'], ['dog'], ['dog'], ['dog'], ['cat'] , ['cat']]
+        predictions = [ 'cat'  , '2'  , 'dog',   'dog',   'cat'  ,  'cat' ]
+        # recall class 'dog'  = 2/3  = 0.666        precision= 2/2 = 1    f1 = 0.8
+        # recall class 'cat'  = 3/3  = 1            precision= 3/3 = 1    f1  =1
+        # macro f1 = 0.9
+        global_target = 0.9
+        global_target_dog = 0.8
+        global_target_cat = 1
+        
+        outputs = apply_metric(metric=metric, predictions=predictions, references=references)
+        self.assertAlmostEqual(global_target_dog, outputs[0]['score']['global']['f1_dog'])
+        self.assertAlmostEqual(global_target_cat, outputs[0]['score']['global']['f1_cat'])
         self.assertAlmostEqual(global_target, outputs[0]['score']['global']['score'])
