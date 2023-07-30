d070927d5908f46c66c4ab1e86e4eb460fe0da40
Elad Venezian <eladv@il.ibm.com>
2023-07-30 09:51:41 +0300

    Remove pip install -e from github actions test flow.

--- tests/test_hf_cache.py
+++ tests/test_hf_cache.py
@@ -1,5 +1,8 @@
-
+import os.path
+import shutil
 import unittest
+from pathlib import Path
+
 from src import unitxt
 from datasets import load_dataset
 from src.unitxt.blocks import (
@@ -88,23 +91,35 @@ spearman_metric = MetricPipeline(
     )
 )
 
+catalog_path = os.path.join(Path(__file__).parent, 'temp_catalog')
+
 
 class TestHfCache(unittest.TestCase):
-    pass
-    # def test_hf_cache_enabling(self):
-    #     add_to_catalog(wnli_recipe, 'tmp.recipes.wnli', overwrite=True)
-    #     wnli_dataset = load_dataset(unitxt.dataset_file, 'tmp.recipes.wnli')
-    #     add_to_catalog(rte_recipe, 'tmp.recipes.wnli', overwrite=True)
-    #     rte_dataset = load_dataset(unitxt.dataset_file, 'tmp.recipes.wnli')
-    #     self.assertEqual(rte_dataset['train'][0], wnli_dataset['train'][0])
-    #
-    # def test_hf_dataset_cache_disabling(self):
-    #     add_to_catalog(wnli_recipe, 'tmp.recipes.wnli2', overwrite=True)
-    #     wnli_dataset = load_dataset(unitxt.dataset_file, 'tmp.recipes.wnli2', download_mode='force_redownload')
-    #     add_to_catalog(rte_recipe, 'tmp.recipes.wnli2', overwrite=True)
-    #     rte_dataset = load_dataset(unitxt.dataset_file, 'tmp.recipes.wnli2', download_mode='force_redownload')
-    #     self.assertNotEqual(rte_dataset['train'][0]['source'], wnli_dataset['train'][0]['source'])
-    #
+
+    @classmethod
+    def setUpClass(cls):
+        if os.path.exists(catalog_path):
+            shutil.rmtree(catalog_path)  # if for some reason prev test didn't finished and didn't clean the dir.
+        os.mkdir(catalog_path)
+        os.environ["UNITXT_ARTIFACTORIES"] = catalog_path
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(catalog_path)
+
+    def test_hf_cache_enabling(self):
+        add_to_catalog(wnli_recipe, 'tmp.recipes.wnli', catalog_path=catalog_path, overwrite=True)
+        wnli_dataset = load_dataset(unitxt.dataset_file, 'tmp.recipes.wnli')
+        add_to_catalog(rte_recipe, 'tmp.recipes.wnli', catalog_path=catalog_path, overwrite=True)
+        rte_dataset = load_dataset(unitxt.dataset_file, 'tmp.recipes.wnli')
+        self.assertEqual(rte_dataset['train'][0], wnli_dataset['train'][0])
+
+    def test_hf_dataset_cache_disabling(self):
+        add_to_catalog(wnli_recipe, 'tmp.recipes.wnli2', catalog_path=catalog_path, overwrite=True)
+        wnli_dataset = load_dataset(unitxt.dataset_file, 'tmp.recipes.wnli2', download_mode='force_redownload')
+        add_to_catalog(rte_recipe, 'tmp.recipes.wnli2', catalog_path=catalog_path, overwrite=True)
+        rte_dataset = load_dataset(unitxt.dataset_file, 'tmp.recipes.wnli2', download_mode='force_redownload')
+        self.assertNotEqual(rte_dataset['train'][0]['source'], wnli_dataset['train'][0]['source'])
 
 
 if __name__ == '__main__':
