d5233d7c5bec676bb5427e311e393a7d566ff1c7
Yoav Katz <katz@il.ibm.com>
2023-07-30 20:19:34 +0300

    Add cnn dailymail
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

---
+++ prepare/cards/cnn_dailymail.py
@@ -0,0 +1,36 @@
+from src.unitxt.blocks import (
+    LoadHF,
+    SplitRandomMix,
+    TaskCard,
+    FormTask,
+    TemplatesList,
+    InputOutputTemplate,
+    CopyFields,
+)
+from src.unitxt.test_utils.card import test_card
+
+from src.unitxt.catalog import add_to_catalog
+
+card = TaskCard(
+    loader=LoadHF(path="cnn_dailymail",name="3.0.0"),
+    preprocess_steps=[
+       # SplitRandomMix({"train": "train[95%]", "validation": "train[5%]", "test": "validation"}),
+        #CopyFields(field_to_field=[["answers/text", "answer"]], use_query=True),
+    ],
+    task=FormTask(
+        inputs=["article"],
+        outputs=["highlights"],
+        metrics=["metrics.rouge"],
+    ),
+    templates=TemplatesList(
+        [
+            InputOutputTemplate(
+                input_format="{article}",
+                output_format="{highlights}"
+            ),
+        ]
+    ),
+)
+
+test_card(card)
+add_to_catalog(card, "cards.cnn_dailymail", overwrite=True)
