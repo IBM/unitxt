4ac3b0cd0b0883c2de47830180f50b1cd89f4be2
Elad Venezian <eladv@il.ibm.com>
2023-08-01 15:48:32 +0300

    Versioning: add imports to needed files

--- src/unitxt/__init__.py
+++ src/unitxt/__init__.py
@@ -1,6 +1,7 @@
 import os
 import random
 
+from ._version import get_versions
 from .catalog import add_to_catalog
 from .load import load_dataset
 from .register import register_all_artifacts, register_local_catalog
@@ -13,6 +14,5 @@ metric_file = os.path.join(os.path.dirname(__file__), "metric.py")
 dataset_url = "unitxt/data"
 metric_url = "unitxt/metric"
 
-from . import _version
 
-__version__ = _version.get_versions()["version"]
+__version__ = get_versions()["version"]
