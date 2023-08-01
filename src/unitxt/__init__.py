bf85dfba976df6ca431cc9fd056d8c23b8b4253a
Elad Venezian <eladv@il.ibm.com>
2023-08-01 15:34:49 +0300

    Versioning: formatting

--- src/unitxt/__init__.py
+++ src/unitxt/__init__.py
@@ -14,4 +14,5 @@ dataset_url = "unitxt/data"
 metric_url = "unitxt/metric"
 
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]
