ec976dd957ae7f76d18ed6cc51d660d143143cea
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-08-03 15:42:20 +0300

    extra lmeval datasets

---
+++ prepare/cards/hellaswag.py
@@ -0,0 +1,51 @@
+from datasets import load_dataset_builder
+from prepare.cards.mmlu import MMLU_TEMPLATES, multiple_choice_preprocess
+from src.unitxt.blocks import (
+    AddFields,
+    FormTask,
+    InputOutputTemplate,
+    LoadHF,
+    MapInstanceValues,
+    NormalizeListFields,
+    SplitRandomMix,
+    TaskCard,
+    TemplatesList,
+)
+from src.unitxt.catalog import add_to_catalog
+from src.unitxt.operators import (
+    CopyFields,
+    IndexOf,
+    JoinStr,
+    RenameFields,
+    TakeByField,
+    ZipFieldValues,
+)
+from src.unitxt.test_utils.card import test_card
+
+# numbering=tuple(str(x) for x in range(200))
+numbering = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+expected_answer = "number"  # 'number_and_answer' #'number'
+
+card = TaskCard(
+    loader=LoadHF(path="hellaswag"),
+    preprocess_steps=[
+        AddFields({"topic": "activity_label"}),
+        RenameFields(field_to_field={"answerKey": "label", "choices": "choices_struct"}),
+        CopyFields(
+            field_to_field={"choices_struct/text": "choices", "choices_struct/label": "numbering"}, use_query=True
+        ),
+        IndexOf(search_in="numbering", index_of="label", to_field="index"),
+        *multiple_choice_preprocess(question="question", numbering="numbering", choices="choices", topic="topic",
+                                    label_index="index"),
+    ],
+    task=FormTask(
+        inputs=["choices", "sentence1", "numbers", "topic"],
+        outputs=[
+            "label",
+        ],
+        metrics=["metrics.accuracy"],
+    ),
+    templates=MMLU_TEMPLATES,
+)
+test_card(card)
+add_to_catalog(card, f'cards.hellaswag', overwrite=True)
---
+++ prepare/cards/openbookqa.py
@@ -0,0 +1,48 @@
+from datasets import load_dataset_builder
+from prepare.cards.mmlu import MMLU_TEMPLATES, multiple_choice_preprocess, multiple_choice_inputs_outputs
+from src.unitxt.blocks import (
+    AddFields,
+    FormTask,
+    InputOutputTemplate,
+    LoadHF,
+    MapInstanceValues,
+    NormalizeListFields,
+    SplitRandomMix,
+    TaskCard,
+    TemplatesList,
+)
+from src.unitxt.catalog import add_to_catalog
+from src.unitxt.operators import (
+    CastFields,
+    CopyFields,
+    IndexOf,
+    JoinStr,
+    RenameFields,
+    TakeByField,
+    ZipFieldValues, ListFieldValues, AddConstant,
+)
+from src.unitxt.test_utils.card import test_card
+
+# numbering=tuple(str(x) for x in range(200))
+numbering = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+expected_answer = 'number'  # 'number_and_answer' #'number'
+
+card = TaskCard(
+    loader=LoadHF(path='vietgpt/openbookqa_en'),
+    preprocess_steps=[
+        AddFields({'topic': 'general continuation', 'numbering': numbering, }, ),
+        RenameFields(field_to_field={"choices/text": "text",
+                                     "choices/label": "numbering"}, use_query=True),
+        IndexOf(search_in="numbering", index_of="answerKey", to_field="index"),
+        *multiple_choice_preprocess(question="question_stem", numbering='numbering', choices='text', topic='topic',
+                                    label_index='index'),
+    ],
+    task=FormTask(
+        **multiple_choice_inputs_outputs(),
+
+        metrics=['metrics.accuracy'],
+    ),
+    templates=MMLU_TEMPLATES,
+)
+test_card(card)
+add_to_catalog(card, f"cards.openbookQA", overwrite=True)
