d962a1f426a3e0049589b90ad760c0cd451a45d7
Elad Venezian <eladv@il.ibm.com>
2023-08-09 12:34:55 +0300

    NER: don't report results for classes which are not in the classes support

--- prepare/metrics/custom_f1.py
+++ prepare/metrics/custom_f1.py
@@ -12,10 +12,10 @@ references = [[("Yaron", "Person"), ("Ran", "Person")], [("Yonatan", "Person")]]
 # total precision = 2/4, recall = 2/3
 # F1 = 2 * 1/2 * 2/3 / (1/2 + 2/3) = 0.57
 instance_targets = [
-    {"f1_Person": 0.5, "f1_macro": 0.5, "f1_micro": 0.5, "score": 0.5},
-    {"f1_Person": 0.67, "f1_macro": 0.67, "f1_micro": 0.67, "score": 0.67},
+    {"f1_Person": 0.5, "f1_macro": 0.5, "in_classes_support": 1.0, "f1_micro": 0.5, "score": 0.5},
+    {"f1_Person": 0.67, "f1_macro": 0.67, "in_classes_support": 1.0, "f1_micro": 0.67, "score": 0.67},
 ]
-global_target = {"f1_Person": 0.57, "f1_macro": 0.57, "f1_micro": 0.57, "score": 0.57}
+global_target = {"f1_Person": 0.57, "f1_macro": 0.57, "in_classes_support": 1.0, "f1_micro": 0.57, "score": 0.57}
 
 outputs = test_metric(
     metric=metric,
@@ -29,8 +29,8 @@ outputs = test_metric(
 predictions = [[("Amir", "Person"), ("Yaron", "Person")]]
 references = [[("Yaron", "Person"), ("Ran", "Person"), ("Yonatan", "Person")]]
 # Precision = 1/2, Recall = 1/3, F1 = 2 * 1/2 * 1/3 / (1/2 + 1/3) = 0.4
-instance_targets = [{"f1_Person": 0.4, "f1_macro": 0.4, "f1_micro": 0.4, "score": 0.4}]
-global_target = {"f1_Person": 0.4, "f1_macro": 0.4, "f1_micro": 0.4, "score": 0.4}
+instance_targets = [{"f1_Person": 0.4, "f1_macro": 0.4, "in_classes_support": 1.0, "f1_micro": 0.4, "score": 0.4}]
+global_target = {"f1_Person": 0.4, "f1_macro": 0.4, "in_classes_support": 1.0, "f1_micro": 0.4, "score": 0.4}
 
 outputs = test_metric(
     metric=metric,
@@ -43,8 +43,8 @@ outputs = test_metric(
 predictions = [[("Amir", "Person"), ("Yaron", "Person"), ("Yaron", "Person")]]
 references = [[("Yaron", "Person"), ("Ran", "Person"), ("Yonatan", "Person")]]
 # Precision = 1/3, Recall = 1/3, F1 = 2 * 1/3 * 1/3 / (1/3 + 1/3) = 0.333333
-instance_targets = [{"f1_Person": 0.33, "f1_macro": 0.33, "f1_micro": 0.33, "score": 0.33}]
-global_target = {"f1_Person": 0.33, "f1_macro": 0.33, "f1_micro": 0.33, "score": 0.33}
+instance_targets = [{"f1_Person": 0.33, "f1_macro": 0.33, "in_classes_support": 1.0, "f1_micro": 0.33, "score": 0.33}]
+global_target = {"f1_Person": 0.33, "f1_macro": 0.33, "in_classes_support": 1.0, "f1_micro": 0.33, "score": 0.33}
 
 outputs = test_metric(
     metric=metric,
@@ -57,8 +57,8 @@ outputs = test_metric(
 predictions = [[]]
 references = [[("Yaron", "Person"), ("Ran", "Person"), ("Yonatan", "Person")]]
 # Precision = 0/0=(by def for prediction)=1, Recall = 0/3, F1 = 2 * 1 * 0 / (1 + 0) = 0
-instance_targets = [{"f1_Person": 0.0, "f1_macro": 0.0, "f1_micro": 0.0, "score": 0.0}]
-global_target = {"f1_Person": 0.0, "f1_macro": 0.0, "f1_micro": 0.0, "score": 0.0}
+instance_targets = [{"f1_Person": 0.0, "f1_macro": 0.0, "in_classes_support": 1.0, "f1_micro": 0.0, "score": 0.0}]
+global_target = {"f1_Person": 0.0, "f1_macro": 0.0, "in_classes_support": 1.0, "f1_micro": 0.0, "score": 0.0}
 
 outputs = test_metric(
     metric=metric,
@@ -94,13 +94,20 @@ references = [
 # Location: Precision = 1/2, Recall = 1/2, F1 = 0.5
 # Person: Precision = 0/3, Recall = 0/0 = 1(!), F1 = 0
 instance_targets = [
-    {"f1_Person": 0.86, "f1_Location": 0.5, "f1_Org": 0.0, "f1_macro": 0.45, "f1_micro": 0.57, "score": 0.57},
+    {
+        "f1_Location": 0.5,
+        "f1_Person": 0.86,
+        "f1_macro": 0.68,
+        "in_classes_support": 0.62,
+        "f1_micro": 0.57,
+        "score": 0.57,
+    },
 ]
 global_target = {
-    "f1_Org": 0.0,
-    "f1_Person": 0.86,
     "f1_Location": 0.5,
-    "f1_macro": 0.45,
+    "f1_Person": 0.86,
+    "f1_macro": 0.68,
+    "in_classes_support": 0.62,
     "f1_micro": 0.57,
     "score": 0.57,
 }
@@ -112,5 +119,53 @@ outputs = test_metric(
     instance_targets=instance_targets,
     global_target=global_target,
 )
+
+
+# 1.5 class in prediction and not in reference
+predictions = [
+    [
+        ("a", "A"),
+        ("b", "B"),
+        ("c", "C"),
+    ]
+]
+references = [
+    [
+        ("a", "B"),
+        ("b", "B"),
+        ("c", "C"),
+        ("d", "D"),
+    ]
+]
+
+instance_targets = [
+    {
+        "f1_D": 0.0,
+        "f1_C": 1.0,
+        "f1_B": 0.67,
+        "f1_macro": 0.56,
+        "in_classes_support": 0.67,
+        "f1_micro": 0.57,
+        "score": 0.57,
+    },
+]
+global_target = {
+    "f1_D": 0.0,
+    "f1_C": 1.0,
+    "f1_B": 0.67,
+    "f1_macro": 0.56,
+    "in_classes_support": 0.67,
+    "f1_micro": 0.57,
+    "score": 0.57,
+}
+
+outputs = test_metric(
+    metric=metric,
+    predictions=predictions,
+    references=references,
+    instance_targets=instance_targets,
+    global_target=global_target,
+)
+
 
 add_to_catalog(metric, "metrics.ner", overwrite=True)
