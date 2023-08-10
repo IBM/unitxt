0f8b21600d905442999ae71b35a2b920ef454c61
Shachar Don-Yehiya <shachar.don-yehiya@ibm.com>
2023-08-10 18:10:31 +0300

    new datasets + add the option to load dataset with streaming=Flase
    Signed-off-by: Shachar Don-Yehiya <shachar.don-yehiya@ibm.com>

---
+++ prepare/cards/qqp.py
@@ -0,0 +1,48 @@
+import datasets as ds
+from src.unitxt import dataset
+from src.unitxt.blocks import (
+    AddFields,
+    FormTask,
+    InputOutputTemplate,
+    LoadHF,
+    MapInstanceValues,
+    NormalizeListFields,
+    SplitRandomMix,
+    TaskCard,
+    TemplatesList,
+)
+from src.unitxt.catalog import add_to_catalog
+from src.unitxt.operators import RenameFields
+from src.unitxt.splitters import RenameSplits
+from src.unitxt.test_utils.card import test_card
+
+card = TaskCard(
+    loader=LoadHF(path="glue", name="qqp"),
+    preprocess_steps=[
+        "splitters.large_no_test",
+        MapInstanceValues(mappers={"label": {"0": "not duplicated", "1": "duplicated"}}),
+        AddFields(
+            fields={
+                "choices": ["not duplicated", "duplicated"],
+            }
+        ),
+    ],
+    task=FormTask(
+        inputs=["choices", "question1", "question2"],
+        outputs=["label"],
+        metrics=["metrics.accuracy"],
+    ),
+    templates=TemplatesList(
+        [
+            InputOutputTemplate(
+                input_format="""
+                    Given this question: {question1}, classify if this question: {question2} is {choices}.
+                """.strip(),
+                output_format="{label}",
+            ),
+        ]
+    ),
+)
+
+test_card(card)
+add_to_catalog(card, "cards.qqp", overwrite=True)
