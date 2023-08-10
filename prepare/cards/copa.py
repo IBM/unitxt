d0168f7da6ad743c4feab80a1dbc252b99a35d63
Leshem Choshen <leshem.choshen@mail.huji.ac.il>
2023-08-10 10:59:34 +0300

    format

--- prepare/cards/copa.py
+++ prepare/cards/copa.py
@@ -1,31 +1,34 @@
-from prepare.cards.mmlu import multiple_choice_preprocess, multiple_choice_inputs_outputs
+from prepare.cards.mmlu import (
+    multiple_choice_inputs_outputs,
+    multiple_choice_preprocess,
+)
 from src.unitxt.blocks import InputOutputTemplate, LoadHF, TemplatesList
 from src.unitxt.card import TaskCard
 from src.unitxt.catalog import add_to_catalog
+from src.unitxt.operators import AddFields, ListFieldValues
 from src.unitxt.prepare_utils.card_types import addClassificationChoices
 from src.unitxt.task import FormTask
 from src.unitxt.test_utils.card import test_card
-from src.unitxt.operators import ListFieldValues, AddFields
 
-numbering = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+numbering = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 card = TaskCard(
     loader=LoadHF(path="super_glue", name="copa"),
-    preprocess_steps=["splitters.small_no_test",
-                      AddFields({'numbering': numbering, 'topic': 'commonsense causal reasoning'}),
-
-                      ListFieldValues(fields=['choice1', 'choice2'],
-                                      to_field="choices"),
-                      *multiple_choice_preprocess(
-                          context='premise',
-                          question='question',
-                          numbering='numbering',
-                          choices='choices',
-                          topic='topic',
-                          label_index='label',
-                      )],
+    preprocess_steps=[
+        "splitters.small_no_test",
+        AddFields({"numbering": numbering, "topic": "commonsense causal reasoning"}),
+        ListFieldValues(fields=["choice1", "choice2"], to_field="choices"),
+        *multiple_choice_preprocess(
+            context="premise",
+            question="question",
+            numbering="numbering",
+            choices="choices",
+            topic="topic",
+            label_index="label",
+        ),
+    ],
     task=FormTask(
         **multiple_choice_inputs_outputs(context=True),
-        metrics=['metrics.accuracy'],
+        metrics=["metrics.accuracy"],
     ),
     templates=TemplatesList(
         [
