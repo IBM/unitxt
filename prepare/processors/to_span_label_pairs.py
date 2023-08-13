b1df112b4515e59d91902a3141d3d4bb7dd8285f
Elad Venezian <eladv@il.ibm.com>
2023-08-13 11:00:25 +0300

    Add processor for single class NER

--- prepare/processors/to_span_label_pairs.py
+++ prepare/processors/to_span_label_pairs.py
@@ -13,3 +13,17 @@ print(parser.process(example))
 assert parser.process(example) == [("h r", "hello"), ("t7 ?t", "world")]
 
 add_to_catalog(parser, "processors.to_span_label_pairs", overwrite=True)
+
+
+# parse string like "1, 2" list of tuples using regex
+regex = r"\s*([^,:]+?)\s*()\s*(?=,|$)"
+
+# test regext parser
+parser = RegexParser(regex=regex)
+
+example = "h r, t7 ?t"
+
+print(parser.process(example))
+assert parser.process(example) == [("h r", ""), ("t7 ?t", "")]
+
+add_to_catalog(parser, "processors.to_span_label_pairs_surface_only", overwrite=True)
