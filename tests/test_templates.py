43f12fb1e2e9c394d4d154ede4a7b579ed325169
Elron Bandel <elron.bandel@ibm.com>
2023-08-13 07:34:51 -0400

    format

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,13 +1,12 @@
 import unittest
 
+from src.unitxt.processors import RegexParser
 from src.unitxt.templates import (
-    SpanLabelingTemplate,
-    InputOutputTemplate,
     AutoInputOutputTemplate,
+    InputOutputTemplate,
+    SpanLabelingTemplate,
 )
 from src.unitxt.test_utils.metrics import apply_metric
-
-from src.unitxt.processors import RegexParser
 
 # parse string like "1:hlle, 2:world" list of tuples using regex
 regex = r"\s*((?:[^,:\\]|\\.)+?)\s*:\s*((?:[^,:\\]|\\.)+?)\s*(?=,|$)"
@@ -15,11 +14,11 @@ regex = r"\s*((?:[^,:\\]|\\.)+?)\s*:\s*((?:[^,:\\]|\\.)+?)\s*(?=,|$)"
 # test regext parser
 parser = RegexParser(regex=regex)
 
+
 class TestTemplates(unittest.TestCase):
-    
     def test_span_labeling_template_escaping(self):
         template = SpanLabelingTemplate()
-        
+
         inputs = [
             {
                 "spans_starts": [0, 19, 41],
@@ -28,18 +27,17 @@ class TestTemplates(unittest.TestCase):
                 "text": "John,: Doe is from New York and works at Goo:gle.",
             }
         ]
-        
+
         processed_targets = [
             "John\,\: Doe: PER, New York: LOC, Goo\:gle: ORG",
         ]
-        
+
         parsed_targets = [
-            [('John\\,\\: Doe', 'PER'), ('New York', 'LOC'), ('Goo\\:gle', 'ORG')],
+            [("John\\,\\: Doe", "PER"), ("New York", "LOC"), ("Goo\\:gle", "ORG")],
         ]
-        
+
         for input, processed_target, parsed_target in zip(inputs, processed_targets, parsed_targets):
             processed = template.process_outputs(input)
             self.assertEqual(processed, processed_target)
             parsed = parser.process(processed)
             self.assertEqual(parsed, parsed_target)
-            
