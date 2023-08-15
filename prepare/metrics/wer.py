3958043aa25fd2a51fcd647799ff948619d816d3
Yoav Katz <katz@il.ibm.com>
2023-08-15 21:55:31 +0300

    Added WER metic
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/metrics/wer.py
+++ prepare/metrics/wer.py
@@ -8,11 +8,11 @@ predictions = ["this is the prediction", "there is an other sample"]
 references = [["this is the reference"], ["there is another sample"]]
 
 instance_targets = [
-    {"wer": 0.25, "score": 0.25},  # 1 errors: reokace 'prediction' with 'reference'
-    {"wer": 0.5, "score": 0.5},  # 2 errors: remove 'an' and replace 'other' with 'another'
+    {"wer": 0.25, "score" : 0.25}, # 1 errors: reokace 'prediction' with 'reference'
+    {"wer": 0.5, "score" : 0.5},   # 2 errors: remove 'an' and replace 'other' with 'another' 
 ]
 
-global_target = {"wer": 0.38, "score": 0.38}  # Should by 0.375, but package rounds the scores
+global_target = {"wer": 0.38 ,"score" : 0.38}  # Should by 0.375, but package rounds the scores
 
 outputs = test_metric(
     metric=metric,
