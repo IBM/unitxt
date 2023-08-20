ef0be92441fa81bbaede740b6e20a65c57bd78a2
Yoav Katz <katz@il.ibm.com>
2023-08-20 12:45:59 +0300

    Formating
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/metrics/char_edit_dist_accuracy.py
+++ prepare/metrics/char_edit_dist_accuracy.py
@@ -4,14 +4,14 @@ from src.unitxt.test_utils.metrics import test_metric
 
 metric = CharEditDistanceAccuracy()
 
-predictions = ["this is the prediction",  "there is an other sample"]  
-references = [["this is the reference"], ["there is another sample"]] 
+predictions = ["this is the prediction", "there is an other sample"]
+references = [["this is the reference"], ["there is another sample"]]
 
 # First sample:   p[re]diction - edit distance (8), len(21)  accuracy = 1 - 8/22 = 0.636
-# Second samlple: [a]nother - edit distance (1), len(22)     accuracy = 1 - 1/22 = 0.958 
+# Second samlple: [a]nother - edit distance (1), len(22)     accuracy = 1 - 1/22 = 0.958
 instance_targets = [
-    {"char_edit_dist_accuracy": 0.64, "score": 0.64},  
-    {"char_edit_dist_accuracy": 0.96, "score": 0.96},  
+    {"char_edit_dist_accuracy": 0.64, "score": 0.64},
+    {"char_edit_dist_accuracy": 0.96, "score": 0.96},
 ]
 
 global_target = {"char_edit_dist_accuracy": 0.8, "score": 0.8}
--- src/unitxt/metrics.py
+++ src/unitxt/metrics.py
@@ -362,14 +362,15 @@ class CharEditDistanceAccuracy(SingleReferenceInstanceMetric):
     reduction_map = {"mean": ["char_edit_dist_accuracy"]}
     main_score = "char_edit_dist_accuracy"
 
-    def compute(self, reference, prediction: str) -> dict:  
+    def compute(self, reference, prediction: str) -> dict:
         formatted_prediction = " ".join(prediction.split())
-        formatted_reference = " ".join(reference.split())            
-        max_length = max(len(formatted_reference),len(formatted_prediction))
-        if (max_length == 0):
+        formatted_reference = " ".join(reference.split())
+        max_length = max(len(formatted_reference), len(formatted_prediction))
+        if max_length == 0:
             return 0
-        edit_dist = eval(formatted_reference,formatted_prediction)
-        return { "char_edit_dist_accuracy" : (1 - edit_dist / max_length) }
+        edit_dist = eval(formatted_reference, formatted_prediction)
+        return {"char_edit_dist_accuracy": (1 - edit_dist / max_length)}
+
 
 class Wer(HuggingfaceMetric):
     metric_name = "wer"
