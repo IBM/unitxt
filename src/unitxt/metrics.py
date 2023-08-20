64aa13abdeb68c85444699b62825ae8834a76ce8
Yoav Katz <katz@il.ibm.com>
2023-08-20 14:02:12 +0300

    Added test for multi template
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- src/unitxt/metrics.py
+++ src/unitxt/metrics.py
@@ -368,7 +368,6 @@ class CharEditDistanceAccuracy(SingleReferenceInstanceMetric):
         edit_dist = eval(formatted_reference, formatted_prediction)
         return {"char_edit_dist_accuracy": (1 - edit_dist / max_length)}
 
-
 class Wer(HuggingfaceMetric):
     metric_name = "wer"
     main_score = "wer"
