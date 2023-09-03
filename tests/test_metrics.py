fa41584e69c2d9cc5082fd142c4736d390f80bec
Assaf Toledo <assaf.toledo@ibm.com>
2023-09-03 17:41:21 +0300

    merge
    Signed-off-by: Assaf Toledo <assaf.toledo@ibm.com>

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -27,7 +27,8 @@ class TestMetrics(unittest.TestCase):
         ]
 
         global_target = {"accuracy": 1 / 3, "score": 1 / 3, "score_name": "accuracy",
-                         "accuracy_ci_high": 1.0, "accuracy_ci_low": 0.0}
+                         "accuracy_ci_high": 1.0, "accuracy_ci_low": 0.0,
+                         "score_ci_high": 1.0, "score_ci_low": 0.0}
 
         outputs = apply_metric(metric=metric, predictions=predictions, references=references)
 
