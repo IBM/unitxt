736a0f463cbf159ee06d9cbe231659ec7d898adb
elronbandel <elronbandel@gmail.com>
2023-09-11 06:31:53 -0400

    Add tests for query

---
+++ tests/test_query.py
@@ -0,0 +1,34 @@
+import unittest
+
+from src.unitxt.dataset import parse
+
+
+class TestQuery(unittest.TestCase):
+    def test_query_works(self):
+        query = "card=cards.sst2,template_item=1000,demos_pool_size=100,num_demos=0"
+        parsed = parse(query)
+        target = {
+            "card": "cards.sst2",
+            "template_item": 1000,
+            "demos_pool_size": 100,
+            "num_demos": 0,
+        }
+        self.assertDictEqual(parsed, target)
+
+    def test_empty_query_fail(self):
+        with self.assertRaises(ValueError):
+            parse("")
+        with self.assertRaises(ValueError):
+            parse(",")
+
+    def test_missing_key_fail(self):
+        with self.assertRaises(ValueError):
+            parse("=cards.sst2,template_item=1000,demos_pool_size=100,num_demos=0")
+        with self.assertRaises(ValueError):
+            parse("cards.sst2,template_item=1000,demos_pool_size=100,num_demos=0")
+
+    def test_missing_value_fail(self):
+        with self.assertRaises(ValueError):
+            parse("cards.sst2=,template_item=1000,demos_pool_size=100,num_demos=0")
+        with self.assertRaises(ValueError):
+            parse("=,template_item=1000,demos_pool_size=100,num_demos=0")
