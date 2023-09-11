86ca5ebc132d7121b239a8660f02d71112cedd5a
elronbandel <elronbandel@gmail.com>
2023-09-11 03:23:47 -0400

    format

--- prepare/instructions/models/llama.py
+++ prepare/instructions/models/llama.py
@@ -1,8 +1,6 @@
-from src.unitxt.instructions import TextualInstruction
 from src.unitxt.catalog import add_to_catalog
+from src.unitxt.instructions import TextualInstruction
 
-instruction = TextualInstruction(
-    "You are a very smart model. solve the following." 
-)
+instruction = TextualInstruction("You are a very smart model. solve the following.")
 
 add_to_catalog(instruction, f"instructions.models.llama", overwrite=True)
