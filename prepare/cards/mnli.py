3e7b0eb28d05123642e43bee7483f3073af1e9d1
elronbandel <elronbandel@gmail.com>
2023-09-13 05:34:58 -0400

    Further improve artifact sharing nli

--- prepare/cards/mnli.py
+++ prepare/cards/mnli.py
@@ -28,7 +28,7 @@ card = TaskCard(
         ),
     ],
     task="tasks.nli",
-    templates=TemplatesList(["templates.classification.nli.simple"]),
+    templates="templates.classification.nli.all",
 )
 
 test_card(card)
--- prepare/templates/qa/open.py
+++ prepare/templates/qa/open.py
@@ -1,5 +1,5 @@
 from src.unitxt.catalog import add_to_catalog
-from src.unitxt.templates import InputOutputTemplate
+from src.unitxt.templates import InputOutputTemplate, TemplatesList
 
 add_to_catalog(
     InputOutputTemplate(input_format="Question: {question}", output_format="{answer}"),
@@ -10,5 +10,16 @@ add_to_catalog(
 add_to_catalog(
     InputOutputTemplate(input_format="answer the question: {question}", output_format="{answer}"),
     "templates.qa.open.simple2",
+    overwrite=True,
+)
+
+add_to_catalog(
+    TemplatesList(
+        [
+            "templates.qa.open.simple",
+            "templates.qa.open.simple2",
+        ]
+    ),
+    "templates.qa.open.all",
     overwrite=True,
 )
