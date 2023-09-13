77d62329e5f598ea73830fb0db516795135f2219
elronbandel <elronbandel@gmail.com>
2023-09-13 05:31:01 -0400

    Further improve artifact sharing qa

--- prepare/cards/squad.py
+++ prepare/cards/squad.py
@@ -1,19 +1,11 @@
-from src.unitxt.blocks import (
-    CopyFields,
-    FormTask,
-    InputOutputTemplate,
-    LoadHF,
-    SplitRandomMix,
-    TaskCard,
-    TemplatesList,
-)
+from src.unitxt.blocks import CopyFields, FormTask, LoadHF, SplitRandomMix, TaskCard
 from src.unitxt.catalog import add_to_catalog
 from src.unitxt.test_utils.card import test_card
 
 card = TaskCard(
     loader=LoadHF(path="squad"),
     preprocess_steps=[
-        SplitRandomMix({"train": "train[95%]", "validation": "train[5%]", "test": "validation"}),
+        "splitters.small_no_test",
         CopyFields(field_to_field=[["answers/text", "answer"]], use_query=True),
     ],
     task=FormTask(
@@ -21,12 +13,7 @@ card = TaskCard(
         outputs=["answer"],
         metrics=["metrics.squad"],
     ),
-    templates=TemplatesList(
-        [
-            "templates.qa.contextual.simple",
-            "templates.qa.contextual.simple2",
-        ]
-    ),
+    templates="templates.qa.contextual.all",
 )
 
 test_card(card)
