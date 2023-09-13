3f06e85144294f3419f3106b5c4b2bc97f63395e
elronbandel <elronbandel@gmail.com>
2023-09-13 05:31:01 -0400

    Further improve artifact sharing qa

--- prepare/templates/qa/contextual.py
+++ prepare/templates/qa/contextual.py
@@ -1,5 +1,5 @@
 from src.unitxt.catalog import add_to_catalog
-from src.unitxt.templates import InputOutputTemplate
+from src.unitxt.templates import InputOutputTemplate, TemplatesList
 
 add_to_catalog(
     InputOutputTemplate(input_format="Context: {context}\nQuestion: {question}", output_format="{answer}"),
@@ -12,5 +12,16 @@ add_to_catalog(
         input_format="based on this text: {context}\n answer the question: {question}", output_format="{answer}"
     ),
     "templates.qa.contextual.simple2",
+    overwrite=True,
+)
+
+add_to_catalog(
+    TemplatesList(
+        [
+            "templates.qa.contextual.simple",
+            "templates.qa.contextual.simple2",
+        ]
+    ),
+    "templates.qa.contextual.all",
     overwrite=True,
 )
