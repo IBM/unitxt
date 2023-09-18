49a9c8300fb4f1931e6e42e53bbf3c820e78b625
elronbandel <elronbandel@gmail.com>
2023-09-18 04:57:29 -0400

    Add catlog path url shortcut

--- src/unitxt/__init__.py
+++ src/unitxt/__init__.py
@@ -9,6 +9,7 @@ register_all_artifacts()
 random.seed(0)
 dataset_file = os.path.join(os.path.dirname(__file__), "dataset.py")
 metric_file = os.path.join(os.path.dirname(__file__), "metric.py")
+local_catalog_path = os.path.join(os.path.dirname(__file__), "catalog")
 
 dataset_url = "unitxt/data"
 metric_url = "unitxt/metric"
