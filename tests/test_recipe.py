bda223a857c4f1a8def11a8842b09cd2272bef2e
elronbandel <elronbandel@gmail.com>
2023-09-19 08:08:34 -0400

    Add tests

--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -2,7 +2,7 @@ import unittest
 
 from src.unitxt.formats import ICLFormat
 from src.unitxt.instructions import TextualInstruction
-from src.unitxt.standard import StandardRecipe
+from src.unitxt.standard import StandardRecipe, StandardRecipeWithIndexes
 from src.unitxt.templates import InputOutputTemplate
 from src.unitxt.text_utils import print_dict
 
@@ -32,6 +32,54 @@ class TestRecipes(unittest.TestCase):
             card="cards.mmlu.marketing",
             instruction="instructions.models.llama",
             template="templates.mmlu.lm_eval_harness",
+            format="formats.user_agent",
+            demos_pool_size=100,
+            num_demos=3,
+        )
+
+        stream = recipe()
+
+        for instance in stream["train"]:
+            print_dict(instance)
+            break
+
+    def test_standard_recipe_with_indexes_with_catalog(self):
+        recipe = StandardRecipeWithIndexes(
+            card="cards.wnli",
+            instruction="instructions.models.llama",
+            template_card_index=0,
+            format="formats.user_agent",
+            demos_pool_size=100,
+            num_demos=3,
+        )
+
+        stream = recipe()
+
+        for instance in stream["train"]:
+            print_dict(instance)
+            break
+
+    def test_empty_template(self):
+        recipe = StandardRecipeWithIndexes(
+            card="cards.wnli",
+            instruction="instructions.models.llama",
+            template="templates.empty",
+            format="formats.user_agent",
+            demos_pool_size=100,
+            num_demos=3,
+        )
+
+        stream = recipe()
+
+        for instance in stream["train"]:
+            print_dict(instance)
+            break
+
+    def test_key_val_template(self):
+        recipe = StandardRecipeWithIndexes(
+            card="cards.wnli",
+            instruction="instructions.models.llama",
+            template="templates.key_val",
             format="formats.user_agent",
             demos_pool_size=100,
             num_demos=3,
