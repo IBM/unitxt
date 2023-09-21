1c98530a69c26447854865f82c94c9cb2d6a2484
elronbandel <elronbandel@gmail.com>
2023-09-21 11:49:29 -0400

    format

--- prepare/formats/human_assistant.py
+++ prepare/formats/human_assistant.py
@@ -1,10 +1,6 @@
 from src.unitxt.catalog import add_to_catalog
 from src.unitxt.formats import ICLFormat
 
-format = ICLFormat(
-    input_prefix="Human: ",
-    output_prefix="Assistant: ",
-    input_output_separator="\n"
-)
+format = ICLFormat(input_prefix="Human: ", output_prefix="Assistant: ", input_output_separator="\n")
 
 add_to_catalog(format, f"formats.human_assistant", overwrite=True)
