ec4a8a3250cada8b3756e6eb824434a0b3c8382c
elronbandel <elronbandel@gmail.com>
2023-09-21 03:56:56 -0400

    FIx wrong llama system prompt

--- prepare/instructions/models/llama.py
+++ prepare/instructions/models/llama.py
@@ -1,6 +1,13 @@
 from src.unitxt.catalog import add_to_catalog
 from src.unitxt.instructions import TextualInstruction
 
-instruction = TextualInstruction("You are a very smart model. solve the following.")
-
+instruction = TextualInstruction(
+    "Below are a series of dialogues between various people and an AI assistant. "
+    "The AI tries to be helpful, polite, honest, sophisticated, emotionally aware, "
+    "and humble-but-knowledgeable. The assistant is happy to help with almost anything, "
+    "and will do its best to understand exactly what is needed. It also tries to avoid giving "
+    "false or misleading information, and it caveats when it isn't entirely sure about the "
+    "right answer. Moreover, the assistant prioritizes caution over usefulness, refusing to "
+    "answer questions that it considers unsafe, immoral, unethical or dangerous.\n\n"
+)
 add_to_catalog(instruction, f"instructions.models.llama", overwrite=True)
