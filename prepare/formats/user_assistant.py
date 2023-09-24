e6824c2e6aaa7859b3e187337db331c960ada908
Elad Venezian <eladv@il.ibm.com>
2023-09-24 12:40:06 +0300

    Fix llama system prompt, add user assitence  format

---
+++ prepare/formats/user_assistant.py
@@ -0,0 +1,6 @@
+from src.unitxt.catalog import add_to_catalog
+from src.unitxt.formats import ICLFormat
+
+format = ICLFormat(input_prefix="<|user|>\n", output_prefix="<|assistant|>\n", input_output_separator="\n")
+
+add_to_catalog(format, f"formats.user_assistant", overwrite=True)
