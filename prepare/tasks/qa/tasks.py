35cbe11a83652e6219c928bab072b2122cb645c9
elronbandel <elronbandel@gmail.com>
2023-09-24 03:16:45 -0400

    prepare base tasks for question answering

---
+++ prepare/tasks/qa/tasks.py
@@ -0,0 +1,12 @@
+from src.unitxt.blocks import FormTask
+from src.unitxt.catalog import add_to_catalog
+
+add_to_catalog(
+    FormTask(
+        inputs=["context", "question"],
+        outputs=["answer"],
+        metrics=["metrics.squad"],
+    ),
+    "tasks.qa.contextual",
+    overwrite=True,
+)
