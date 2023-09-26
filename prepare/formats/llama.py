c7bf0d8f3663281577eac2016004903123952bf5
Elad Venezian <eladv@il.ibm.com>
2023-09-26 14:27:06 +0300

    Fix llama format

--- prepare/formats/llama.py
+++ prepare/formats/llama.py
@@ -2,10 +2,8 @@ from src.unitxt.catalog import add_to_catalog
 from src.unitxt.formats import ICLFormat
 
 format = ICLFormat(
-    input_prefix="User: ",
-    output_prefix="Agent: ",
     target_prefix="",
-    prefix="[INST] <<SYS>>\n",
+    prefix="[INST] ",
     suffix="[/INST]",
 )
 
