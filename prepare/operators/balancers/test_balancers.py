b04af4c733fc5f1c8e5c92cb97f12a2b5d6491fa
elronbandel <elronbandel@gmail.com>
2023-10-02 04:22:28 -0400

    Changes to api

--- prepare/operators/balancers/test_balancers.py
+++ prepare/operators/balancers/test_balancers.py
@@ -3,12 +3,4 @@ from src.unitxt.operators import DeterministicBalancer
 
 balancer = DeterministicBalancer(fields=["target"])
 
-add_to_catalog(balancer, "operators.balancers.outputs_balancer", overwrite=True)
-
-balancer = DeterministicBalancer(fields=["target"], apply_to_streams=["test"])
-
-add_to_catalog(balancer, "operators.balancers.test_outputs_balancer", overwrite=True)
-
-balancer = DeterministicBalancer(fields=["target"], apply_to_streams=["validation", "test"])
-
-add_to_catalog(balancer, "operators.balancers.test_validation_outputs_balancer", overwrite=True)
+add_to_catalog(balancer, "operators.balancers.balanced_targets", overwrite=True)
