260c8f849c38bda48bb85cd2b2cfca1218c96e05
Naama Zwerdling <naamazwerdling@Naamas-MacBook-Pro.local>
2023-10-22 10:49:47 +0300

    ethos classification

--- prepare/processors/processors.py
+++ prepare/processors/processors.py
@@ -1,6 +1,16 @@
 from src.unitxt import add_to_catalog
-from src.unitxt.processors import TakeFirstNonEmptyLine
+from src.unitxt.processors import TakeFirstNonEmptyLine, LowerCaseTillPunc, HateOrNot
 
 operator = TakeFirstNonEmptyLine()
 
 add_to_catalog(operator, "processors.take_first_non_empty_line", overwrite=True)
+
+
+
+operator2 = LowerCaseTillPunc()
+
+add_to_catalog(operator2, "processors.lower_case_till_punc", overwrite=True)
+
+operator3 = HateOrNot()
+
+add_to_catalog(operator3, "processors.hate_or_not", overwrite=True)
