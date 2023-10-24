b8f17d07b65a130e876b4c504a12f7c5bcb9e3d5
Yoav Katz <katz@il.ibm.com>
2023-10-24 23:00:42 +0300

    Made stub test for COS loader
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -2,9 +2,36 @@ import os
 import tempfile
 import unittest
 from math import isnan
+from unittest.mock import patch
 
+import ibm_boto3
 import pandas as pd
 from src.unitxt.loaders import LoadCSV, LoadFromIBMCloud
+
+
+class DummyBody:
+    pass
+
+
+class DummyObject:
+    def get(self):
+        return {"ContentLength": 1, "Body": DummyBody}
+
+
+class DummyBucket:
+    def download_file(self, item_name, local_file, Callback):
+        with open(local_file, "w") as f:
+            print(local_file)
+            f.write("a,b\n")
+            f.write("1,2\n")
+
+
+class DummyS3:
+    def Object(self, bucket_name, item_name):
+        return DummyObject()
+
+    def Bucket(self, bucket_name):
+        return DummyBucket()
 
 
 class TestLoaders(unittest.TestCase):
@@ -38,5 +65,8 @@ class TestLoaders(unittest.TestCase):
             aws_secret_access_key_env="DUMMY_SECRET_ENV",
             bucket_name="DUMMY_BUCKET",
             data_dir="DUMMY_DATA_DIR",
-            data_files="DUMMY_DATA_FILES",
+            data_files=["train.csv", "test.csv"],
         )
+        with (patch.object(ibm_boto3, "resource", return_value=DummyS3())):
+            ms = loader()
+            self.assertEqual(ms.to_dataset()["test"][0], {"a": 1, "b": 2})
