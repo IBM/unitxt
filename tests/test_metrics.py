c7eb3957717173eaaa04d9ae9e500cc8e44dea24
Naama Zwerdling <naamazwerdling@Naamas-MacBook-Pro.local>
2023-11-02 13:27:21 +0200

    accuracy one of reference

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -23,7 +23,7 @@ class TestMetrics(unittest.TestCase):
         metric = Accuracy()
 
         predictions = ["A", "B", "C"]
-        references = [["B"], ["A"], ["C"]]
+        references = [["B", "C"], ["A"], ["C", "B"]]
 
         instance_targets = [
             {"accuracy": 0.0, "score": 0.0, "score_name": "accuracy"},
