ff00022a632b1a38145a5ff30331b37567f02c5a
Yoav Katz <katz@il.ibm.com>
2023-11-05 21:33:57 +0200

    Added caching on loader
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    Signed-off-by: yotamp <yotam.perlitz@ibm.com>

--- src/unitxt/standard.py
+++ src/unitxt/standard.py
@@ -81,6 +81,8 @@ class BaseRecipe(Recipe, SourceSequentialOperator):
                 )
 
     def prepare(self):
+
+        self.card.loader.caching = True
         self.steps = [
             self.card.loader,
         ]
