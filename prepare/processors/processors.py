479484983fa6919001688546954f28e3eb04a54f
Yoav Katz <katz@il.ibm.com>
2023-11-07 17:07:46 +0200

    Added new processor to extract single letter
    in multi choice question answering datasets such as mmlu and belebele

--- prepare/processors/processors.py
+++ prepare/processors/processors.py
@@ -1,5 +1,6 @@
 from src.unitxt import add_to_catalog
 from src.unitxt.processors import (
+    FirstCharacter,
     LowerCaseTillPunc,
     StringOrNotString,
     TakeFirstNonEmptyLine,
@@ -16,3 +17,12 @@ add_to_catalog(operator2, "processors.lower_case_till_punc", overwrite=True)
 operator3 = StringOrNotString(string="hate speech")
 
 add_to_catalog(operator3, "processors.hate_speech_or_not_hate_speech", overwrite=True)
+
+parser = FirstCharacter()
+
+example = " A. This is the answer."
+
+print(parser.process(example))
+assert parser.process(example) == "A"
+
+add_to_catalog(parser, "processors.first_character", overwrite=True)
