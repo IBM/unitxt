48b9192d9eaaec25cce590a5078496846acdc346
Matan Orbach <MATANO@il.ibm.com>
2023-11-09 17:47:03 +0200

    add a test on the examplar_outputs type add corresponding test
    Signed-off-by: Matan Orbach <MATANO@il.ibm.com>

--- tests/test_splitters.py
+++ tests/test_splitters.py
@@ -9,25 +9,36 @@ class TestDiverseLabelsSampler(unittest.TestCase):
     """
 
     @staticmethod
-    def get_correct_examplar():
+    def new_examplar(input_choices=["class_a", "class_b"], output_choices=["class_a"]):
         """
         return an examplar in a correct format.
         """
         return {
-            "inputs": {"choices": ["class_a", "class_b"]},
+            "inputs": {"choices": input_choices},
             "outputs": {
-                "choices": ["class_a"],
+                "choices": output_choices,
             },
         }
 
     def test_examplar_repr(self):
         sampler = DiverseLabelsSampler()
         expected_results = ["class_a"]
-        result = sampler.examplar_repr(examplar=self.get_correct_examplar())
+        result = sampler.examplar_repr(examplar=self.new_examplar())
         self.assertEqual(str(expected_results), result)
 
+    def test_examplar_repr_with_string_for_output_choices(self):
+        sampler = DiverseLabelsSampler()
+        examplar_output_choices = "a string which is a wrong value"
+        wrong_examplar = self.new_examplar(output_choices=examplar_output_choices)
+        with self.assertRaises(ValueError) as cm:
+            sampler.examplar_repr(examplar=wrong_examplar)
+        self.assertEquals(
+            f"Unexpected examplar_outputs value '{examplar_output_choices}'. Expected a list.",
+            str(cm.exception),
+        )
+
     def _test_examplar_repr_missing_field(self, missing_field):
-        examplar = self.get_correct_examplar()
+        examplar = self.new_examplar()
         del examplar[missing_field]
         with self.assertRaises(ValueError) as cm:
             sampler = DiverseLabelsSampler()
