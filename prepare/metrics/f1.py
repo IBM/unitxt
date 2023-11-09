06e7ed29af8bb388ae8efd53ae567d83ba4ca39e
Állysson Oliveira <allysson@br.ibm.com>
2023-11-09 16:26:27 -0300

    F1 weighted metric. Fixes #292
    Signed-off-by: Állysson Oliveira <allysson@br.ibm.com>

--- prepare/metrics/f1.py
+++ prepare/metrics/f1.py
@@ -5,6 +5,7 @@ from src.unitxt.metrics import (
     F1MacroMultiLabel,
     F1Micro,
     F1MicroMultiLabel,
+    F1Weighted,
 )
 
 metric = F1Macro()
@@ -18,3 +19,6 @@ add_to_catalog(metric, "metrics.f1_macro_multi_label", overwrite=True)
 
 metric = F1MicroMultiLabel()
 add_to_catalog(metric, "metrics.f1_micro_multi_label", overwrite=True)
+
+metric = F1Weighted()
+add_to_catalog(metric, "metrics.f1_weighted", overwrite=True)
