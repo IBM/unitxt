f0be26087fea9f8297bba4fc19cf8e39c8a746e8
dafnapension <dafnashein@yahoo.com>
2023-11-20 18:31:20 +0200

    english first
    Signed-off-by: dafnapension <dafnashein@yahoo.com>

--- prepare/cards/almostEvilML_qa_by_lang.py
+++ prepare/cards/almostEvilML_qa_by_lang.py
@@ -3,7 +3,7 @@ from src.unitxt.catalog import add_to_catalog
 from src.unitxt.operators import Apply, FilterByValues, RemoveFields
 from src.unitxt.test_utils.card import test_card
 
-langs = ["de", "it", "fr", "es", "ru", "nl", "en", "pt"]
+langs = ["en", "de", "it", "fr", "es", "ru", "nl", "pt"]
 # Counter({'de': 2302, 'it': 2210, 'fr': 2156, 'es': 2090, 'ru': 2058, 'nl': 2017, 'en': 1995, 'pt': 1994})
 
 
@@ -22,7 +22,7 @@ for lang in langs:
             Apply("METADATA", function=extract_lang_from_METADATA, to_field="extracted_language"),
             FilterByValues(required_values={"extracted_language": lang}),
             RemoveFields(fields=["extracted_language"]),
-            SplitRandomMix({"train": "train[80%]", "validation": "train[10%]", "test": "train[10%]"}),
+            SplitRandomMix({"train": "train[90%]", "validation": "train[5%]", "test": "train[5%]"}),
             RenameFields(field_to_field={"INSTRUCTION": "question", "RESPONSE": "answer"}),
         ],
         task=FormTask(
@@ -34,7 +34,5 @@ for lang in langs:
     )
 
     if lang == langs[0]:
-        test_card(card, debug=False, loader_limit=16000)
-        # because most of the records loaded by the loader are screened out by language,
-        # and langs are not randomly spread over the dataset
+        test_card(card, debug=False)
     add_to_catalog(card, f"cards.almostEvilML_qa_by_lang.{lang}", overwrite=True)
