9cea68e27229c810bf9775705f2885cbb0364897
“dafnapension” <dafnashein@yahoo.com>
2023-11-21 11:52:50 +0000

    fixed the use function in Apply per education from Yoav
    Signed-off-by: “dafnapension” <dafnashein@yahoo.com>

--- prepare/cards/almostEvilML_qa_by_lang.py
+++ prepare/cards/almostEvilML_qa_by_lang.py
@@ -1,27 +1,22 @@
+import json
+
 from src.unitxt.blocks import FormTask, LoadHF, RenameFields, SplitRandomMix, TaskCard
 from src.unitxt.catalog import add_to_catalog
-from src.unitxt.operators import Apply, FilterByValues, RemoveFields
+from src.unitxt.operators import Apply, CopyFields, FilterByValues, RemoveFields
 from src.unitxt.test_utils.card import test_card
 
 langs = ["en", "de", "it", "fr", "es", "ru", "nl", "pt"]
-# Counter({'de': 2302, 'it': 2210, 'fr': 2156, 'es': 2090, 'ru': 2058, 'nl': 2017, 'en': 1995, 'pt': 1994})
-
-
-def extract_lang_from_METADATA(md_as_string: str) -> str:
-    import json
-
-    md_as_json = json.loads(md_as_string)
-    return md_as_json["language"]
-
+# Counter({'en': 1995, 'de': 2302, 'it': 2210, 'fr': 2156, 'es': 2090, 'ru': 2058, 'nl': 2017, 'pt': 1994})
 
 for lang in langs:
 
     card = TaskCard(
         loader=LoadHF(path="0x22almostEvil/multilingual-wikihow-qa-16k"),
         preprocess_steps=[
-            Apply("METADATA", function=extract_lang_from_METADATA, to_field="extracted_language"),
+            Apply("METADATA", function=json.loads, to_field="metadata"),
+            CopyFields(field_to_field=[("metadata/language", "extracted_language")], use_query=True),
             FilterByValues(required_values={"extracted_language": lang}),
-            RemoveFields(fields=["extracted_language"]),
+            RemoveFields(fields=["extracted_language", "metadata"]),
             SplitRandomMix({"train": "train[90%]", "validation": "train[5%]", "test": "train[5%]"}),
             RenameFields(field_to_field={"INSTRUCTION": "question", "RESPONSE": "answer"}),
         ],
