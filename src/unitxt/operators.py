0b4d9c0bc94bda50e0663324e10dcb751f84889f
Yoav Katz <katz@il.ibm.com>
2023-11-22 11:03:03 +0200

    Fixed error message
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- src/unitxt/operators.py
+++ src/unitxt/operators.py
@@ -213,18 +213,14 @@ class FieldOperator(StreamInstanceOperator):
                     not_exist_ok=self.not_exist_ok,
                 )
             except Exception as e:
-                raise ValueError(
-                    f"{self.__class__.__name__}: Failed to get '{from_field}' from {instance} due to : {e}"
-                ) from e
+                raise ValueError(f"Failed to get '{from_field}' from {instance} due to : {e}") from e
             try:
                 if self.process_every_value:
                     new_value = [self.process_value(value) for value in old_value]
                 else:
                     new_value = self.process_value(old_value)
             except Exception as e:
-                raise ValueError(
-                    f"{self.__class__.__name__}: Failed to process '{from_field}' from {instance} due to : {e}"
-                ) from e
+                raise ValueError(f"Failed to process '{from_field}' from {instance} due to : {e}") from e
             if self.use_query and is_subpath(from_field, to_field):
                 dict_delete(instance, from_field)
             dict_set(instance, to_field, new_value, use_dpath=self.use_query, not_exist_ok=True)
