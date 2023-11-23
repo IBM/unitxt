682bd3bb2cc79c827cac870011a151fcffd70a8b
Yoav Katz <katz@il.ibm.com>
2023-11-23 21:57:02 +0200

    Changed sst2 debug back to true
    To ensure coverage of this path
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/cards/sst2.py
+++ prepare/cards/sst2.py
@@ -18,5 +18,5 @@ card = TaskCard(
     templates="templates.classification.multi_class.all",
 )
 
-test_card(card, debug=False)
+test_card(card, debug=True)
 add_to_catalog(card, "cards.sst2", overwrite=True)
