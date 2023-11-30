ae87a6f5d13c551ec213a4f23cfb67a6242e2005
Elron Bandel <elron.bandel@ibm.com>
2023-11-30 03:38:26 -0500

    remove output format as it is not necassery

--- prepare/templates/qa/contextual.py
+++ prepare/templates/qa/contextual.py
@@ -2,9 +2,7 @@ from src.unitxt.catalog import add_to_catalog
 from src.unitxt.templates import MultiReferenceTemplate, TemplatesList
 
 add_to_catalog(
-    MultiReferenceTemplate(
-        input_format="Context: {context}\nQuestion: {question}", output_format="{answers}", references_field="answers"
-    ),
+    MultiReferenceTemplate(input_format="Context: {context}\nQuestion: {question}", references_field="answers"),
     "templates.qa.contextual.simple",
     overwrite=True,
 )
@@ -12,7 +10,6 @@ add_to_catalog(
 add_to_catalog(
     MultiReferenceTemplate(
         input_format="based on this text: {context}\n answer the question: {question}",
-        output_format="{answers}",
         references_field="answers",
     ),
     "templates.qa.contextual.simple2",
--- prepare/templates/qa/open.py
+++ prepare/templates/qa/open.py
@@ -2,15 +2,13 @@ from src.unitxt.catalog import add_to_catalog
 from src.unitxt.templates import MultiReferenceTemplate, TemplatesList
 
 add_to_catalog(
-    MultiReferenceTemplate(input_format="Question: {question}", output_format="{answers}", references_field="answers"),
+    MultiReferenceTemplate(input_format="Question: {question}", references_field="answers"),
     "templates.qa.open.simple",
     overwrite=True,
 )
 
 add_to_catalog(
-    MultiReferenceTemplate(
-        input_format="answer the question: {question}", output_format="{answers}", references_field="answers"
-    ),
+    MultiReferenceTemplate(input_format="answer the question: {question}", references_field="answers"),
     "templates.qa.open.simple2",
     overwrite=True,
 )
