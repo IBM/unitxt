ad816dc0e4190d0ba0e294162484c63787b0354f
Elron Bandel <elron.bandel@ibm.com>
2023-12-06 05:47:16 -0500

    Delete faulty enivroment control tools

--- tests/test_hf_cache.py
+++ tests/test_hf_cache.py
@@ -2,42 +2,39 @@ import tempfile
 import unittest
 
 from src.unitxt.stream import MultiStream, Stream
-from src.unitxt.test_utils.environment import modified_environment
 from src.unitxt.test_utils.storage import get_directory_size
 
 
 class TestHfCache(unittest.TestCase):
     def test_caching_stream(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
-            with modified_environment(HF_DATASETS_CACHE=tmp_dir):
-                self.assertEqual(get_directory_size(tmp_dir), 0)
+            self.assertEqual(get_directory_size(tmp_dir), 0)
 
-                def gen():
-                    for i in range(100000):  # must be big or else hf wont cache
-                        yield {"x": i}
+            def gen():
+                for i in range(100000):  # must be big or else hf wont cache
+                    yield {"x": i}
 
-                ds = MultiStream({"test": Stream(generator=gen)}).to_dataset(
-                    disable_cache=False, cache_dir=tmp_dir
-                )
-                for i, item in enumerate(ds["test"]):
-                    self.assertEqual(item["x"], i)
-                self.assertNotEqual(get_directory_size(tmp_dir), 0)
+            ds = MultiStream({"test": Stream(generator=gen)}).to_dataset(
+                disable_cache=False, cache_dir=tmp_dir
+            )
+            for i, item in enumerate(ds["test"]):
+                self.assertEqual(item["x"], i)
+            self.assertNotEqual(get_directory_size(tmp_dir), 0)
 
     def test_not_caching_stream(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
-            with modified_environment(HF_DATASETS_CACHE=tmp_dir):
-                self.assertEqual(get_directory_size(tmp_dir), 0)
+            self.assertEqual(get_directory_size(tmp_dir), 0)
 
-                def gen():
-                    for i in range(100000):  # must be big or else hf wont cache
-                        yield {"x": i}
+            def gen():
+                for i in range(100000):  # must be big or else hf wont cache
+                    yield {"x": i}
 
-                ds = MultiStream({"test": Stream(generator=gen)}).to_dataset(
-                    cache_dir=tmp_dir
-                )
-                for i, item in enumerate(ds["test"]):
-                    self.assertEqual(item["x"], i)
-                self.assertEqual(get_directory_size(tmp_dir), 0)
+            ds = MultiStream({"test": Stream(generator=gen)}).to_dataset(
+                cache_dir=tmp_dir
+            )
+            for i, item in enumerate(ds["test"]):
+                self.assertEqual(item["x"], i)
+            self.assertEqual(get_directory_size(tmp_dir), 0)
 
 
 if __name__ == "__main__":
