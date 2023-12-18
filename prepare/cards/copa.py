9f9ffe34dd8934060ffc828c10ea0d124fc349b4
elronbandel <elronbandel@gmail.com>
2023-12-18 10:51:05 -0500

    Update all multiple choice datasets
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- prepare/cards/copa.py
+++ prepare/cards/copa.py
@@ -1,44 +1,26 @@
-from prepare.cards.mmlu_old import (
-    multiple_choice_inputs_outputs,
-    multiple_choice_preprocess,
-)
-from src.unitxt.blocks import InputOutputTemplate, LoadHF, TemplatesList
+from src.unitxt.blocks import LoadHF
 from src.unitxt.card import TaskCard
 from src.unitxt.catalog import add_to_catalog
-from src.unitxt.operators import AddFields, ListFieldValues
-from src.unitxt.task import FormTask
+from src.unitxt.operators import ListFieldValues, MapInstanceValues, RenameFields
 from src.unitxt.test_utils.card import test_card
 
-numbering = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 card = TaskCard(
     loader=LoadHF(path="super_glue", name="copa"),
     preprocess_steps=[
         "splitters.small_no_test",
-        AddFields({"numbering": numbering, "topic": "commonsense causal reasoning"}),
         ListFieldValues(fields=["choice1", "choice2"], to_field="choices"),
-        *multiple_choice_preprocess(
-            context="premise",
-            question="question",
-            numbering="numbering",
-            choices="choices",
-            topic="topic",
-            label_index="label",
+        RenameFields(field_to_field={"premise": "context", "label": "answer"}),
+        MapInstanceValues(
+            mappers={
+                "question": {  # https://people.ict.usc.edu/~gordon/copa.html
+                    "cause": "What was the cause of this?",
+                    "effect": "What happened as a result?",
+                }
+            }
         ),
     ],
-    task=FormTask(
-        **multiple_choice_inputs_outputs(context=True),
-        metrics=["metrics.accuracy"],
-    ),
-    templates=TemplatesList(
-        [
-            InputOutputTemplate(
-                input_format="""
-                    What was the {sentence1} of the following:\n{context}\nAnswers: {choices}\nAnswer:
-                """.strip(),
-                output_format="{label}",
-            ),
-        ]
-    ),
+    task="tasks.qa.multiple_choice.contextual",
+    templates="templates.qa.multiple_choice.contextual.all",
 )
 
 test_card(card)
