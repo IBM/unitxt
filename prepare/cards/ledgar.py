a2e7978087d7f4dc5517f1bede6a431c82c4fed8
ilyashn <ilyashn@il.ibm.com>
2023-12-18 08:46:26 -0500

    adding ledgar
    Signed-off-by: ilyashn <ilyashn@il.ibm.com>

---
+++ prepare/cards/ledgar.py
@@ -0,0 +1,39 @@
+from datasets import load_dataset_builder
+
+from src.unitxt import add_to_catalog
+from src.unitxt.blocks import (
+    AddFields,
+    LoadHF,
+    MapInstanceValues,
+    TaskCard,
+)
+from src.unitxt.test_utils.card import test_card
+
+dataset_name = "ledgar"
+
+ds_builder = load_dataset_builder(
+    "lex_glue", dataset_name
+)  # TODO should we take all from lex_glue?
+classlabels = ds_builder.info.features["label"]
+
+mappers = {}
+for i in range(len(classlabels.names)):
+    mappers[str(i)] = classlabels.names[i]
+
+card = TaskCard(
+    loader=LoadHF(path="lex_glue", name=f"{dataset_name}"),
+    preprocess_steps=[
+        MapInstanceValues({"label": mappers}),
+        AddFields(
+            fields={
+                "classes": classlabels.names,
+                "text_type": "text",
+                "type_of_class": "contractual clauses",
+            }
+        ),
+    ],
+    task="tasks.classification.multi_class",
+    templates="templates.classification.multi_class.all",
+)
+test_card(card, debug=False)
+add_to_catalog(card, f"cards.{dataset_name}", overwrite=True)
