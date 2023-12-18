0d274ba33be2d2aa2cac8970419cf8086fe67bd6
ilyashn <ilyashn@il.ibm.com>
2023-12-18 11:38:30 -0500

    cosmetics
    Signed-off-by: ilyashn <ilyashn@il.ibm.com>

--- prepare/cards/clinc_oos.py
+++ prepare/cards/clinc_oos.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset_builder
+from datasets import get_dataset_config_names, load_dataset_builder
 
 from src.unitxt import add_to_catalog
 from src.unitxt.blocks import (
@@ -10,31 +10,31 @@ from src.unitxt.blocks import (
 )
 from src.unitxt.test_utils.card import test_card
 
-dataset_name = "clinc150"
-subset = "plus"  # TODO add imbalanced, small
-ds_builder = load_dataset_builder("clinc_oos", subset)
-classlabels = ds_builder.info.features["intent"]
+dataset_name = "clinc_oos"
 
-mappers = {}
-for i in range(len(classlabels.names)):
-    mappers[str(i)] = classlabels.names[i]
+for subset in get_dataset_config_names(dataset_name):
+    ds_builder = load_dataset_builder(dataset_name, subset)
+    classlabels = ds_builder.info.features["intent"]
 
+    mappers = {}
+    for i in range(len(classlabels.names)):
+        mappers[str(i)] = classlabels.names[i]
 
-card = TaskCard(
-    loader=LoadHF(path="clinc_oos", name=subset),
-    preprocess_steps=[
-        RenameFields(field_to_field={"intent": "label"}),
-        MapInstanceValues(mappers={"label": mappers}),
-        AddFields(
-            fields={
-                "classes": mappers,
-                "text_type": "sentence",
-                "type_of_class": "intent",
-            }
-        ),
-    ],
-    task="tasks.classification.multi_class",
-    templates="templates.classification.multi_class.all",
-)
-test_card(card, debug=False)
-add_to_catalog(artifact=card, name=f"cards.{dataset_name}.{subset}", overwrite=True)
+    card = TaskCard(
+        loader=LoadHF(path=dataset_name, name=subset),
+        preprocess_steps=[
+            RenameFields(field_to_field={"intent": "label"}),
+            MapInstanceValues(mappers={"label": mappers}),
+            AddFields(
+                fields={
+                    "classes": classlabels.names,
+                    "text_type": "sentence",
+                    "type_of_class": "intent",
+                }
+            ),
+        ],
+        task="tasks.classification.multi_class",
+        templates="templates.classification.multi_class.all",
+    )
+    test_card(card, debug=False)
+    add_to_catalog(artifact=card, name=f"cards.{dataset_name}.{subset}", overwrite=True)
