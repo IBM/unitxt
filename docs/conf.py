94d4e0fcbec441a898f53665a01817cb6ec791ad
elronbandel <elronbandel@gmail.com>
2023-12-20 08:13:08 -0500

    Elimnate warning
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = []
 html_theme_options = {
     "logo_only": True,
     "display_version": False,
