d92ef271c1bfb4acd8649b51fdcffae927b1a389
Assaf Toledo <assaf.toledo@ibm.com>
2023-12-21 23:24:05 +0200

    adding retrieval metrics
    Signed-off-by: Assaf Toledo <assaf.toledo@ibm.com>

---
+++ prepare/metrics/map.py
@@ -0,0 +1,45 @@
+from src.unitxt import add_to_catalog
+from src.unitxt.metrics import MAP
+from src.unitxt.test_utils.metrics import test_metric
+
+metric = MAP()
+
+predictions = [["a", "b", "c", "d", "e", "f"],
+               ["g", "r", "u"],
+               ["a", "b"],
+               [],
+               ["c"]]
+
+references = [["c", "d"],  # third hit
+              ["g"],  # first hit
+              [],  # no hit
+              ["a"],  # no hit
+              ["b"]]  # no hit
+
+instance_targets = [
+    {"map": 0.42, "score": 0.42, "score_name": "map"},
+    {"map": 1.0, "score": 1.0, "score_name": "map"},
+    {"map": 0, "score": 0, "score_name": "map"},
+    {"map": 0, "score": 0, "score_name": "map"},
+    {"map": 0, "score": 0, "score_name": "map"}
+]
+
+global_target = {
+    "map": 0.28,
+    "score": 0.28,
+    "score_name": "map",
+    "map_ci_low": 0.0,
+    "map_ci_high": 0.8,
+    "score_ci_low": 0.0,
+    "score_ci_high": 0.8,
+}
+
+outputs = test_metric(
+    metric=metric,
+    predictions=predictions,
+    references=references,
+    instance_targets=instance_targets,
+    global_target=global_target,
+)
+
+add_to_catalog(metric, "metrics.map", overwrite=True)
---
+++ prepare/metrics/retrieval.py
@@ -0,0 +1,55 @@
+from src.unitxt import add_to_catalog
+from src.unitxt.metrics import MAP, RetrievalAtK
+from src.unitxt.test_utils.metrics import test_metric
+
+metric = RetrievalAtK(k_list=[1, 3])
+
+predictions = [["a", "b", "c", "d", "e", "f"],
+               ["g", "r", "u"],
+               ["a", "b", "c"]]
+
+references = [["c", "d"],  # third hit
+              ["g"],  # first hit
+              []]  # no hit
+
+instance_targets = [
+    {"match_at_1": 0, "match_at_3": 1,
+     "precision_at_1": 0.0, "precision_at_3": 0.33,
+     "recall_at_1": 0.0, "recall_at_3": 0.5,
+     "score": 0, "score_name": "match_at_1"},
+
+    {"match_at_1": 1, "match_at_3": 1,
+     "precision_at_1": 1.0, "precision_at_3": 0.33,
+     "recall_at_1": 1.0, "recall_at_3": 1.0,
+     "score": 1, "score_name": "match_at_1"},
+
+    {"match_at_1": 0, "match_at_3": 0,
+     "precision_at_1": 0.0, "precision_at_3": 0.0,
+     "recall_at_1": 0, "recall_at_3": 0,
+     "score": 0, "score_name": "match_at_1"}
+]
+
+global_target = {
+    "score_ci_high": 1.0,
+    "score_ci_low": 0.0,
+    "score": 0.33,
+    "score_name": "match_at_1",
+    "match_at_1_ci_high": 1.0,
+    "match_at_1_ci_low": 0.0,
+    "match_at_1": 0.33,
+    "match_at_3": 0.67,
+    "precision_at_1": 0.33,
+    "precision_at_3": 0.22,
+    "recall_at_1": 0.33,
+    "recall_at_3": 0.5,
+}
+
+outputs = test_metric(
+    metric=metric,
+    predictions=predictions,
+    references=references,
+    instance_targets=instance_targets,
+    global_target=global_target,
+)
+
+add_to_catalog(metric, "metrics.retrieval_at_k", overwrite=True)
