8652f00e6051786fd7e4ed7e4131e9bf8427e83c
lilacheden <lilach.edel@gmail.com>
2023-12-25 16:52:00 +0200

    Use F1MultiLabel for binary precision and recall
    Signed-off-by: lilacheden <lilach.edel@gmail.com>

--- prepare/metrics/binary_metrics.py
+++ prepare/metrics/binary_metrics.py
@@ -1,28 +1,31 @@
+from numpy import NaN
+
 from src.unitxt import add_to_catalog
-from src.unitxt.metrics import BinaryF1, BinaryPrecision, BinaryRecall
+from src.unitxt.metrics import PrecisionMicroMultiLabel, RecallMicroMultiLabel
 from src.unitxt.test_utils.metrics import test_metric
 
-predictions = ["1", "1", "0", "0"]
-references = [["1"], ["0"], ["1"], ["1"]]
+#
+predictions = [["1"], ["1"], [], []]
+references = [[["1"]], [[]], [["1"]], [["1"]]]
 
-precision_metric = BinaryPrecision()
+precision_metric = PrecisionMicroMultiLabel()
 
 
 instance_targets_precision = [
-    {"binary_precision": 1.0, "score": 1.0, "score_name": "binary_precision"},
-    {"binary_precision": 0.0, "score": 0.0, "score_name": "binary_precision"},
-    {"binary_precision": 0.0, "score": 0.0, "score_name": "binary_precision"},
-    {"binary_precision": 0.0, "score": 0.0, "score_name": "binary_precision"},
+    {"precision": 1.0, "score": 1.0, "score_name": "precision"},
+    {"precision": NaN, "score": NaN, "score_name": "precision"},
+    {"precision": 0.0, "score": 0.0, "score_name": "precision"},
+    {"precision": 0.0, "score": 0.0, "score_name": "precision"},
 ]
 
 global_target_precision = {
-    "binary_precision": 0.5,
+    "precision": 0.5,
     "score": 0.5,
-    "score_name": "binary_precision",
+    "score_name": "precision",
     "score_ci_low": 0.5,
     "score_ci_high": 0.86,
-    "binary_precision_ci_low": 0.5,
-    "binary_precision_ci_high": 0.86,
+    "precision_ci_low": 0.5,
+    "precision_ci_high": 0.86,
 }
 
 outputs = test_metric(
@@ -32,26 +35,26 @@ outputs = test_metric(
     instance_targets=instance_targets_precision,
     global_target=global_target_precision,
 )
-add_to_catalog(precision_metric, "metrics.binary_precision", overwrite=True)
+add_to_catalog(precision_metric, "metrics.precision", overwrite=True)
 
 
-recall_metric = BinaryRecall()
+recall_metric = RecallMicroMultiLabel()
 
 global_target_recall = {
-    "binary_recall": 0.33,
+    "recall": 0.33,
     "score": 0.33,
-    "score_name": "binary_recall",
+    "score_name": "recall",
     "score_ci_low": 0.33,
     "score_ci_high": 0.47,
-    "binary_recall_ci_low": 0.33,
-    "binary_recall_ci_high": 0.47,
+    "recall_ci_low": 0.33,
+    "recall_ci_high": 0.47,
 }
 
 instance_targets_recall = [
-    {"binary_recall": 1.0, "score": 1.0, "score_name": "binary_recall"},
-    {"binary_recall": 0.0, "score": 0.0, "score_name": "binary_recall"},
-    {"binary_recall": 0.0, "score": 0.0, "score_name": "binary_recall"},
-    {"binary_recall": 0.0, "score": 0.0, "score_name": "binary_recall"},
+    {"recall": 1.0, "score": 1.0, "score_name": "recall"},
+    {"recall": NaN, "score": NaN, "score_name": "recall"},
+    {"recall": 0.0, "score": 0.0, "score_name": "recall"},
+    {"recall": 0.0, "score": 0.0, "score_name": "recall"},
 ]
 outputs = test_metric(
     metric=recall_metric,
@@ -60,34 +63,4 @@ outputs = test_metric(
     instance_targets=instance_targets_recall,
     global_target=global_target_recall,
 )
-add_to_catalog(recall_metric, "metrics.binary_recall", overwrite=True)
-
-
-f1_metric = BinaryF1()
-
-global_target_recall = {
-    "binary_f1": 0.4,
-    "score": 0.4,
-    "score_name": "binary_f1",
-    "score_ci_low": 0.4,
-    "score_ci_high": 0.62,
-    "binary_f1_ci_low": 0.4,
-    "binary_f1_ci_high": 0.62,
-}
-
-instance_targets_recall = [
-    {"binary_f1": 1.0, "score": 1.0, "score_name": "binary_f1"},
-    {"binary_f1": 0.0, "score": 0.0, "score_name": "binary_f1"},
-    {"binary_f1": 0.0, "score": 0.0, "score_name": "binary_f1"},
-    {"binary_f1": 0.0, "score": 0.0, "score_name": "binary_f1"},
-]
-
-outputs = test_metric(
-    metric=f1_metric,
-    predictions=predictions,
-    references=references,
-    instance_targets=instance_targets_recall,
-    global_target=global_target_recall,
-)
-
-add_to_catalog(f1_metric, "metrics.binary_f1", overwrite=True)
+add_to_catalog(recall_metric, "metrics.recall", overwrite=True)
