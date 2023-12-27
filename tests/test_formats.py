67f8c526661840e839762cfbab06f72eafe738eb
dafnapension <dafnashein@yahoo.com>
2023-12-27 14:42:34 +0200

    added tests to verify compatibility of ModelInputFormatter with ICLFormat
    Signed-off-by: dafnapension <dafnashein@yahoo.com>

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1,6 +1,7 @@
 import unittest
 
 from src.unitxt.formats import ICLFormat
+from src.unitxt.operators import ModelInputFormatter
 
 
 class TestFormats(unittest.TestCase):
@@ -34,6 +35,18 @@ User:1+1
 Agent:"""
         self.assertEqual(result, target)
 
+        # compare here with ModelInputFormatter
+        model_input_formatter = ModelInputFormatter(
+            demo_format="User:{source}\nAgent: {target}\n\n",
+            model_input_format="Instruction:{instruction}{demos}User:{source}\nAgent:",
+        )
+        # refresh instance, from which icl_format popped the instruction, and add demos into it:
+        instance["instruction"] = "solve the math exercises"
+        instance["demos"] = demos_instances
+
+        instance_out = model_input_formatter.process(instance)
+        self.assertEqual(instance_out["source"], target)
+
     def test_icl_format_with_demonstrations_and_instruction_after_demos(self):
         iclformat = ICLFormat(
             input_prefix="User:",
@@ -66,6 +79,18 @@ User:solve the math exercises
 Agent:"""
         self.assertEqual(result, target)
 
+        # compare here with ModelInputFormatter
+        model_input_formatter = ModelInputFormatter(
+            demo_format="User:{source}\nAgent: {target}\n\n",
+            model_input_format="{demos}User:{instruction}{source}\nAgent:",
+        )
+        # refresh instance, from which icl_format popped the instruction, and add demos into it:
+        instance["instruction"] = "solve the math exercises"
+        instance["demos"] = demos_instances
+
+        instance_out = model_input_formatter.process(instance)
+        self.assertEqual(instance_out["source"], target)
+
     def test_icl_format_without_demonstrations(self):
         format = ICLFormat(
             input_prefix="User:",
@@ -76,15 +101,26 @@ Agent:"""
         instance = {
             "source": "1+1",
             "target": "2",
-            "instruction": "solve the math excercises",
+            "instruction": "solve the math exercises",
         }
 
         result = format.format(instance)
-        target = """Instruction:solve the math excercises
+        target = """Instruction:solve the math exercises
 
 User:1+1
 Agent:"""
         self.assertEqual(result, target)
+
+        # compare here with ModelInputFormatter
+        model_input_formatter = ModelInputFormatter(
+            demo_format="User:{source}\nAgent: {target}\n\n",
+            model_input_format="Instruction:{instruction}{demos}User:{source}\nAgent:",
+        )
+        # refresh instance, from which icl_format popped the instruction
+        instance["instruction"] = "solve the math exercises"
+
+        instance_out = model_input_formatter.process(instance)
+        self.assertEqual(instance_out["source"], target)
 
     def test_icl_format_without_demonstrations_or_instruction(self):
         format = ICLFormat(
@@ -99,6 +135,15 @@ Agent:"""
         target = """User:1+1
 Agent:"""
         self.assertEqual(result, target)
+
+        # compare here with ModelInputFormatter
+        model_input_formatter = ModelInputFormatter(
+            demo_format="User:{source}\nAgent: {target}\n\n",
+            model_input_format="{instruction}{demos}User:{source}\nAgent:",
+        )
+        # no need to refresh instance, no instructions and no demo.
+        instance_out = model_input_formatter.process(instance)
+        self.assertEqual(instance_out["source"], target)
 
     def test_icl_format_without_demonstrations_and_empty_instruction(self):
         format = ICLFormat(
