ed54413621138babfb4053f820b85231119997da
Matan Orbach <MATANO@il.ibm.com>
2024-01-10 18:33:31 +0200

    rearrange rst generation tool

--- docs/catalog.py
+++ docs/catalog.py
@@ -24,10 +24,10 @@ def write_title(title, label):
 def custom_walk(top):
     for entry in os.scandir(top):
         if entry.is_dir():
-            yield (entry.path, True)
+            yield entry
             yield from custom_walk(entry.path)
         else:
-            yield (entry.path, False)
+            yield entry
 
 
 def make_content(artifact, label, all_labels):
@@ -55,66 +55,108 @@ def make_content(artifact, label, all_labels):
     return result
 
 
-def build_catalog_rst():
-    all_labels = set()
-    current_dir = os.path.dirname(__file__)
-    start_directory = os.path.join(current_dir, "..", "src", "unitxt", "catalog")
+class CatalogEntry:
+    def __init__(self, path: str, is_dir: bool, start_directory: str):
+        self.path = path
+        self.is_dir = is_dir
+        self.rel_path = path.replace(start_directory + "/", "")
 
-    for path, is_dir in custom_walk(start_directory):
-        rel_path = path.replace(start_directory + "/", "")
-        if not is_dir and ".json" in rel_path:
-            rel_path = rel_path.replace(".json", "")
-            label = rel_path.replace("/", ".")
-            all_labels.add(label)
+    @staticmethod
+    def from_dir_entry(dir_entry: os.DirEntry, start_directory):
+        return CatalogEntry(
+            path=dir_entry.path,
+            is_dir=dir_entry.is_dir(),
+            start_directory=start_directory,
+        )
 
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    for path, is_dir in custom_walk(start_directory):
-        rel_path = path.replace(start_directory + "/", "")
-        if is_dir:
-            create_dir_contents_rst(start_directory, current_directory, path, rel_path)
-        else:
-            if ".json" in rel_path:
-                artifact = load_json(path)
-                label = rel_path.replace("/", ".")
+    def is_json(self):
+        return not self.is_dir and ".json" in self.rel_path
+
+    def get_label(self):
+        label = self.rel_path.replace(".json", "")
+        return label.replace("/", ".")
+
+    def get_title(self):
+        return self.rel_path.split("/")[-1]
+
+    def get_artifact_doc_path(self, destination_directory):
+        return os.path.join(
+            destination_directory,
+            os.path.dirname(self.rel_path),
+            Path(self.rel_path).stem + ".rst",
+        )
+
+    def write_dir_contents_to_rst(self, destination_directory, start_directory):
+        title = self.get_title()
+        if title:
+            title = f"{title[0].upper()}{title[1:]}"
+        label = self.get_label()
+        dir_doc_content = write_title(title, label)
+
+        for sub_dir_entry in os.scandir(self.path):
+            sub_catalog_entry = CatalogEntry.from_dir_entry(
+                dir_entry=sub_dir_entry, start_directory=start_directory
+            )
+            sub_label = sub_catalog_entry.get_label()
+            sub_name = os.path.basename(sub_dir_entry.path)
+            dir_doc_content += f":ref:`{sub_name} <{sub_label}>`\n\n"
+
+        dir_doc_path = os.path.join(
+            destination_directory,
+            self.rel_path + ".rst",
+        )
+        with open(dir_doc_path, "w+") as f:
+            f.write(dir_doc_content)
+
+
+class CatalogDocsBuilder:
+    def __init__(self):
+        current_dir = os.path.dirname(__file__)
+        self.start_directory = os.path.join(
+            current_dir, "..", "src", "unitxt", "catalog"
+        )
+
+    def create_catalog_entries(self):
+        return [
+            CatalogEntry.from_dir_entry(dir_entry, self.start_directory)
+            for dir_entry in custom_walk(self.start_directory)
+        ]
+
+    def run(self):
+        catalog_entries = self.create_catalog_entries()
+        all_labels = {
+            catalog_entry.get_label()
+            for catalog_entry in catalog_entries
+            if catalog_entry.is_json()
+        }
+
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        for catalog_entry in catalog_entries:
+            if catalog_entry.is_dir:
+                catalog_entry.write_dir_contents_to_rst(
+                    destination_directory=current_directory,
+                    start_directory=self.start_directory,
+                )
+            elif catalog_entry.is_json():
+                artifact = load_json(catalog_entry.path)
+                label = catalog_entry.get_label()
                 content = make_content(artifact, label, all_labels)
-                section = write_section(rel_path.split("/")[-1], content, label)
-
-                artifact_doc_path = os.path.join(
-                    current_directory,
-                    os.path.dirname(rel_path),
-                    Path(rel_path).stem + ".rst",
+                section = write_section(catalog_entry.get_title(), content, label)
+                artifact_doc_path = catalog_entry.get_artifact_doc_path(
+                    destination_directory=current_directory
                 )
                 Path(artifact_doc_path).parent.mkdir(parents=True, exist_ok=True)
                 with open(artifact_doc_path, "w+") as f:
                     f.write(section)
 
-    create_dir_contents_rst(
-        start_directory,
-        current_directory,
-        dir_path=start_directory,
-        rel_path="catalog",
-    )
-
-
-def create_dir_contents_rst(start_directory, current_directory, dir_path, rel_path):
-    dir_doc_path = os.path.join(
-        current_directory,
-        rel_path + ".rst",
-    )
-    title = rel_path.split("/")[-1]
-    if title:
-        title = f"{title[0].upper()}{title[1:]}"
-    label = rel_path.replace("/", ".")
-    dir_doc_content = write_title(title, label)
-
-    for dir_entry in os.scandir(dir_path):
-        sub_rel_path = dir_entry.path.replace(start_directory + "/", "")
-        sub_label = sub_rel_path.replace("/", ".")
-        sub_name = os.path.basename(dir_entry.path)
-        dir_doc_content += f":ref:`{sub_name} <{sub_label}>`\n\n"
-    with open(dir_doc_path, "w+") as f:
-        f.write(dir_doc_content)
+        catalog_main_entry = CatalogEntry(
+            path=self.start_directory, is_dir=True, start_directory=self.start_directory
+        )
+        catalog_main_entry.write_dir_contents_to_rst(
+            destination_directory=current_directory,
+            start_directory=self.start_directory,
+        )
 
 
 if __name__ == "__main__":
-    build_catalog_rst()
+    CatalogDocsBuilder().run()
