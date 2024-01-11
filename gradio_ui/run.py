7097f70b1f4ab7720a1df0adde0b65ea51e612e2
Roni Friedman-Melamed <Roni.friedman-melamed@il.ibm.com>
2024-01-11 11:19:18 +0200

    add target and metrics

--- gradio_ui/run.py
+++ gradio_ui/run.py
@@ -13,28 +13,34 @@ augmentors = gr.Dropdown(choices=get_catalog_items('augmentors'),label='Augmento
 
 parameters = [tasks,cards,templates,instructions, formats, num_shots, augmentors]
 
+
+#TODO - avoid error in dataset when task is cleared
 #TODO - allow changing to None in each dropdown
 #TODO - move between samples
-#TODO - fix display of text 
-#TODO - add output, score etc.
-#TODO - cache dataset instead of loading from HF
+#TODO - allow choosing augmentor only when possible
+#TODO - add model, output, score etc.
+# if no train, choose test (add to ui?)
 # augmentor as additional option
 
 def safe_add(parameter,key, args):
     if isinstance(parameter,str):
         args[key] = parameter
 
+
 def run_unitxt(task,dataset,template,instruction,format,num_demos,augmentor):
     if not isinstance(dataset,str) or not isinstance(template,str):
-        return '',''
-    prompt_args = {'card':dataset, 'template': template, 'num_demos':num_demos}
+        return '','','',''
+    prompt_args = {'card':dataset, 'template': template}
+    if num_demos!=0:
+        prompt_args.update({'num_demos':num_demos, 'demos_pool_size':100})
     safe_add(instruction,'instruction',prompt_args)
     safe_add(format,'format',prompt_args)
     safe_add(augmentor,'augmentor',prompt_args)
 
     prompt = build_prompt(prompt_args)
     command = build_command(prompt_args)
-    return prompt,command
+    print(prompt)
+    return prompt['source'],prompt['metrics'],prompt['target'],command
 
 
 def build_prompt(prompt_args): 
@@ -48,11 +54,13 @@ def build_command(prompt_data):
     parameters_str = ",".join(parameters_str)
         
     command = f"""
-    from datasets import load_dataset </br>
+    from datasets import load_dataset 
 
-    dataset = load_dataset('unitxt/data', {parameters_str})
+    dataset = load_dataset(
+        'unitxt/data', {parameters_str}
+    )
     """
-    return command,command
+    return command
 
 def get_datasets(task_choice):
     return gr.update(choices=data[task_choice].keys())
@@ -73,8 +81,10 @@ with demo:
     prompt = gr.Interface(
                 fn=run_unitxt,
                 inputs=parameters,
-                outputs=[gr.Textbox(lines=10,show_copy_button=True,label='Prompt'),
-                         gr.Textbox(lines=5,show_copy_button=True,label='Code')],
+                outputs=[gr.Textbox(lines=5,show_copy_button=True,label='Prompt'),
+                         gr.Textbox(lines=1,show_copy_button=True,label='Metrics'),
+                         gr.Textbox(lines=1,show_copy_button=True,label='Target'),
+                         gr.Textbox(lines=3,show_copy_button=True,label='Code')],
                 allow_flagging=False,
                 live=True,
                 title="Unitxt Demo"
