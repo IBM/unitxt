7dc015f61a72faa4ca1abb6cfce08cd68dacedac
Roni Friedman-Melamed <Roni.friedman-melamed@il.ibm.com>
2024-01-11 12:25:50 +0200

    provide augmentors only when allowed

--- gradio_ui/constants.py
+++ gradio_ui/constants.py
@@ -1,4 +1,5 @@
 import os
 CATALOG_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'src',
                                'unitxt','catalog')
-    
+
+AUGMENTABLE = 'augmentable_inputs'
