5f3a69244c95aba7425bbe128aee580ea1fe274f
Elron Bandel <elron.bandel@ibm.com>
2024-01-14 08:31:41 -0500

    refactor
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- gradio_ui/constants.py
+++ gradio_ui/constants.py
@@ -1,6 +1,16 @@
 import os
-CATALOG_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'src',
-                               'unitxt','catalog')
 
-AUGMENTABLE = 'augmentable_inputs'
-LOADER_LIMIT = 'loader_limit'
+CATALOG_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    "src",
+    "unitxt",
+    "catalog",
+)
+
+AUGMENTABLE_STR = "augmentable_inputs"
+LOADER_LIMIT_STR = "loader_limit"
+PROMPT_SOURCE_STR = "source"
+PROMPT_METRICS_STR = "metrics"
+PROPT_TARGET_STR = "target"
+DEMOS_POOL_SIZE = 100
+PROMPT_SAMPLE_SIZE = 10
--- gradio_ui/run.py
+++ gradio_ui/run.py
@@ -1,117 +1,153 @@
-import gradio as gr
-from gradio_ui.load_catalog_data import load_cards_data, get_catalog_items
-from unitxt.standard import StandardRecipe
 import constants as cons
+import gradio as gr
+from unitxt.standard import StandardRecipe
 
-data = load_cards_data()
-tasks = gr.Dropdown(choices=data.keys(), label="Task")
-cards = gr.Dropdown(choices=[], label="Dataset Card")
-templates = gr.Dropdown(choices=[],label='Template')
-instructions = gr.Dropdown(choices=[None]+get_catalog_items('instructions'), label='Instruction')
-formats = gr.Dropdown(choices=[None]+get_catalog_items('formats'),label='Format')
-num_shots = gr.Slider(minimum=0,maximum=5,step=1,label='Num Shots')
-# with gr.Accordion('Advanced'):  
-augmentors = gr.Dropdown(choices=[],label='Augmentor')
+from gradio_ui.load_catalog_data import get_catalog_items, load_cards_data
 
-parameters = [tasks,cards,templates,instructions, formats, num_shots, augmentors]
-
-#TODO - move between samples
-#TODO - minimize code to copy, minimize augmentor
-
-#TODO color parts of the prompt
-#TODO - add model, output, score etc.
-#TODO if no train, choose test (add to ui?)
+# TODO - move between samples
+# TODO - minimize code to copy, minimize augmentor
+# TODO additional comments from Yotam
+# TODO color parts of the prompt
+# TODO - add model, output, score etc.
 
 
-def safe_add(parameter,key, args):
-    if isinstance(parameter,str):
+def safe_add(parameter, key, args):
+    if isinstance(parameter, str):
         args[key] = parameter
 
 
-def run_unitxt(task,dataset,template,instruction,format,num_demos,augmentor):
-    if not isinstance(dataset,str) or not isinstance(template,str):
-        return '','','',''
-    prompt_args = {'card':dataset, 'template': template, cons.LOADER_LIMIT:100}
-    if num_demos!=0:
-        prompt_args.update({'num_demos':num_demos, 'demos_pool_size':100})
-    safe_add(instruction,'instruction',prompt_args)
-    safe_add(format,'format',prompt_args)
-    safe_add(augmentor,'augmentor',prompt_args)
+def run_unitxt(task, dataset, template, instruction, format, num_demos, augmentor):
+    if not isinstance(dataset, str) or not isinstance(template, str):
+        return "", "", "", ""
+    prompt_args = {"card": dataset, "template": template, cons.LOADER_LIMIT_STR: 100}
+    if num_demos != 0:
+        prompt_args.update(
+            {"num_demos": num_demos, "demos_pool_size": cons.DEMOS_POOL_SIZE}
+        )
+    safe_add(instruction, "instruction", prompt_args)
+    safe_add(format, "format", prompt_args)
+    safe_add(augmentor, "augmentor", prompt_args)
 
-    prompt_list = build_prompt(prompt_args)
-    prompt = prompt_list[0]
+    prompts_list = build_prompt(prompt_args)
+    prompt = prompts_list[0]
     command = build_command(prompt_args)
-    return prompt['source'],prompt['metrics'],prompt['target'],command
+    return (
+        prompt[cons.PROMPT_SOURCE_STR],
+        prompt[cons.PROMPT_METRICS_STR],
+        prompt[cons.PROPT_TARGET_STR],
+        command,
+    )
 
 
-def build_prompt(prompt_args): 
+def build_prompt(prompt_args):
+    def collect_prompts(split_name):
+        prompt_list = []
+        for instance in dataset[split_name]:
+            if len(prompt_list) == cons.PROMPT_SAMPLE_SIZE:
+                return prompt_list
+            prompt_list.append(instance)
+        return None
+
     recipe = StandardRecipe(**prompt_args)
     dataset = recipe()
     prompt_list = []
-    for instance in dataset["train"]:
-        if len(prompt_list)==10:
-            return prompt_list
-        prompt_list.append(instance)
+    try:
+        prompt_list = collect_prompts("train")
+    except RuntimeError:
+        prompt_list = collect_prompts("test")
+    return prompt_list
+
 
 def build_command(prompt_data):
-    parameters_str = [f"{key}='{prompt_data[key]}'" for key in prompt_data if key!=cons.LOADER_LIMIT]
-    parameters_str = ",".join(parameters_str).replace("'",'')
-        
-    command = f"""
-    from datasets import load_dataset 
+    parameters_str = [
+        f"{key}='{prompt_data[key]}'"
+        for key in prompt_data
+        if key != cons.LOADER_LIMIT_STR
+    ]
+    parameters_str = ",".join(parameters_str).replace("'", "")
 
-    dataset = load_dataset(
-        'unitxt/data', '{parameters_str}'
-    )
-    """
-    return command
+    return f"dataset = load_dataset('unitxt/data', '{parameters_str}')"
+
 
 def update_choices_per_task(task_choice):
     datasets_choices = gr.update(choices=[])
     augmentors_choices = gr.update(choices=[])
-    if isinstance(task_choice,str):
-         if task_choice in data:
+    if isinstance(task_choice, str):
+        if task_choice in data:
             datasets_choices = gr.update(choices=get_datasets(task_choice))
             augmentors_choices = gr.update(choices=get_augmentors(task_choice))
     return datasets_choices, augmentors_choices
 
 
 def get_datasets(task_choice):
-     datasets_list = list(data[task_choice].keys())
-     datasets_list.remove(cons.AUGMENTABLE)
-     return datasets_list
+    datasets_list = list(data[task_choice].keys())
+    datasets_list.remove(cons.AUGMENTABLE_STR)
+    return datasets_list
+
 
 def get_augmentors(task_choice):
-    if data[task_choice][cons.AUGMENTABLE]:
-       return [None]+get_catalog_items('augmentors')
+    if data[task_choice][cons.AUGMENTABLE_STR]:
+        return [None, *get_catalog_items("augmentors")]
     return []
 
-def get_templates(task_choice, dataset_choice):
-    if not isinstance(dataset_choice,str):
-        return gr.update(choices = [],value = None)
-    return gr.update(choices = data[task_choice][dataset_choice])
 
-################################
+def get_templates(task_choice, dataset_choice):
+    if not isinstance(dataset_choice, str):
+        return gr.update(choices=[], value=None)
+    return gr.update(choices=data[task_choice][dataset_choice])
+
+
+# LOAD DATA
+data = load_cards_data()
+
+# UI ELEMENTS
+# input
+tasks = gr.Dropdown(choices=data.keys(), label="Task")
+cards = gr.Dropdown(choices=[], label="Dataset Card")
+templates = gr.Dropdown(choices=[], label="Template")
+instructions = gr.Dropdown(
+    choices=[None, *get_catalog_items("instructions")], label="Instruction"
+)
+formats = gr.Dropdown(choices=[None, *get_catalog_items("formats")], label="Format")
+num_shots = gr.Slider(minimum=0, maximum=5, step=1, label="Num Shots")
+augmentors = gr.Dropdown(choices=[], label="Augmentor")
+parameters = [tasks, cards, templates, instructions, formats, num_shots, augmentors]
+# output
+prompt = gr.Textbox(lines=5, show_copy_button=True, label="Prompt")
+metrics = gr.Textbox(lines=1, show_copy_button=True, label="Metrics")
+target = gr.Textbox(lines=1, show_copy_button=True, label="Target")
+code = gr.Code(label="Code", language="python", min_width=10)
+output_components = [prompt, metrics, target, code]
+
+
+######################
+### UI STARTS HERE ###
+######################
+
 demo = gr.Blocks()
 
 with demo:
-    logo = gr.Image('assets/banner.png', height=50, width=70,show_label=False,show_download_button=False,show_share_button=False)
-    tasks.change(update_choices_per_task,inputs=tasks,outputs=[cards,augmentors])
-    cards.change(get_templates,inputs=[tasks,cards], outputs=templates)
-    prompt = gr.Interface(
-                fn=run_unitxt,
-                inputs=parameters,
-                outputs=[gr.Textbox(lines=5,show_copy_button=True,label='Prompt'),
-                         gr.Textbox(lines=1,show_copy_button=True,label='Metrics'),
-                         gr.Textbox(lines=1,show_copy_button=True,label='Target'),
-                         gr.Textbox(lines=3,show_copy_button=True,label='Code'),
-                         ],
-                allow_flagging=False,
-                live=True,
+    # LOGO
+    logo = gr.Image(
+        "assets/banner.png",
+        height=50,
+        width=70,
+        show_label=False,
+        show_download_button=False,
+        show_share_button=False,
     )
-   
-    
-  
+
+    # DROPDOWN OPTIONS CHANGE ACCORDING TO SELECTION
+    tasks.change(update_choices_per_task, inputs=tasks, outputs=[cards, augmentors])
+    cards.change(get_templates, inputs=[tasks, cards], outputs=templates)
+
+    get_prompts = gr.Interface(
+        fn=run_unitxt,
+        inputs=parameters,
+        outputs=output_components,
+        allow_flagging=False,
+        live=True,
+    )
 
 
 demo.launch(debug=True)
