50a9fd8e6b724d7398862f328472fff476ffbe4f
Roni Friedman-Melamed <Roni.friedman-melamed@il.ibm.com>
2024-01-21 23:51:35 +0200

    modular UI!

--- src/unitxt/ui/run.py
+++ src/unitxt/ui/run.py
@@ -1,19 +1,21 @@
 from functools import lru_cache
 
-import evaluate
 import gradio as gr
-from transformers import pipeline
 
-from unitxt.standard import StandardRecipe
 from unitxt.ui import constants as cons
-from unitxt.ui.load_catalog_data import get_catalog_items, load_cards_data
-
-metric = evaluate.load(cons.UNITEXT_METRIC_STR)
-
-
-def safe_add(parameter, key, args):
-    if isinstance(parameter, str):
-        args[key] = parameter
+from unitxt.ui.ui_tiny_utils import *
+from unitxt.ui.ui_utils import (
+    build_command,
+    create_dataframe,
+    data,
+    get_catalog_items,
+    get_predictions_and_scores,
+    get_prompts,
+    get_templates,
+    hash_dict,
+    jsons,
+    update_choices_per_task,
+)
 
 
 def run_unitxt_entry(
@@ -24,9 +26,8 @@ def run_unitxt_entry(
     format=None,
     num_demos=0,
     augmentor=None,
+    index=1,
     run_model=False,
-    index=0,
-    # max_new_tokens=cons.MAX_NEW_TOKENS,
 ):
     is_dataset = isinstance(dataset, str)
     is_template = isinstance(template, str)
@@ -47,7 +48,7 @@ def run_unitxt_entry(
     if not isinstance(augmentor, str):
         augmentor = None
     if not isinstance(index, int):
-        index = 0
+        index = 1
     # if not isinstance(max_new_tokens, float):
     #     max_new_tokens = cons.MAX_NEW_TOKENS
     return run_unitxt(
@@ -64,72 +65,6 @@ def run_unitxt_entry(
 
 
 @lru_cache
-def get_prompts(dataset, template, num_demos, instruction, format, augmentor):
-    prompt_args = {"card": dataset, "template": template, cons.LOADER_LIMIT_STR: 100}
-    if num_demos != 0:
-        prompt_args.update(
-            {"num_demos": num_demos, "demos_pool_size": cons.DEMOS_POOL_SIZE}
-        )
-    safe_add(instruction, "instruction", prompt_args)
-    safe_add(format, "format", prompt_args)
-    safe_add(augmentor, "augmentor", prompt_args)
-
-    prompts_list = build_prompt(prompt_args)
-    return prompts_list, prompt_args
-
-
-@lru_cache
-def get_predictions_and_scores(prompts_hashable):
-    prompts_list = [unhash_dict(prompt) for prompt in prompts_hashable]
-    prompts_sources = [prompt[cons.PROMPT_SOURCE_STR] for prompt in prompts_list]
-    predictions = generate(
-        model_name=cons.FLAN_T5_BASE,
-        prompts=prompts_sources,
-    )
-    results = metric.compute(
-        predictions=predictions,
-        references=prompts_list,
-    )
-    return predictions, results
-
-
-def hash_dict(input_dict):
-    return frozenset(
-        (
-            key,
-            hash_dict(value)
-            if isinstance(value, dict)
-            else tuple(value)
-            if isinstance(value, list)
-            else value,
-        )
-        for key, value in input_dict.items()
-    )
-
-
-def unhash_dict(input_frozenset):
-    return {
-        key: unhash_dict(value)
-        if isinstance(value, frozenset)
-        else value
-        if not isinstance(value, tuple)
-        else list(value)
-        for key, value in input_frozenset
-    }
-
-
-def create_dataframe(scores):
-    try:
-        for val in ["score_name", "score"]:
-            if val in scores:
-                scores.pop(val)
-        rounded_scores = {key: round(value, 3) for key, value in scores.items()}
-        return list(rounded_scores.items())
-    except Exception:
-        return cons.EMPTY_SCORES_FRAME
-
-
-@lru_cache
 def run_unitxt(
     dataset,
     template,
@@ -138,9 +73,10 @@ def run_unitxt(
     num_demos=0,
     augmentor=None,
     run_model=False,
-    index=0,
+    index=1,
     # max_new_tokens=cons.MAX_NEW_TOKENS,
 ):
+    index = index - 1
     try:
         prompts_list, prompt_args = get_prompts(
             dataset, template, num_demos, instruction, format, augmentor
@@ -186,156 +122,44 @@ def run_unitxt(
     )
 
 
-def build_prompt(prompt_args):
-    def collect_prompts(split_name):
-        prompt_list = []
-        for instance in dataset[split_name]:
-            if len(prompt_list) == cons.PROMPT_SAMPLE_SIZE:
-                return prompt_list
-            prompt_list.append(instance)
-        return None
+def display_json(element, cards, templates, instructions, formats):
+    def get_json(element_key, value):
+        if isinstance(value, str):
+            if value in jsons:
+                to_print = jsons[value]
+                if "loader" in to_print:
+                    if "path" in to_print["loader"]:
+                        to_print["loader"][" path"] = to_print["loader"].pop("path")
+                return to_print
 
-    recipe = StandardRecipe(**prompt_args)
-    dataset = recipe()
-    prompt_list = []
-    try:
-        prompt_list = collect_prompts("train")
-    except (RuntimeError, KeyError):
-        prompt_list = collect_prompts("test")
-    return prompt_list
+            else:
+                return {"": f"Error: {value}'s json not found"}
+        else:
+            return {"": f"Select {element_key} to view json"}
 
-
-def build_command(prompt_data, with_prediction):
-    parameters_str = [
-        f"{key}='{prompt_data[key]}'"
-        for key in prompt_data
-        if key != cons.LOADER_LIMIT_STR
-    ]
-    parameters_str = ",".join(parameters_str).replace("'", "")
-    load_dataset_code = f"dataset = load_dataset('unitxt/data', '{parameters_str},max_train_instances=5', split='train')"
-
-    code = f"""
-{cons.DATASET_IMPORT_STR}
-
-{load_dataset_code}
-    """
-    if with_prediction:
-        imports_code = f"""
-{cons.PREDICTIONS_IMPORTS_STR}
-{cons.DATASET_IMPORT_STR}
-        """
-
-        code = f"""
-{imports_code}
-
-{load_dataset_code}
-{cons.PREDICTION_CODE_STR}
-        """
-    return code
+    if element == cons.DATASET_CARD:
+        element_name = cards
+    elif element == cons.TEMPLATE:
+        element_name = templates
+    elif element == cons.FORMAT:
+        element_name = formats
+    elif element == cons.INSTRUCTION:
+        element_name = instructions
+    message = get_json(element, element_name)
+    return element_name, message
 
 
-def update_choices_per_task(task_choice):
-    datasets_choices = gr.update(choices=[])
-    augmentors_choices = gr.update(choices=[])
-    if isinstance(task_choice, str):
-        if task_choice in data:
-            datasets_choices = gr.update(choices=get_datasets(task_choice))
-            augmentors_choices = gr.update(choices=get_augmentors(task_choice))
-    return datasets_choices, augmentors_choices
+def display_json_button(element):
+    tabs = change_tab()
+    if element in jsons:
+        json_el = jsons[element]
+        if "loader" in json_el:
+            if "path" in json_el["loader"]:
+                json_el["loader"][" path"] = json_el["loader"].pop("path")
+        return tabs, element, json_el
 
-
-def get_datasets(task_choice):
-    datasets_list = list(data[task_choice].keys())
-    datasets_list.remove(cons.AUGMENTABLE_STR)
-    return sorted(datasets_list)
-
-
-def get_augmentors(task_choice):
-    if data[task_choice][cons.AUGMENTABLE_STR]:
-        return [None, *get_catalog_items("augmentors")]
-    return []
-
-
-def get_templates(task_choice, dataset_choice):
-    if not isinstance(dataset_choice, str):
-        return gr.update(choices=[], value=None)
-    return gr.update(choices=sorted(data[task_choice][dataset_choice]))
-
-
-def generate(model_name, prompts, max_new_tokens=cons.MAX_NEW_TOKENS):
-    model = pipeline(model=f"google/{model_name}")
-    return [
-        output["generated_text"]
-        for output in model(prompts, max_new_tokens=max_new_tokens)
-    ]
-
-
-# LOAD DATA
-data = load_cards_data()
-
-# UI ELEMENTS
-# input
-tasks = gr.Dropdown(choices=sorted(data.keys()), label="Task")
-cards = gr.Dropdown(choices=[], label="Dataset Card")
-templates = gr.Dropdown(choices=[], label="Template")
-instructions = gr.Dropdown(
-    choices=[None, *get_catalog_items("instructions")], label="Instruction"
-)
-formats = gr.Dropdown(choices=[None, *get_catalog_items("formats")], label="Format")
-num_shots = gr.Radio(choices=list(range(6)), label="Num Shots", value=0)
-augmentors = gr.Dropdown(choices=[], label="Augmentor")
-run_model = gr.Checkbox(value=False, label=f"Predict with {cons.FLAN_T5_BASE}")
-
-sample_choice = gr.Radio(
-    label="Select sample to view",
-    choices=list(range(cons.PROMPT_SAMPLE_SIZE)),
-    info="switch to see a different sample from the dataset",
-    value=0,
-)
-# max_new_tokens = gr.Number(label="Max New Tokens", value=cons.MAX_NEW_TOKENS)
-parameters = [
-    tasks,
-    cards,
-    templates,
-    instructions,
-    formats,
-    num_shots,
-    augmentors,
-    run_model,
-    sample_choice,
-    # max_new_tokens,
-]
-# output
-selected_prompt = gr.Textbox(lines=3, show_copy_button=True, label="Prompt")
-metrics = gr.Textbox(lines=1, label="Metrics")
-target = gr.Textbox(lines=1, label="Target")
-prediction = gr.Textbox(
-    lines=1,
-    label="Model prediction",
-    value=" ",
-)
-instance_scores = gr.DataFrame(
-    label="Instance scores",
-    value=cons.EMPTY_SCORES_FRAME,
-    headers=cons.SCORE_FRAME_HEADERS,
-    height=150,
-)
-global_scores = gr.DataFrame(
-    label=f"Aggregated scores for {cons.PROMPT_SAMPLE_SIZE} predictions",
-    value=cons.EMPTY_SCORES_FRAME,
-    headers=cons.SCORE_FRAME_HEADERS,
-    height=150,
-    wrap=True,
-)
-code = gr.Code(label="Code", language="python", lines=1)
-output_components = [
-    selected_prompt,
-    target,
-    prediction,
-    instance_scores,
-    global_scores,
-    code,
-]
+    else:
+        return tabs, element, {"": f"Error: {element}'s json not found"}
 
 
 ######################
@@ -345,26 +169,167 @@ output_components = [
 demo = gr.Blocks()
 
 with demo:
-    # LOGO
-    logo = gr.Image(
-        cons.BANNER_PATH,
-        width="25%",
-        show_label=False,
-        show_download_button=False,
-        show_share_button=False,
-    )
+    with gr.Row():
+        # LOGO
+        logo = gr.Image(
+            cons.BANNER_PATH,
+            width="10%",
+            show_label=False,
+            show_download_button=False,
+            show_share_button=False,
+            scale=0.5,
+        )
+        links = gr.Markdown(value=cons.INTRO_TXT)
 
-    # DROPDOWN OPTIONS CHANGE ACCORDING TO SELECTION
+    # input
+    with gr.Row():
+        with gr.Column(scale=1):
+            tasks = gr.Dropdown(choices=sorted(data.keys()), label="Task", scale=3)
+            with gr.Row():
+                cards = gr.Dropdown(choices=[], label="Dataset Card", scale=5)
+                cards_js_button = gr.Button(
+                    "Dataset Card json",
+                    scale=1,
+                    size="sm",
+                    min_width=1,
+                    interactive=True,
+                )
+            with gr.Row():
+                templates = gr.Dropdown(choices=[], label="Template", scale=5)
+                templates_js_button = gr.Button(
+                    "Template json", scale=1, size="sm", min_width=1, interactive=False
+                )
+            with gr.Row():
+                instructions = gr.Dropdown(
+                    choices=[None, *get_catalog_items("instructions")[0]],
+                    label="Instruction",
+                    scale=5,
+                )
+                instructiosn_js_button = gr.Button(
+                    "Instruction json",
+                    scale=1,
+                    size="sm",
+                    min_width=1,
+                    interactive=False,
+                )
+            with gr.Row():
+                formats = gr.Dropdown(
+                    choices=[None, *get_catalog_items("formats")[0]],
+                    label="Format",
+                    scale=5,
+                )
+                formats_js_button = gr.Button(
+                    "Format json", scale=1, size="sm", min_width=1, interactive=False
+                )
+
+            num_shots = gr.Slider(label="Num Shots", maximum=5, step=1, value=0)
+            with gr.Accordion(label="Additional Parameters", open=False):
+                augmentors = gr.Dropdown(choices=[], label="Augmentor")
+
+            generat_prompts = gr.Button(value="Generate Prompts")
+            model_button = gr.Button(
+                value=f"Infer with {cons.FLAN_T5_BASE}", interactive=False
+            )
+            clear_fields = gr.ClearButton()
+
+        with gr.Column(scale=2):
+            with gr.Tabs() as tabs:
+                with gr.TabItem("Prompts and predictions", id=0):
+                    gr.Markdown("## Prompts:")
+                    with gr.Group():
+                        with gr.Row():
+                            sample_choice = gr.Radio(
+                                label="Browse between samples:",
+                                choices=list(range(1, cons.PROMPT_SAMPLE_SIZE + 1)),
+                                value=1,
+                                scale=1,
+                            )
+                        selected_prompt = gr.Textbox(
+                            lines=5, show_copy_button=True, label="Prompt"
+                        )
+
+                        target = gr.Textbox(lines=1, label="Target", scale=3)
+
+                    gr.Markdown("## Inference:")
+
+                    with gr.Group():
+                        prediction = gr.Textbox(
+                            lines=1,
+                            label="Model prediction",
+                            value=" ",
+                        )
+                        with gr.Row():
+                            instance_scores = gr.DataFrame(
+                                label="Instance scores",
+                                value=cons.EMPTY_SCORES_FRAME,
+                                headers=cons.SCORE_FRAME_HEADERS,
+                                height=150,
+                            )
+                            global_scores = gr.DataFrame(
+                                label=f"Aggregated scores for {cons.PROMPT_SAMPLE_SIZE} predictions",
+                                value=cons.EMPTY_SCORES_FRAME,
+                                headers=cons.SCORE_FRAME_HEADERS,
+                                height=150,
+                                wrap=True,
+                            )
+                with gr.TabItem("Catlog Json", id=1):
+                    select_element_json = gr.Radio(
+                        choices=[
+                            cons.DATASET_CARD,
+                            cons.TEMPLATE,
+                            cons.INSTRUCTION,
+                            cons.FORMAT,
+                        ]
+                    )
+                    element_name = gr.Text(label="Displayed Json")
+                    json_viewer = gr.Json(value={"Message": "Select element to watch"})
+
+    with gr.Accordion(label="Code to reproduce above run", open=False):
+        code = gr.Code(language="python", lines=1)
+
     tasks.change(update_choices_per_task, inputs=tasks, outputs=[cards, augmentors])
     cards.change(get_templates, inputs=[tasks, cards], outputs=templates)
-
-    result = gr.Interface(
-        fn=run_unitxt_entry,
-        inputs=parameters,
-        outputs=output_components,
-        allow_flagging="never",
-        # live=True,
+    select_element_json.select(
+        display_json,
+        [select_element_json, cards, templates, instructions, formats],
+        [element_name, json_viewer],
     )
+    run_model = gr.Checkbox(value=False, visible=False)
+
+    cards_js_button.click(display_json_button, cards, [tabs, element_name, json_viewer])
+
+    parameters = [
+        tasks,
+        cards,
+        templates,
+        instructions,
+        formats,
+        num_shots,
+        augmentors,
+        sample_choice,
+        run_model,
+    ]
+
+    outputs = {
+        selected_prompt,
+        target,
+        prediction,
+        instance_scores,
+        global_scores,
+        code,
+    }
+
+    clear_fields.click(lambda: [None] * len(parameters), outputs=parameters).then(
+        run_unitxt_entry, parameters, outputs=outputs
+    )
+    generat_prompts.click(run_unitxt_entry, parameters, outputs=outputs).then(
+        activate_button, outputs=model_button
+    )
+    model_button.click(select_checkbox, outputs=run_model).then(
+        run_unitxt_entry, parameters, outputs=outputs
+    ).then(deactivate_button, outputs=model_button)
+    sample_choice.change(run_unitxt_entry, parameters, outputs=outputs)
+
 
 if __name__ == "__main__":
     demo.launch(debug=True)
