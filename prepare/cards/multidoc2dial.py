0c4d3823110357d4f6af9d5ec118eef5ed266fa3
Ariel Gera <ariel.gera1@ibm.com>
2024-01-21 16:57:52 +0200

    multidoc2dial data
    Signed-off-by: Ariel Gera <ariel.gera1@ibm.com>

---
+++ prepare/cards/multidoc2dial.py
@@ -0,0 +1,43 @@
+from src.unitxt.blocks import LoadHF, TaskCard
+from src.unitxt.catalog import add_to_catalog
+from src.unitxt.operators import Apply, ListFieldValues, RenameFields
+from src.unitxt.test_utils.card import test_card
+
+
+def extract_question(text):
+    return text.split("[SEP]")[0]
+
+
+card_abstractive = TaskCard(
+    loader=LoadHF(path="multidoc2dial"),
+    preprocess_steps=[
+        RenameFields(
+            field_to_field={"answers/text/0": "relevant_context"},
+            use_query=True,
+        ),
+        ListFieldValues(fields=["utterance"], to_field="answer"),
+        Apply("question", function=extract_question, to_field="question"),
+    ],
+    task="tasks.qa.contextual.abstractive",
+    templates="templates.qa.contextual.all",
+)
+
+card_extractive = TaskCard(
+    loader=LoadHF(path="multidoc2dial"),
+    preprocess_steps=[
+        RenameFields(
+            field_to_field={"answers/text/0": "relevant_context"},
+            use_query=True,
+        ),
+        ListFieldValues(fields=["relevant_context"], to_field="answer"),
+        Apply("question", function=extract_question, to_field="question"),
+    ],
+    task="tasks.qa.contextual.extractive",
+    templates="templates.qa.contextual.all",
+)
+
+for name, card in zip(
+    ["abstractive", "extractive"], [card_abstractive, card_extractive]
+):
+    test_card(card)
+    add_to_catalog(card, f"cards.multidoc2dial.{name}", overwrite=True)
