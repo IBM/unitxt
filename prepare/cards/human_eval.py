48b30ecece30410805404c90ec6975ece06f846d
ofirarviv <ofir.arviv@ibm.com>
2024-01-22 13:06:33 +0200

    add human eval dataset

---
+++ prepare/cards/human_eval.py
@@ -0,0 +1,56 @@
+import json
+import re
+
+from unitxt import add_to_catalog
+from unitxt.blocks import (
+    FormTask,
+    InputOutputTemplate,
+    LoadHF,
+    TaskCard,
+    TemplatesList,
+)
+from unitxt.operators import Apply
+from unitxt.test_utils.card import test_card
+
+_ASSERT_REGEX = r"assert.*?(?=\n\s*assert|$)"
+
+
+def extract_asserts(test, entry_point):
+    return json.dumps(
+        [
+            t.replace("candidate", entry_point)
+            for t in re.findall(_ASSERT_REGEX, test, re.DOTALL)
+        ]
+    )
+
+
+card = TaskCard(
+    loader=LoadHF(path="openai_humaneval", split="test"),
+    preprocess_steps=[
+        Apply("test", "entry_point", function=extract_asserts, to_field="test_list")
+    ],
+    task=FormTask(
+        inputs=["prompt"],
+        outputs=["prompt", "canonical_solution", "test_list"],
+        metrics=["metrics.code_acc"],
+    ),
+    templates=TemplatesList(
+        [
+            InputOutputTemplate(
+                input_format="{prompt}\n",
+                output_format="{prompt}\n{canonical_solution}",
+            ),
+        ]
+    ),
+)
+
+test_card(
+    card,
+    demos_taken_from="test",
+    demos_pool_size=1,
+    num_demos=0,
+    strict=False,
+    loader_limit=500,
+    debug=False,
+)
+add_to_catalog(card, "cards.human_eval", overwrite=True)
