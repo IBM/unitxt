e0f3fc72bcae095533410a7b22f124ac7d4532e7
Matan Orbach <MATANO@il.ibm.com>
2024-01-22 16:18:45 +0200

    add new evaluate_rag example

--- src/unitxt/eval_utils.py
+++ src/unitxt/eval_utils.py
@@ -1,5 +1,3 @@
-import sys
-from ast import literal_eval
 from typing import List
 
 import pandas as pd
@@ -21,28 +19,3 @@ def evaluate(dataset: pd.DataFrame, metric_names: List[str]):
             instance["score"]["instance"]["score"] for instance in instances
         ]
     return result
-
-
-if __name__ == "__main__":
-    df = pd.read_csv(
-        filepath_or_buffer=sys.argv[1],
-        converters={
-            "ground_truths": literal_eval,
-            "ground_truths_context_ids": literal_eval,
-            "contexts": literal_eval,
-            "context_ids": literal_eval,
-        },
-    )
-
-    evaluate(
-        df,
-        metric_names=[
-            "metrics.rag.mrr",
-            "metrics.rag.map",
-            "metrics.rag.answer_correctness",
-            "metrics.rag.context_relevance",
-            "metrics.rag.faithfulness",
-            "metrics.rag.answer_relevance",
-            "metrics.rag.context_perplexity",
-        ],
-    ).round(2).to_csv("dataset_out.csv")
