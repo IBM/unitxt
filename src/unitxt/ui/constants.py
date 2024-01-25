806cf49dfe046d2a291d9e064d627a557108fe88
Roni Friedman-Melamed <Roni.friedman-melamed@il.ibm.com>
2024-01-25 12:33:31 +0200

    acknowledge openmoji

--- src/unitxt/ui/constants.py
+++ src/unitxt/ui/constants.py
@@ -51,3 +51,7 @@ MAIN_INTRO_TXT = """
 # then click the Generate Prompts button
 """
 JSON_BUTTON_TXT = "View"
+
+ACK_TEXT = "Unitxt emoji designed by [OpenMoji](http://openmoji.org]) – the open-source emoji and icon project. License: [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/#)"
+
+
