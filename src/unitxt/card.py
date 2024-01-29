c08ab3617f2f5e1a128c5ba465f469224a913fea
dafnapension <dafnashein@yahoo.com>
2024-01-29 21:10:15 +0200

    add more docstrings
    Signed-off-by: dafnapension <dafnashein@yahoo.com>

--- src/unitxt/card.py
+++ src/unitxt/card.py
@@ -12,14 +12,15 @@ from .task import FormTask
 class TaskCard(Artifact):
     """TaskCard delineates the phases in transforming the source dataset into a model-input, and specifies the metrics for evaluation of model-output.
 
-    Args:
-        loader: specifies the source address and the loading operator that can access that source and transform it into
-            a unitxt multistream.
-        preprocess_steps: list of unitxt operators to process the data source into a model-input
-        task: specifies the fields (of the already (pre)processed instance) making the inputs, the fields making the outputs, and the metrics to be used for evaluating
-            the output of the model
-        templates: format strings to be applied on the input fields (specified by the task) and the output fields. The templates also carry with them
-            the list of postprocessing steps, to be applied to the model output.
+    Attributes:
+        loader: specifies the source address and the loading operator that can access that source and transform it into a unitxt multistream.
+
+        preprocess_steps: list of unitxt operators to process the data source into a model-input.
+
+        task: specifies the fields (of the already (pre)processed instance) making the inputs, the fields making the outputs, and the metrics to be used for evaluating the model output.
+
+        templates: format strings to be applied on the input fields (specified by the task) and the output fields. The templates also carry with them the list of postprocessing steps, to be applied to the model output.
+
         instructions: as presented to the executor of the task.
     """
 
