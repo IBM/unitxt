cd8c025b0acbb695b183863f33b0896f8161bf0c
Matan Orbach <MATANO@il.ibm.com>
2024-02-01 14:51:42 +0200

    add docstrings

--- service/metrics/client_config.py
+++ service/metrics/client_config.py
@@ -1,8 +1,19 @@
 import json
 import os
 
-UNITXT_REMOTE_METRICS_ENDPOINT = "UNITXT_REMOTE_METRICS_ENDPOINT"
+"""
+This module enables loading the metric service client configuration from
+local environment variables
+"""
+
+# A list of metrics to be executed remotely.
+# For example: '["metrics.rag.context_relevance","metrics.rag.bert_k_precision"]'
+# This value should be a valid json list
 UNITXT_REMOTE_METRICS = "UNITXT_REMOTE_METRICS"
+
+# The remote endpoint on which the remote metrics are available.
+# For example, 'http://127.0.0.1:8000/compute'
+UNITXT_REMOTE_METRICS_ENDPOINT = "UNITXT_REMOTE_METRICS_ENDPOINT"
 
 
 def get_env_variable(variable_name: str, default_value: str) -> str:
@@ -12,6 +23,13 @@ def get_env_variable(variable_name: str, default_value: str) -> str:
 
 
 def get_metrics_client_config():
+    """Load the remote metrics configuration from environment variables.
+
+    Returns:
+        A tuple (remote_metrics, remote_metrics_endpoint) containing
+            remote_metrics: List[str] - names of metrics to be executed remotely.
+            remote_metrics_endpoint: str - The remote endpoint on which the remote metrics are available.
+    """
     remote_metrics = get_env_variable(UNITXT_REMOTE_METRICS, default_value=[])
     remote_metrics = json.loads(remote_metrics)
     if not isinstance(remote_metrics, list):
