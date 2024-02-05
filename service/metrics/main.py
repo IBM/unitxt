c656817952daaf9636f61f5713247b1ec044fd16
Matan Orbach <MATANO@il.ibm.com>
2024-02-05 15:42:05 +0200

    run main.py from docker  update the call to uvicorn.main() from main.py

--- service/metrics/main.py
+++ service/metrics/main.py
@@ -133,4 +133,4 @@ def print_gpus_status():
 
 if __name__ == "__main__":
     print_gpus_status()
-    uvicorn.run(app, log_config=None)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_config=None)
