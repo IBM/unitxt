deb982cd46fd79f4682e2ea8b96a1909cddf87e5
Elron Bandel <elron.bandel@ibm.com>
2024-02-08 18:35:39 +0200

    Small fix to ui and fix recipe
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -56,7 +56,7 @@ class TestRecipes(UnitxtTestCase):
         recipe = StandardRecipe(
             card="cards.mmlu.marketing",
             instruction="instructions.models.llama",
-            template="templates.qa.multiple_choice.original.lm_eval_harness",
+            template="templates.qa.multiple_choice.with_topic.lm_eval_harness",
             format="formats.user_agent",
             demos_pool_size=100,
             num_demos=3,
