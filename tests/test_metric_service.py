ccc2529518b00c8521bbc6188d57e8590502c571
Matan Orbach <MATANO@il.ibm.com>
2024-02-08 17:16:59 +0200

    add test_remote_service_with_valid_response()

--- tests/test_metric_service.py
+++ tests/test_metric_service.py
@@ -2,12 +2,16 @@ import json
 import os
 import unittest
 
+import httpretty
+
 from service.metrics.client_config import (
     UNITXT_REMOTE_METRICS,
     UNITXT_REMOTE_METRICS_ENDPOINT,
     get_remote_metrics_endpoint,
     get_remote_metrics_names,
 )
+from src.unitxt.metrics import RemoteMetric
+from src.unitxt.test_utils.metrics import test_metric
 
 
 class TestMetricsServiceClientConfig(unittest.TestCase):
@@ -56,3 +60,77 @@ class TestMetricsServiceClientConfig(unittest.TestCase):
 
         with self.assertRaises(RuntimeError):
             get_remote_metrics_names()
+
+
+class TestRemoteMetrics(unittest.TestCase):
+    @httpretty.activate(verbose=True)
+    def test_remote_service_with_valid_response(self):
+        """Test RemoteService with a mocked response that contains the expected response values."""
+        instance_targets = [
+            {
+                "f1": 0.8,
+                "precision": 0.86,
+                "recall": 0.84,
+                "score": 0.8,
+                "score_name": "f1",
+            },
+            {
+                "f1": 1.0,
+                "precision": 1.0,
+                "recall": 1.0,
+                "score": 1.0,
+                "score_name": "f1",
+            },
+        ]
+
+        global_target = {
+            "dummy_target_score_1": 0.9,
+            "dummy_target_score_2": 1.0,
+            "score_name": "dummy_target_score_1",
+            "score": 0.9,
+        }
+
+        predictions = ["prediction text 1", "prediction text 2"]
+        references = [
+            ["reference text 1.1", "reference text 1.2"],
+            ["reference text 2.1", "reference text 2.2"],
+        ]
+
+        def request_callback(request, uri, response_headers):
+            content_type = request.headers.get("Content-Type")
+            assert (
+                content_type == "application/json"
+            ), f"expected application/json but received Content-Type: {content_type}"
+
+            # Check that the input is as expected
+            inputs = json.loads(request.body)
+            for instance_i, input_instance in enumerate(inputs["instance_inputs"]):
+                self.assertEqual(input_instance["prediction"], predictions[instance_i])
+                self.assertListEqual(
+                    input_instance["references"], references[instance_i]
+                )
+                self.assertEqual(input_instance["additional_inputs"], {})
+
+            # Return the expected response
+            response = {
+                "instances_scores": instance_targets,
+                "global_score": global_target,
+            }
+            return [200, response_headers, json.dumps(response)]
+
+        host = "www.dummy_hostname.com"
+        endpoint = "http" + "://" + f"{host}/compute"
+        metric_name = "metrics.bert_score.deberta.xlarge.mnli"
+        httpretty.register_uri(
+            httpretty.POST, f"{endpoint}/{metric_name}", body=request_callback
+        )
+
+        metric = RemoteMetric(endpoint=endpoint, metric_name=metric_name)
+
+        test_metric(
+            metric=metric,
+            predictions=predictions,
+            references=references,
+            instance_targets=instance_targets,
+            global_target=global_target,
+        )
