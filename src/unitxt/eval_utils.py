7c447eaf3d940e3b78c91e2119768cf5ddbb2c08
lilacheden <lilach.edel@gmail.com>
2024-02-12 17:09:14 +0200

    add global scores and disable resampling for evaluate
    Signed-off-by: lilacheden <lilach.edel@gmail.com>

--- src/unitxt/eval_utils.py
+++ src/unitxt/eval_utils.py
@@ -1,5 +1,5 @@
 from functools import singledispatch
-from typing import List
+from typing import List, Optional
 
 import pandas as pd
 
@@ -8,22 +8,48 @@ from .stream import MultiStream
 
 
 @singledispatch
-def evaluate(dataset, metric_names: List[str]):
+def evaluate(
+    dataset, metric_names: List[str], compute_conf_intervals: Optional[bool] = False
+):
     """Placeholder for overloading the function, supporting both dataframe input and list input."""
     pass
 
 
 @evaluate.register
-def _(dataset: list, metric_names: List[str]):
+def _(
+    dataset: list,
+    metric_names: List[str],
+    compute_conf_intervals: Optional[bool] = False,
+):
+    global_scores = {}
     for metric_name in metric_names:
         multi_stream = MultiStream.from_iterables({"test": dataset}, copying=True)
         metrics_operator = SequentialOperator(steps=[metric_name])
+
+        if not compute_conf_intervals:
+            metrics_operator.steps[0].metric.n_resamples = None
+
         instances = list(metrics_operator(multi_stream)["test"])
         for entry, instance in zip(dataset, instances):
             entry[metric_name] = instance["score"]["instance"]["score"]
-    return dataset
+
+        if len(instances) > 0:
+            global_scores[metric_name] = instances[0]["score"].get("global", {})
+    return dataset, global_scores
 
 
 @evaluate.register
-def _(dataset: pd.DataFrame, metric_names: List[str]):
-    return pd.DataFrame(evaluate(dataset.to_dict("records"), metric_names=metric_names))
+def _(
+    dataset: pd.DataFrame,
+    metric_names: List[str],
+    compute_conf_intervals: Optional[bool] = False,
+):
+    results, global_scores = evaluate(
+        dataset.to_dict("records"),
+        metric_names=metric_names,
+        compute_conf_intervals=compute_conf_intervals,
+    )
+    return pd.DataFrame(results), global_scores
+
+
+#
