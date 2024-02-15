002206423cacd9e6365567880556fc33ebc3fd7a
Elron Bandel <elron.bandel@ibm.com>
2024-02-15 15:33:47 +0200

    fix missing import
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/test_utils/catalog.py
+++ src/unitxt/test_utils/catalog.py
@@ -1,11 +1,12 @@
 import os
 
-from ..register import UNITXT_ARTIFACTORIES_ENV_VAR, _reset_env_local_catalogs
-from ..settings_utils import get_constants
+from ..register import _reset_env_local_catalogs
+from ..settings_utils import get_constants, get_settings
 
 constants = get_constants()
+settings = get_settings()
 
 
 def register_local_catalog_for_tests():
-    os.environ[UNITXT_ARTIFACTORIES_ENV_VAR] = constants.catalog_dir
+    os.environ[settings.artifactories_key] = constants.catalog_dir
     _reset_env_local_catalogs()
