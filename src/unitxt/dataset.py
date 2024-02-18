0e2d06e092403f249a030bdace913aa69785f1fd
Elron Bandel <elron.bandel@ibm.com>
2024-02-18 09:28:34 +0200

    Format
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/dataset.py
+++ src/unitxt/dataset.py
@@ -94,7 +94,9 @@ class Dataset(datasets.GeneratorBasedBuilder):
         generator = self.generators[split_name]
         yield from enumerate(generator)
 
-    def _download_and_prepare(self, dl_manager, verification_mode, **prepare_splits_kwargs):
+    def _download_and_prepare(
+        self, dl_manager, verification_mode, **prepare_splits_kwargs
+    ):
         return super()._download_and_prepare(
             dl_manager, "no_checks", **prepare_splits_kwargs
         )
