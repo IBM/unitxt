d5582be62fc9565a3d2a4ca85c25d5c7762384a0
Rajmohan <rajmohanc1@in.ibm.com>
2024-02-20 18:07:57 +0530

    updated docstring in operators.py

--- src/unitxt/operators.py
+++ src/unitxt/operators.py
@@ -20,7 +20,7 @@ Some operators are specielized in specific task such as:
 
 - :class:`loaders<unitxt.loaders>` for loading data.
 - :class:`splitters<unitxt.splitters>` for fixing data splits.
-- :class:`table_operators<unitxt.table_operators>` for tabular data operators.
+- :class:`struct_data_operators<unitxt.struct_data_operators>` for structured data operators.
 
 Other specelized operators are used by unitxt internally:
 
