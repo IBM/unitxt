89d50a61937eda34c7c18e83d2481a551a7bac1b
yotamp <yotam.perlitz@ibm.com>
2024-02-22 19:36:41 +0200

    fix score naming
    Signed-off-by: yotamp <yotam.perlitz@ibm.com>

--- prepare/metrics/llama_index_correctness.py
+++ prepare/metrics/llama_index_correctness.py
@@ -9,7 +9,7 @@ metric = LlamaIndexCorrectnessMetric(model_name=model_name)
 
 predictions = ["The right answer"]
 references = [["The right answer", "The wrong answer"]]
-additional_inputs = [
+task_data = [
     {
         "question": "question number 1",
         "contexts": "['context number 1']",
@@ -17,26 +17,29 @@ additional_inputs = [
     },
 ]
 
+model_name_normalized = model_name.replace(".", "_").replace("-", "_")
+score_name = f"correctness_llama_index_by_{model_name_normalized}_judge"
+
 instance_targets = [  # nDCG is undefined at instance level
     {
         "score": 1.0,
-        "score_name": f"{model_name}_judge",
-        f"{model_name}_judge": 1.0,
+        "score_name": score_name,
+        score_name: 1.0,
         # "feedback": "The generated answer is fully correct and relevant to the user query, matching the reference answer exactly.",
     }
 ] * len(predictions)
 
 global_target = {
     "score": 1.0,
-    "score_name": f"{model_name}_judge",
-    f"{model_name}_judge": 1.0,
+    "score_name": score_name,
+    score_name: 1.0,
 }
 
 outputs = test_metric(
     metric=metric,
     predictions=predictions,
     references=references,
-    additional_inputs=additional_inputs,
+    task_data=task_data,
     instance_targets=instance_targets,
     global_target=global_target,
 )
