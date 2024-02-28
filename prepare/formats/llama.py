0da9b3011e1c24a9adb58aa5b799ec71075aa57f
Elron Bandel <elron.bandel@ibm.com>
2024-02-28 17:58:57 +0200

    Revrse some fixes for backward compatibility
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- prepare/formats/llama.py
+++ prepare/formats/llama.py
@@ -3,7 +3,7 @@ from src.unitxt.formats import SystemFormat
 
 format = SystemFormat(
     demo_format="{source}\n{target_prefix}{target}\n\n",
-    model_input_format="[INST] {system_prompt}\n{instruction}\n{demos}\n{source}\n[/INST]{target_prefix}",
+    model_input_format="[INST] {system_prompt}{instruction}\n{demos}\n{source}\n[/INST]{target_prefix}",
 )
 
 add_to_catalog(format, "formats.llama", overwrite=True)
