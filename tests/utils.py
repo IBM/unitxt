38babe1c43c0dfa7aefcb1beec5dd2c2ef66b9b8
Elron Bandel <elron.bandel@ibm.com>
2024-02-28 17:22:42 +0200

    Limit log in tests to 1000 chars
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- tests/utils.py
+++ tests/utils.py
@@ -16,7 +16,7 @@ class UnitxtTestCase(unittest.TestCase):
         unitxt.settings.allow_unverified_code = True
         unitxt.settings.use_only_local_catalogs = True
         unitxt.settings.global_loader_limit = 300
-        unitxt.settings.max_log_message_size = 2000
+        unitxt.settings.max_log_message_size = 1000
         register_local_catalog_for_tests()
 
     def setUp(self):
