2e6835baf7674be0476521188e686ae36e1a4764
Elron Bandel <elron.bandel@ibm.com>
2024-02-28 17:58:57 +0200

    Revrse some fixes for backward compatibility
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- prepare/formats/empty.py
+++ prepare/formats/empty.py
@@ -2,7 +2,7 @@ from src.unitxt.catalog import add_to_catalog
 from src.unitxt.formats import SystemFormat
 
 format = SystemFormat(
-    demo_format="{source}\n{target_prefix}{target}\n\n",
+    demo_format="{source}\n {target_prefix}{target}\n\n",
     model_input_format="{system_prompt}{instruction}{demos}{source}\n{target_prefix}",
 )
 
