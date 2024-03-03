2b8d0c5550c08075a806c2565d80b04986609d66
Elron Bandel <elron.bandel@ibm.com>
2024-03-03 16:09:39 +0200

    Fix undefined value
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/dataclass.py
+++ src/unitxt/dataclass.py
@@ -6,6 +6,10 @@ from typing import Any, final
 _FIELDS = "__fields__"
 
 
+class Undefined:
+    pass
+
+
 @dataclasses.dataclass
 class Field:
     """An alternative to dataclasses.dataclass decorator for a more flexible field definition.
@@ -21,7 +25,7 @@ class Field:
         origin_cls (type, optional): The original class that defined the field. Defaults to None.
     """
 
-    default: Any = None
+    default: Any = Undefined
     name: str = None
     type: type = None
     init: bool = True
@@ -59,7 +63,7 @@ class MissingDefaultError(TypeError):
 class OptionalField(Field):
     def __post_init__(self):
         self.required = False
-        if self.default is None and self.default_factory is None:
+        if self.default is Undefined and self.default_factory is None:
             raise MissingDefaultError(
                 "OptionalField must have default or default_factory"
             )
