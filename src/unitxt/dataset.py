a3b61b2337ed4ebbaceed4edbbd7fca690ca1347
Elron Bandel <elron.bandel@ibm.com>
2024-03-04 16:16:10 +0200

    Fix tests
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/dataset.py
+++ src/unitxt/dataset.py
@@ -32,6 +32,7 @@ from .recipe import __file__ as _
 from .register import __file__ as _
 from .schema import __file__ as _
 from .settings_utils import get_constants
+from .span_lableing_operators import __file__ as _
 from .split_utils import __file__ as _
 from .splitters import __file__ as _
 from .standard import __file__ as _
