95d70cd31373f101f31b225e92da02e3e085c319
michal <shmueli@il.ibm.com>
2024-03-12 19:41:43 +0200

    fix summarization template

--- prepare/templates/summarization/abstractive.py
+++ prepare/templates/summarization/abstractive.py
@@ -5,9 +5,6 @@ add_to_catalog(
     InputOutputTemplate(
         input_format="Summarize the following {document_type}: {document}.",
         output_format="{summary}",
-        postprocessors=[
-            "processors.take_first_non_empty_line",
-        ],
     ),
     "templates.summarization.abstractive.full",
     overwrite=True,
@@ -17,9 +14,6 @@ add_to_catalog(
     InputOutputTemplate(
         input_format="Summarize the following text into one sentence: {document}.",
         output_format="{summary}",
-        postprocessors=[
-            "processors.take_first_non_empty_line",
-        ],
     ),
     "templates.summarization.abstractive.one_sentence",
     overwrite=True,
@@ -29,9 +23,6 @@ add_to_catalog(
     InputOutputTemplate(
         input_format="The following {document_type} is to be summarized into one sentence: {document}.",
         output_format="{summary}",
-        postprocessors=[
-            "processors.take_first_non_empty_line",
-        ],
     ),
     "templates.summarization.abstractive.passive",
     overwrite=True,
@@ -41,9 +32,6 @@ add_to_catalog(
     InputOutputTemplate(
         input_format="Write a succinct summary of the following {document_type}: {document}.",
         output_format="{summary}",
-        postprocessors=[
-            "processors.take_first_non_empty_line",
-        ],
     ),
     "templates.summarization.abstractive.write_succinct",
     overwrite=True,
