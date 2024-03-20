841cb58da4586c0530192fa8f755bb8f9d6fc6ba
Elron Bandel <elron.bandel@ibm.com>
2024-03-20 14:46:34 +0200

    Improve error message
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/utils.py
+++ src/unitxt/utils.py
@@ -110,4 +110,6 @@ def safe_eval(expression: str, context: dict, allowed_tokens: list) -> any:
     allowd_sub_strings = list(context.keys()) + allowed_tokens
     if is_made_of_sub_strings(expression, allowd_sub_strings):
         return eval(expression, {"__builtins__": {}}, context)
-    raise ValueError(f"Unreocgnized Expression: {expression}")
+    raise ValueError(
+        f"The expression '{expression}' can not be evaluated because it contains tokens outside the allowed list of {allowd_sub_strings}."
+    )
