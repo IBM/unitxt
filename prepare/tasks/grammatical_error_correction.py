bc150408ba4d3d3fc64c140814d833944e97bbc8
Ariel Gera <ariel.gera1@ibm.com>
2024-03-26 15:57:46 +0200

    Grammatical error correction task
    Signed-off-by: Ariel Gera <ariel.gera1@ibm.com>

---
+++ prepare/tasks/grammatical_error_correction.py
@@ -0,0 +1,12 @@
+from unitxt import add_to_catalog
+from unitxt.task import FormTask
+
+add_to_catalog(
+    FormTask(
+        inputs=["original_text"],
+        outputs=["corrected_texts"],
+        metrics=["metrics.char_edit_dist_accuracy", "metrics.rouge"],
+    ),
+    "tasks.grammatical_error_correction",
+    overwrite=True,
+)
---
+++ prepare/templates/editing/grammatical_error_correction.py
@@ -0,0 +1,13 @@
+from unitxt import add_to_catalog
+from unitxt.templates import MultiReferenceTemplate
+
+add_to_catalog(
+    MultiReferenceTemplate(
+        instruction="Make the minimal amount of changes to correct grammar and spelling errors in the following text.",
+        input_format="Original text: {original_text}",
+        references_field="corrected_texts",
+        target_prefix="Corrected text: ",
+    ),
+    "templates.grammatical_error_correction.simple",
+    overwrite=True,
+)
