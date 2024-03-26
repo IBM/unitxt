53d6adba1a144b0e4e7f25974f60a4abb55354fc
Ariel Gera <ariel.gera1@ibm.com>
2024-03-26 15:59:20 +0200

    Add coedit grammar correction card
    Signed-off-by: Ariel Gera <ariel.gera1@ibm.com>

---
+++ prepare/cards/coedit.py
@@ -0,0 +1,27 @@
+from unitxt import add_to_catalog
+from unitxt.card import TaskCard
+from unitxt.loaders import LoadHF
+from unitxt.operators import (
+    ExecuteExpression,
+    FilterByCondition,
+    ListFieldValues,
+    RenameFields,
+)
+from unitxt.test_utils.card import test_card
+
+card = TaskCard(
+    loader=LoadHF(path="grammarly/coedit", streaming=True),
+    preprocess_steps=[
+        FilterByCondition(values={"task": "gec"}, condition="eq"),
+        "splitters.small_no_test",
+        ExecuteExpression(expression="': '.join(src.split(': ')[1:])", to_field="src"),
+        RenameFields(field_to_field={"src": "original_text"}),
+        ListFieldValues(fields=["tgt"], to_field="corrected_texts"),
+    ],
+    task="tasks.grammatical_error_correction",
+    templates=["templates.grammatical_error_correction.simple"],
+)
+
+
+test_card(card, strict=False)
+add_to_catalog(card, "cards.coedit_gec", overwrite=True)
