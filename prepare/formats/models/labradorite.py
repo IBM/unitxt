1b827c5860a1e5dc6ce3462101cb50567253ea10
Elad Venezian <eladv@il.ibm.com>
2024-03-28 12:14:19 +0200

    Rename to correct model name

--- prepare/formats/models/labradorite.py
+++ prepare/formats/models/labradorite.py
@@ -5,7 +5,7 @@ format = SystemFormat(
     model_input_format="<|system|>\n{system_prompt}\n<|user|>\n{instruction}\n{source}\n<|assistant|>\n{target_prefix}",
 )
 
-add_to_catalog(format, "formats.models.labrador.zero_shot", overwrite=True)
+add_to_catalog(format, "formats.models.labradorite.zero_shot", overwrite=True)
 
 format = SystemFormat(
     demo_format="{source}\n{target_prefix}{target}\n\n",
@@ -24,4 +24,4 @@ format = SystemFormat(
     ),
 )
 
-add_to_catalog(format, "formats.models.labrador.few_shot", overwrite=True)
+add_to_catalog(format, "formats.models.labradorite.few_shot", overwrite=True)
--- prepare/system_prompts/models/labradorite.py
+++ prepare/system_prompts/models/labradorite.py
@@ -5,4 +5,4 @@ system_prompt = TextualSystemPrompt(
     "You are Labrador, an AI language model developed by IBM DMF (Data Model Factory) Alignment Team. You are a cautious assistant. You carefully follow instructions. You are helpful and harmless and you follow ethical guidelines and promote positive behavior. You always respond to greetings (for example, hi, hello, g'day, morning, afternoon, evening, night, what's up, nice to meet you, sup, etc) with \"Hello! I am an AI language model, created by IBM. How can I help you today?\". Please do not say anything else and do not start a conversation."
 )
 
-add_to_catalog(system_prompt, "system_prompts.models.labrador", overwrite=True)
+add_to_catalog(system_prompt, "system_prompts.models.labradorite", overwrite=True)
