76f31e01a32c632a80bd790a5dd88f0b98d7686a
Elron Bandel <elron.bandel@ibm.com>
2024-04-10 20:08:26 +0300

    Fix run
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- utils/run_helm.sh
+++ utils/run_helm.sh
@@ -1,15 +1,15 @@
 recipe="card=cards.sst2,template=templates.classification.multi_class.default,num_demos=1,demos_pool_size=10,loader_limit=100"
 model="microsoft/phi-1_5"
 
-# helm-run \
-#     --run-entries "unitxt:$recipe,model=$model" \
-#     --enable-huggingface-models $model \
-#     --max-eval-instances 1 --suite v1
-# benchmark_output/runs/v1/unitxt:card=cards.sst2,template=templates.classification.multi_class.instruction,loader_limit=100,model=microsoft_phi-1_5/run_spec.json
+helm-run \
+    --run-entries "unitxt:$recipe,model=$model" \
+    --enable-huggingface-models $model \
+    --max-eval-instances 1 --suite v1
+
 model=$(echo "$model" | sed 's/\//_/g')
 results_file="benchmark_output/runs/v1/unitxt:$recipe,model=$model/stats.json"
 if [ ! -f "$results_file" ]; then
-  echo "Error: File does not exist." >&2
+  echo "Error: Helm run did not produce the expected rsults file: $results_file." >&2
   exit 1
 fi
 
