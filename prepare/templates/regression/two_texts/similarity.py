ea8e7ef1e7c529697580e54165a38b07a41dad30
Yoav Katz <68273864+yoavkatz@users.noreply.github.com>
2024-04-14 19:45:58 +0300

    Added post processor to take first line in quantization templates (#770)
    * Added post processor to take first line in quantization templates
    
    Also fixed template missing quotes.
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    * Fixed unit tests
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    ---------
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/templates/regression/two_texts/similarity.py
+++ prepare/templates/regression/two_texts/similarity.py
@@ -15,6 +15,7 @@ add_to_catalog(
         input_format="Sentence 1: {text1} Sentence 2: {text2}",
         output_format="{attribute_value}",
         quantum=1,
+        postprocessors=["processors.take_first_non_empty_line"],
     ),
     "templates.regression.two_texts.similarity.flan",
     overwrite=True,
