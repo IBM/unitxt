518d01691c652b5e73d7c223bef82ce57e2eb7d9
ofirarviv <ofir.arviv@ibm.com>
2024-05-05 17:23:42 +0300

    update llam3 metric with correct format

--- prepare/metrics/llm_as_judge_metric_absolute_score_single_turn_mt_bench_template.py
+++ prepare/metrics/llm_as_judge_metric_absolute_score_single_turn_mt_bench_template.py
@@ -16,10 +16,10 @@ for model_name in model_list:
     recipe = (
         "card=cards.rag.model_response_assessment.llm_as_judge_metric_absolute_score_single_turn,"
         "template=templates.model_response_assessment.mt_bench_absolute_score_single_turn,"
+        "format=formats.llama3_chat,"
         "demos_pool_size=0,"
         "num_demos=0"
     )
-    # llm_as_judge_metric_absolute_score_single_turn_mt_bench_template
     model_label = model_name.split("/")[1].replace("-", "_")
     main_score = f"llm_as_judge_metric_absolute_score_single_turn_mt_bench_template_using_{model_label}_on_ibm_genai"
     metric = LLMAsJudge(
