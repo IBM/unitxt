e4f92f8995e104558782fc847e67fc68a39aded8
ofirarviv <ofir.arviv@ibm.com>
2024-05-05 17:49:41 +0300

    add model assestmnt tasks with reference

---
+++ prepare/tasks/model_response_assessment/absolute_score_dialog_with_reference.py
@@ -0,0 +1,12 @@
+from unitxt.blocks import FormTask
+from unitxt.catalog import add_to_catalog
+
+add_to_catalog(
+    FormTask(
+        inputs={"dialog": "List[Tuple[str, str]]", "reference": "str"},
+        outputs={"rating_label": "float"},
+        metrics=["metrics.spearman"],
+    ),
+    "tasks.model_response_assessment.absolute_score_dialog",
+    overwrite=True,
+)
---
+++ prepare/tasks/model_response_assessment/absolute_score_single_turn_with_reference.py
@@ -0,0 +1,12 @@
+from unitxt.blocks import FormTask
+from unitxt.catalog import add_to_catalog
+
+add_to_catalog(
+    FormTask(
+        inputs={"question": "str", "model_output": "str", "reference": "str"},
+        outputs={"rating_label": "float"},
+        metrics=["metrics.spearman"],
+    ),
+    "tasks.model_response_assessment.absolute_score_single_turn",
+    overwrite=True,
+)
