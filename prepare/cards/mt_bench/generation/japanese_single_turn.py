f6078a153f0572d40edc4c299f6fe2a50bf98748
ofirarviv <ofir.arviv@ibm.com>
2024-05-12 21:19:18 +0300

    add mt becnh generation datasets

---
+++ prepare/cards/mt_bench/generation/japanese_single_turn.py
@@ -0,0 +1,42 @@
+from unitxt.blocks import (
+    TaskCard,
+)
+from unitxt.catalog import add_to_catalog
+from unitxt.loaders import LoadHF
+from unitxt.operators import (
+    AddFields,
+    CopyFields,
+    RenameFields,
+)
+from unitxt.splitters import RenameSplits
+from unitxt.test_utils.card import test_card
+
+card = TaskCard(
+    loader=LoadHF(path="shi3z/MTbenchJapanese", split="train"),
+    preprocess_steps=[
+        RenameSplits({"train": "test"}),
+        CopyFields(field_to_field={"turns/0": "turns"}),
+        RenameFields(
+            field_to_field={
+                "turns": "input",
+                "category": "group",
+            }
+        ),
+        AddFields(
+            fields={
+                "output": "None",
+                "type_of_input": "question",
+                "type_of_output": "answer",
+            }
+        ),
+    ],
+    task="tasks.generation",
+    templates=["templates.empty"],
+)
+
+test_card(card, demos_taken_from="test", strict=True)
+add_to_catalog(
+    card,
+    "cards.mt_bench.generation.japanese_single_turn",
+    overwrite=True,
+)
