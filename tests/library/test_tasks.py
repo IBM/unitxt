7b22668373eebfa398c0b0e195e9580cfdfda695
Elron Bandel <elron.bandel@ibm.com>
2024-05-16 18:08:45 +0300

    remove multiple choice task
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- tests/library/test_tasks.py
+++ tests/library/test_tasks.py
@@ -1,58 +1,9 @@
-from unitxt.task import MultipleChoiceTask, Task
-from unitxt.test_utils.operators import apply_operator
+from unitxt.task import Task
 
 from tests.utils import UnitxtTestCase
 
 
 class TestTasks(UnitxtTestCase):
-    def test_multiple_choice_task(self):
-        operator = MultipleChoiceTask(
-            inputs=["choices", "sentence1", "sentence2"],
-            outputs=["label"],
-            metrics=["metrics.accuracy"],
-        )
-
-        inputs = [
-            {
-                "choices": ["entailment", "not_entailment"],
-                "sentence1": "This is a sentence",
-                "sentence2": "This is another sentence",
-                "label": "entailment",
-            },
-            {
-                "choices": ["entailment", "not_entailment"],
-                "sentence1": "This is a sentence",
-                "sentence2": "This is another sentence",
-                "label": "not_entailment",
-            },
-        ]
-
-        targets = [
-            {
-                "inputs": {
-                    "choices": "A. entailment\nB. not_entailment",
-                    "sentence1": "This is a sentence",
-                    "sentence2": "This is another sentence",
-                },
-                "outputs": {"label": "A"},
-                "metrics": ["metrics.accuracy"],
-            },
-            {
-                "inputs": {
-                    "choices": "A. entailment\nB. not_entailment",
-                    "sentence1": "This is a sentence",
-                    "sentence2": "This is another sentence",
-                },
-                "outputs": {"label": "B"},
-                "metrics": ["metrics.accuracy"],
-            },
-        ]
-
-        outputs = apply_operator(operator=operator, inputs=inputs)
-
-        for output, target in zip(outputs, targets):
-            self.assertDictEqual(output, target)
-
     def test_task_metrics_type_checking(self):
         operator = Task(
             inputs={"input": "str"},
