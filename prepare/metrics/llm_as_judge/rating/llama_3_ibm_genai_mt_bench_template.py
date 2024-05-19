f15ce3de5d740fdc72142b62b01b3a5d854d6d8e
ofirarviv <ofir.arviv@ibm.com>
2024-05-19 10:57:22 +0300

    update metric

--- prepare/metrics/llm_as_judge/rating/llama_3_ibm_genai_mt_bench_template.py
+++ prepare/metrics/llm_as_judge/rating/llama_3_ibm_genai_mt_bench_template.py
@@ -3,26 +3,30 @@ from unitxt.inference import (
     IbmGenAiInferenceEngine,
     IbmGenAiInferenceEngineParams,
 )
-from unitxt.llm_as_judge import LLMAsJudge
+from unitxt.llm_as_judge import LLMAsJudgeSingleModelSingleTurn
 
 model_list = ["meta-llama/llama-3-8b-instruct", "meta-llama/llama-3-70b-instruct"]
-task = "rating.single_turn"
 format = "formats.llama3_chat"
 template = "templates.response_assessment.rating.mt_bench_single_turn"
+template_model_input_field_name = "question"
+template_model_output_model_field_name = "answer"
+template_reference_field_name = None
 
 gen_params = IbmGenAiInferenceEngineParams(max_new_tokens=252)
 for model_id in model_list:
     inference_model = IbmGenAiInferenceEngine(
         model_name=model_id, parameters=gen_params
     )
-    model_label = model_id.split("/")[1].replace("-", "_")
+    model_label = model_id.split("/")[1].replace("-", "_").replace(".", ",").lower()
     model_label = f"{model_label}_ibm_genai"
     template_label = template.split(".")[-1]
     metric_label = f"{model_label}_template_{template_label}"
-    metric = LLMAsJudge(
+    metric = LLMAsJudgeSingleModelSingleTurn(
         inference_model=inference_model,
-        task=task,
         template=template,
+        template_model_input_field_name=template_model_input_field_name,
+        template_model_output_field_name=template_model_output_model_field_name,
+        template_reference_field_name=template_reference_field_name,
         format=format,
         main_score=metric_label,
     )
