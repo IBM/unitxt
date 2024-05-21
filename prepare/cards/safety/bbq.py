6ab0df1965bf8382ec3b5bb1b1f31f15618e3f3a
Jonathan Bnayahu <bnayahu@il.ibm.com>
2024-05-21 11:26:22 +0300

    Initial implementation of bbq and discrim-eval. Signed-off-by: Jonathan Bnayahu <bnayahu@il.ibm.com>

---
+++ prepare/cards/safety/bbq.py
@@ -0,0 +1,38 @@
+from unitxt import add_to_catalog
+from unitxt.blocks import LoadHF, TaskCard
+from unitxt.operators import AddFields, ListFieldValues, RenameFields
+from unitxt.settings_utils import get_settings
+from unitxt.test_utils.card import test_card
+
+settings = get_settings()
+orig_settings = settings.allow_unverified_code
+settings.allow_unverified_code = True
+
+for dataset_name in [
+    "Age",
+    "Disability_status",
+    "Gender_identity",
+    "Nationality",
+    "Physical_appearance",
+    "Race_ethnicity",
+    "Race_x_SES",
+    "Race_x_gender",
+    "Religion",
+    "SES",
+    "Sexual_orientation",
+]:
+    card = TaskCard(
+        loader=LoadHF(path="heegyu/bbq", name=dataset_name),
+        preprocess_steps=[
+            AddFields({"context_type": "description"}),
+            RenameFields(field_to_field={"label": "answer"}),
+            ListFieldValues(fields=["ans0", "ans1", "ans2"], to_field="choices"),
+        ],
+        task="tasks.qa.multiple_choice.with_context",
+        templates=["templates.qa.multiple_choice.with_context.match"],
+    )
+
+    test_card(card, strict=False, demos_taken_from="test", num_demos=0)
+    add_to_catalog(card, "cards.safety.bbq." + dataset_name, overwrite=True)
+
+settings.allow_unverified_code = orig_settings
