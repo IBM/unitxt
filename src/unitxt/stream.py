87c08f859e99dc26de142d74f75c200c966026b5
dafnapension <dafnashein@yahoo.com>
2024-05-29 15:07:20 +0300

    returned ListStream generation back to post_init. It is eager now.
    Signed-off-by: dafnapension <dafnashein@yahoo.com>

--- src/unitxt/stream.py
+++ src/unitxt/stream.py
@@ -101,32 +101,30 @@ class DynamicStream(Stream):
 
     def __post_init__(self):
         self.stream = None
-
-    def _verify_init(self):
-        if self.stream is None:
-            if settings.use_eager_execution:
+        if settings.use_eager_execution:
+            try:
                 instances_list = []
                 for instance in self.generator(**self.gen_kwargs):
                     instances_list.append(instance)
                 self.stream = ListStream(instances_list=instances_list)
-            else:
-                self.stream = GeneratorStream(
-                    generator=self.generator,
-                    gen_kwargs=self.gen_kwargs,
-                    caching=self.caching,
-                    copying=self.copying,
-                )
+            except:
+                # input stream is empty
+                self.stream = None
+        if self.stream is None:
+            self.stream = GeneratorStream(
+                generator=self.generator,
+                gen_kwargs=self.gen_kwargs,
+                caching=self.caching,
+                copying=self.copying,
+            )
 
     def __iter__(self):
-        self._verify_init()
         return self.stream.__iter__()
 
     def peek(self):
-        self._verify_init()
         return self.stream.peek()
 
     def take(self, n):
-        self._verify_init()
         return self.stream.take(n)
 
 
--- tests/library/test_recipe.py
+++ tests/library/test_recipe.py
@@ -126,16 +126,11 @@ class TestRecipes(UnitxtTestCase):
             recipe.production_demos_pool(), recipe.production_demos_pool()
         )
 
-        i1 = recipe.production_preprocess(instances)[0]
-        del i1["recipe_metadata"]
-        i2 = recipe.production_preprocess(instances)[0]
-        del i2["recipe_metadata"]
         self.assertDictEqual(
-            i1,
-            i2,
-            # recipe.production_preprocess(instances)[0],
-            # recipe.production_preprocess(instances)[0],
+            recipe.production_preprocess(instances)[0],
+            recipe.production_preprocess(instances)[0],
         )
+
         self.assertDictEqual(
             recipe.produce(instances)[0],
             recipe.produce(instances)[0],
