65552c99f0fed9b8135b5f686d53f77eb67fc703
Elron Bandel <elron.bandel@ibm.com>
2024-06-06 13:40:03 +0300

    Touch up numeric nlg
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- prepare/cards/numeric_nlg.py
+++ prepare/cards/numeric_nlg.py
@@ -7,17 +7,15 @@ from unitxt.blocks import (
     TaskCard,
 )
 from unitxt.catalog import add_to_catalog
-from unitxt.splitters import SplitRandomMix
 from unitxt.test_utils.card import test_card
 
 card = TaskCard(
     loader=LoadHF(path="kasnerz/numericnlg"),  # TODO: load from github repo
     preprocess_steps=[
-        SplitRandomMix({"train": "train", "validation": "validation", "test": "test"}),
         AddFields(fields={"type_of_input": "table", "type_of_output": "description"}),
-        MapHTMLTableToJSON(field_to_field=[["table_html_clean", "table_out"]]),
-        SerializeTableAsMarkdown(field_to_field=[["table_out", "input"]]),
-        RenameFields(field_to_field={"description": "output"}),
+        MapHTMLTableToJSON(field="table_html_clean", to_field="table_out"),
+        SerializeTableAsMarkdown(field="table_out", to_field="input"),
+        RenameFields(field="description", to_field="output"),
     ],
     task="tasks.generation[metrics=[metrics.bleu,metrics.rouge,metrics.bert_score.bert_base_uncased,metrics.meteor]]",
     templates="templates.generation.all",
