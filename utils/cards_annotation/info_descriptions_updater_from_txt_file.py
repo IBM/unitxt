51683db1b9c00d1564355a70061a76761e157605
Elron Bandel <elronbandel@gmail.com>
2024-06-09 14:39:08 +0300

    Fix demo and organize files (#897)
    * Reorganize files and fix demo display
    
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>
    
    * Updates
    
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>
    
    ---------
    
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- utils/cards_annotation/info_descriptions_updater_from_txt_file.py
+++ utils/cards_annotation/info_descriptions_updater_from_txt_file.py
@@ -35,8 +35,8 @@ import logging
 import os
 import re
 
-from ..logging_utils import get_logger
-from ..text_utils import lines_defining_obj_in_card
+from unitxt.logging_utils import get_logger
+from unitxt.text_utils import lines_defining_obj_in_card
 
 logger = get_logger()
 logger.setLevel(logging.INFO)
