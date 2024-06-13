7163dbd9b14529f6220c964a7907246d92c6fbce
ofirarviv <ofir.arviv@ibm.com>
2024-06-13 15:31:39 +0300

    add multi turn rag task and template

---
+++ prepare/tasks/rag/response_generation_multi_turn.py
@@ -0,0 +1,23 @@
+from unitxt import add_to_catalog
+from unitxt.blocks import (
+    Task,
+)
+
+add_to_catalog(
+    Task(
+        inputs={
+            "contexts": "List[str]",
+            "contexts_ids": "List[int]",
+            "dialog": "List[Dict]",
+        },
+        outputs={"reference_answers": "List[str]"},
+        metrics=[
+            "metrics.rag.response_generation.correctness.token_overlap",
+            "metrics.rag.response_generation.faithfullness.token_overlap",
+            "metrics.rag.response_generation.correctness.bert_score.deberta_large_mnli",
+        ],
+        augmentable_inputs=["contexts"],
+    ),
+    "tasks.rag.response_generation.multi_turn",
+    overwrite=True,
+)
