005180a6016ab45738e84a97c4f5bb8a29f3d87b
michal <shmueli@il.ibm.com>
2024-06-21 15:02:13 +0300

    added instruction to classification and summarization
    Signed-off-by: Yotam Perlitz <yotam.perlitz@ibm.com>

--- prepare/templates/summarization/abstractive.py
+++ prepare/templates/summarization/abstractive.py
@@ -1,5 +1,8 @@
 from unitxt.catalog import add_to_catalog
-from unitxt.templates import InputOutputTemplate, TemplatesList
+from unitxt.templates import (
+    InputOutputTemplate,
+    TemplatesList,
+)
 
 add_to_catalog(
     InputOutputTemplate(
@@ -7,6 +10,16 @@ add_to_catalog(
         output_format="{summary}",
     ),
     "templates.summarization.abstractive.full",
+    overwrite=True,
+)
+
+add_to_catalog(
+    InputOutputTemplate(
+        instruction="Summarize the following {document_type}.",
+        input_format="{document_type}:\n{document}\nSummary:\n",
+        output_format="{summary}",
+    ),
+    "templates.summarization.abstractive.instruct_full",
     overwrite=True,
 )
 
@@ -24,6 +37,20 @@ add_to_catalog(
 
 add_to_catalog(
     InputOutputTemplate(
+        instruction="Summarize the following text into one sentence.",
+        input_format="Text:\n{document}\nSummary:\n",
+        output_format="{summary}",
+        postprocessors=[
+            "processors.take_first_non_empty_line",
+        ],
+    ),
+    "templates.summarization.abstractive.instruct_one_sentence",
+    overwrite=True,
+)
+
+
+add_to_catalog(
+    InputOutputTemplate(
         input_format="The following {document_type} is to be summarized into one sentence: {document}.",
         output_format="{summary}",
         postprocessors=[
@@ -36,12 +63,36 @@ add_to_catalog(
 
 add_to_catalog(
     InputOutputTemplate(
+        instruction="The following {document_type} is to be summarized into one sentence.",
+        input_format="{document_type}:\n{document}Summary:\n",
+        output_format="{summary}",
+        postprocessors=[
+            "processors.take_first_non_empty_line",
+        ],
+    ),
+    "templates.summarization.abstractive.instruct_passive",
+    overwrite=True,
+)
+
+add_to_catalog(
+    InputOutputTemplate(
         input_format="Write a succinct summary of the following {document_type}: {document}.",
         output_format="{summary}",
     ),
     "templates.summarization.abstractive.write_succinct",
     overwrite=True,
 )
+
+add_to_catalog(
+    InputOutputTemplate(
+        instruction="Write a succinct summary of the following {document_type}.",
+        input_format="{document_type}:\n{document}Summary:\n",
+        output_format="{summary}",
+    ),
+    "templates.summarization.abstractive.instruct_write_succinct",
+    overwrite=True,
+)
+
 
 add_to_catalog(
     InputOutputTemplate(
@@ -106,6 +157,16 @@ add_to_catalog(
 )
 
 add_to_catalog(
+    InputOutputTemplate(
+        instruction="TL;DR:",
+        input_format="{document}",
+    ),
+    "templates.summarization.abstractive.instruct_tldr",
+    overwrite=True,
+)
+
+
+add_to_catalog(
     TemplatesList(
         [
             "templates.summarization.abstractive.formal",
@@ -118,6 +179,11 @@ add_to_catalog(
             "templates.summarization.abstractive.passive",
             "templates.summarization.abstractive.write_succinct",
             "templates.summarization.abstractive.title",
+            "templates.summarization.abstractive.instruct_full",
+            "templates.summarization.abstractive.instruct_one_sentence",
+            "templates.summarization.abstractive.instruct_passive",
+            "templates.summarization.abstractive.instruct_write_succinct",
+            "templates.summarization.abstractive.instruct_tldr",
         ]
     ),
     "templates.summarization.abstractive.all",
