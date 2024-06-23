97bfbe0b4b59451ab72ee6095641ee74a679f954
ofirarviv <ofir.arviv@ibm.com>
2024-06-23 10:38:35 +0300

    arena hard tasks and cards

---
+++ prepare/cards/arena_hard/response_assessment/pairwise_comparative_rating/both_games_mean_judgment_gpt4_judge.py
@@ -0,0 +1,69 @@
+from unitxt.blocks import (
+    TaskCard,
+)
+from unitxt.catalog import add_to_catalog
+from unitxt.loaders import LoadFromHFSpace
+from unitxt.operators import (
+    ExecuteExpression,
+    MapInstanceValues,
+    RenameFields,
+)
+from unitxt.test_utils.card import test_card
+
+score_mapper = {"A=B": 0, "A>B": 1, "A>>B": 3, "B>A": -1, "B>>A": -3}
+
+score_mapper_reversed = {k: -1 * v for k, v in score_mapper.items()}
+
+score_to_label_mapper = {
+    "0": "A=B",
+    "1": "A>B",
+    "2": "A>>B",
+    "3": "A>>B",
+    "-1": "B>A",
+    "-2": "B>>A",
+    "-3": "B>>A",
+}
+
+card = TaskCard(
+    loader=LoadFromHFSpace(
+        space_name="lmsys/arena-hard-browser",
+        revision="03b91ca",  # May 26, 2024
+        data_files={
+            "questions": "data/arena-hard-v0.1/question.jsonl",
+            "model_answer": "data/arena-hard-v0.1/model_answer/*.jsonl",
+            "judgment": "data/arena-hard-v0.1/model_judgment/gpt-4-1106-preview/*.jsonl",
+        },
+    ),
+    data_classification_policy=["public"],
+    preprocess_steps=[
+        "operators.arena_hard_hf_space_processing_steps",
+        MapInstanceValues(
+            {
+                "score_model_1_ordered_first": score_mapper,
+                "score_model_2_ordered_first": score_mapper_reversed,
+            }
+        ),
+        ExecuteExpression(
+            to_field="winner",
+            expression="int(round((score_model_1_ordered_first+score_model_2_ordered_first)/2))",
+        ),
+        MapInstanceValues(mappers={"winner": score_to_label_mapper}),
+        RenameFields(
+            field_to_field={
+                "model_input": "question",
+                "model_1_output": "answer_a",
+                "model_2_output": "answer_b",
+                "category": "group",
+            }
+        ),
+    ],
+    task="tasks.response_assessment.pairwise_comparative_rating.single_turn",
+    templates=["templates.response_assessment.pairwise_comparative_rating.arena_hard"],
+)
+
+test_card(card, demos_taken_from="test", strict=False, loader_limit=100000)
+add_to_catalog(
+    card,
+    "cards.arena_hard.response_assessment.pairwise_comparative_rating.both_games_mean_judgment_gpt4_judge",
+    overwrite=True,
+)
---
+++ prepare/cards/arena_hard/response_assessment/pairwise_comparative_rating/first_game_only_gpt4_judge.py
@@ -0,0 +1,43 @@
+from unitxt.blocks import (
+    TaskCard,
+)
+from unitxt.catalog import add_to_catalog
+from unitxt.loaders import LoadFromHFSpace
+from unitxt.operators import (
+    RenameFields,
+)
+from unitxt.test_utils.card import test_card
+
+card = TaskCard(
+    loader=LoadFromHFSpace(
+        space_name="lmsys/arena-hard-browser",
+        revision="03b91ca",  # May 26, 2024
+        data_files={
+            "questions": "data/arena-hard-v0.1/question.jsonl",
+            "model_answer": "data/arena-hard-v0.1/model_answer/*.jsonl",
+            "judgment": "data/arena-hard-v0.1/model_judgment/gpt-4-1106-preview/*.jsonl",
+        },
+    ),
+    data_classification_policy=["public"],
+    preprocess_steps=[
+        "operators.arena_hard_hf_space_processing_steps",
+        RenameFields(
+            field_to_field={
+                "model_input": "question",
+                "model_1_output": "answer_a",
+                "model_2_output": "answer_b",
+                "score_model_1_ordered_first": "winner",
+                "category": "group",
+            }
+        ),
+    ],
+    task="tasks.response_assessment.pairwise_comparative_rating.single_turn",
+    templates=["templates.response_assessment.pairwise_comparative_rating.arena_hard"],
+)
+
+test_card(card, demos_taken_from="test", strict=False, loader_limit=100000)
+add_to_catalog(
+    card,
+    "cards.arena_hard.response_assessment.pairwise_comparative_rating.first_game_only_gpt_4_judge",
+    overwrite=True,
+)
