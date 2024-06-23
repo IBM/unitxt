62117239c01ea0cc0cbe55b2092aaa38c12b5262
Yotam Perlitz <yotam.perlitz@ibm.com>
2024-06-22 18:41:28 -0600

    add response generation templates
    Signed-off-by: Yotam Perlitz <yotam.perlitz@ibm.com>

--- prepare/templates/rag/response_generation.py
+++ prepare/templates/rag/response_generation.py
@@ -10,6 +10,28 @@ add_to_catalog(
         target_prefix="Response:",
         references_field="reference_answers",
     ),
-    "templates.rag.response_generation.simple",
+    "templates.rag.response_generation.please_respond",
+    overwrite=True,
+)
+
+add_to_catalog(
+    MultiReferenceTemplate(
+        instruction="Answer the question, basing your answer on the context",
+        input_format="Context: {contexts}\nQuestion: {question}.\n",
+        target_prefix="Answer:",
+        references_field="reference_answers",
+    ),
+    "templates.rag.response_generation.answer_based_on_context",
+    overwrite=True,
+)
+
+add_to_catalog(
+    MultiReferenceTemplate(
+        instruction="Answer the question, basing your answer on the context",
+        input_format="Question: {question}.\nContext: {contexts}\n",
+        target_prefix="Answer:",
+        references_field="reference_answers",
+    ),
+    "templates.rag.response_generation.answer_based_on_context_inverted",
     overwrite=True,
 )
