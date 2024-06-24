dee74fe3649e26e4a26d2c439ad101f3751ee008
ofirarviv <ofir.arviv@ibm.com>
2024-06-24 21:15:16 +0300

    update jsons

--- prepare/metrics/win_rate.py
+++ prepare/metrics/win_rate.py
@@ -1,8 +1,5 @@
 from unitxt.catalog import add_to_catalog
-from unitxt.metrics import WeightedWinRate, WeightedWinRateCorrelation
+from unitxt.metrics import WeightedWinRateCorrelation
 
 metric = WeightedWinRateCorrelation()
 add_to_catalog(metric, "metrics.weighted_win_rate_correlation", overwrite=True)
-
-metric = WeightedWinRate()
-add_to_catalog(metric, "metrics.weighted_win_rate", overwrite=True)
