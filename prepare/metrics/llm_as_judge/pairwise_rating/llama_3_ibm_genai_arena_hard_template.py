684c087da427f03873cdb506a5e260abf9b11faf
ofirarviv <ofir.arviv@ibm.com>
2024-06-26 11:49:20 +0300

    fix

--- prepare/metrics/llm_as_judge/pairwise_rating/llama_3_ibm_genai_arena_hard_template.py
+++ prepare/metrics/llm_as_judge/pairwise_rating/llama_3_ibm_genai_arena_hard_template.py
@@ -7,28 +7,32 @@ from unitxt.llm_as_judge import LLMAsJudge
 
 model_list = ["meta-llama/llama-3-8b-instruct", "meta-llama/llama-3-70b-instruct"]
 format = "formats.llama3_chat"
-template = "templates.response_assessment.pairwise_comparative_rating.arena_hard"
-task = "pairwise_comparative_rating.single_turn"
+templates = [
+    "templates.response_assessment.pairwise_comparative_rating.arena_hard",
+    "templates.response_assessment.pairwise_comparative_rating.arena_hard_with_shuffling",
+]
+for template in templates:
+    task = "pairwise_comparative_rating.single_turn"
 
-gen_params = IbmGenAiInferenceEngineParams(max_new_tokens=1024)
-for model_id in model_list:
-    inference_model = IbmGenAiInferenceEngine(
-        model_name=model_id, parameters=gen_params
-    )
-    model_label = model_id.split("/")[1].replace("-", "_").replace(".", ",").lower()
-    model_label = f"{model_label}_ibm_genai"
-    template_label = template.split(".")[-1]
-    metric_label = f"{model_label}_template_{template_label}"
-    metric = LLMAsJudge(
-        inference_model=inference_model,
-        template=template,
-        task=task,
-        format=format,
-        main_score=metric_label,
-    )
+    gen_params = IbmGenAiInferenceEngineParams(max_new_tokens=1024)
+    for model_id in model_list:
+        inference_model = IbmGenAiInferenceEngine(
+            model_name=model_id, parameters=gen_params
+        )
+        model_label = model_id.split("/")[1].replace("-", "_").replace(".", ",").lower()
+        model_label = f"{model_label}_ibm_genai"
+        template_label = template.split(".")[-1]
+        metric_label = f"{model_label}_template_{template_label}"
+        metric = LLMAsJudge(
+            inference_model=inference_model,
+            template=template,
+            task=task,
+            format=format,
+            main_score=metric_label,
+        )
 
-    add_to_catalog(
-        metric,
-        f"metrics.llm_as_judge.pairwise_comparative_rating.{model_label}_template_{template_label}",
-        overwrite=True,
-    )
+        add_to_catalog(
+            metric,
+            f"metrics.llm_as_judge.pairwise_comparative_rating.{model_label}_template_{template_label}",
+            overwrite=True,
+        )
