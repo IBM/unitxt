e98db0482765a0d6eb5e9c8d0f2d0acf6907e71a
Paweł Knes <pawel.knes@ibm.com>
2024-06-28 10:14:28 +0200

    Added support for ibm-watsonx-ai inference
    Signed-off-by: Paweł Knes <pawel.knes@ibm.com>

---
+++ examples/inference_using_ibm_watsonx_ai.py
@@ -0,0 +1,42 @@
+import os
+
+from unitxt.api import load_dataset
+from unitxt.inference import WMLInferenceEngine, WMLInferenceEngineParams
+from unitxt.text_utils import print_dict
+
+if __name__ == "__main__":
+    # Set required env variables using your WML credentials:
+    os.environ["WML_URL"] = ""
+    os.environ["WML_PROJECT_ID"] = ""
+    os.environ["WML_APIKEY"] = ""
+
+    # Preparing WML inference engine:
+    wml_parameters = WMLInferenceEngineParams(
+        random_seed=111,
+        min_new_tokens=16,
+        max_new_tokens=128,
+        top_p=0.5,
+        top_k=1,
+        repetition_penalty=1.5,
+        decoding_method="greedy",
+    )
+    model_name = "google/flan-t5-xl"
+    wml_inference = WMLInferenceEngine(
+        model_name=model_name,
+        parameters=wml_parameters,
+        data_classification_policy=["public"],
+    )
+
+    # Loading dataset:
+    dataset = load_dataset(
+        card="cards.go_emotions.simplified",
+        template="templates.classification.multi_label.empty",
+        loader_limit=3,
+    )
+    test_data = dataset["test"]
+
+    # Performing inference:
+    predictions = wml_inference.infer(test_data)
+    for inp, prediction in zip(test_data, predictions):
+        result = {**inp, "output": prediction}
+        print_dict(result, keys_to_print=["source", "output"])
