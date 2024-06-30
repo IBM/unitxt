fe4400f40fef7892b974636d75d65b6e27547be7
ShirApp <58909189+ShirApp@users.noreply.github.com>
2024-06-30 10:46:42 +0300

    Update wikitq, tab_fact taskcards (#963)
    Co-authored-by: Rajmohan <rajmohanc1@in.ibm.com>
    Co-authored-by: Elron Bandel <elronbandel@gmail.com>

--- prepare/processors/to_list_by_comma.py
+++ prepare/processors/to_list_by_comma.py
@@ -1,6 +1,6 @@
 from unitxt import add_to_catalog
 from unitxt.operator import SequentialOperator
-from unitxt.processors import ToListByComma
+from unitxt.processors import ToListByComma, ToListByCommaSpace
 
 add_to_catalog(
     SequentialOperator(
@@ -16,5 +16,16 @@ add_to_catalog(
 add_to_catalog(
     ToListByComma(field="references", process_every_value=True),
     "processors.to_list_by_comma_from_references",
+    overwrite=True,
+)
+
+add_to_catalog(
+    SequentialOperator(
+        steps=[
+            ToListByCommaSpace(field="prediction", process_every_value=False),
+            ToListByCommaSpace(field="references", process_every_value=True),
+        ]
+    ),
+    "processors.to_list_by_comma_space",
     overwrite=True,
 )
