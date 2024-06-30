424ca148e70504ec7ced940c2bbdc04e4546a912
Yoav Katz <katz@il.ibm.com>
2024-06-30 14:16:49 +0300

    Updated evaluation to use more examples
    and show confidence internvals
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- examples/evaluate_different_formats.py
+++ examples/evaluate_different_formats.py
@@ -6,11 +6,13 @@ from unitxt.text_utils import print_dict
 logger = get_logger()
 
 
-model_name = "meta-llama/llama-3-70b-instruct"
+model_name = "meta-llama/llama-3-8b-instruct"
 gen_params = IbmGenAiInferenceEngineParams(max_new_tokens=32)
 inference_model = IbmGenAiInferenceEngine(model_name=model_name, parameters=gen_params)
-card = "cards.boolq"
-template = "templates.classification.multi_class.title"
+card = "cards.boolq.classification"
+template = "templates.classification.multi_class.relation.default"
+
+all_scores = {}
 for format in [
     "formats.llama3_instruct",
     "formats.empty",
@@ -22,9 +24,10 @@ for format in [
             template=template,
             format=format,
             system_prompt=system_prompt,
-            num_demos=4,
+            num_demos=2,
             demos_pool_size=100,
-            loader_limit=200,
+            loader_limit=1000,
+            max_test_instances=300,
         )
 
         test_dataset = dataset["test"]
@@ -33,13 +36,25 @@ for format in [
         evaluated_dataset = evaluate(predictions=predictions, data=test_dataset)
 
         logger.info(
-            f"Results for format '{format}' and system prompt '{system_prompt}':"
+            f"Sample input and output for format '{format}' and system prompt '{system_prompt}':"
         )
         print_dict(
             evaluated_dataset[0],
             keys_to_print=[
                 "source",
                 "prediction",
-                "score",
             ],
         )
+        global_scores = evaluated_dataset[0]["score"]["global"]
+        print_dict(
+            global_scores,
+            keys_to_print=["score_name", "score", "score_ci_low", "score_ci_high"],
+        )
+        all_scores[(format, system_prompt)] = global_scores
+
+
+for (format, system_prompt), global_scores in all_scores.items():
+    logger.info(f"**** score for format '{format}' and system prompt '{system_prompt}'")
+    logger.info(
+        f"**** {global_scores['score_name']} : {global_scores['score']} - 95% confidence internal [{global_scores['score_ci_low']},{global_scores['score_ci_high']}]"
+    )
