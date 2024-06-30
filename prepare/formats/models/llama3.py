5d9620616f5ae930de6b1c82130bca3910ad9331
Oktie <oktie@oktie.com>
2024-06-30 08:49:15 -0400

    llama3 instruct and chat system prompts (#950)
    * llama3 instruct and chat system prompts
    
    * fixing llama3_chat prompt + adding jsons
    
    * fixing llama3 formats + a boolqa system prompt
    
    * Start of example to check different formats.
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    * Added instructions to llama3 model.
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    * replacing llama3_chat with llama3_instruct + 2 alts
    
    * Added example for multiple formats
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    * prepare artifcats (whitespace changes)
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    * Updated evaluation to use more examples
    
    and show confidence internvals
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    * Do not run examples that require
    large model inference
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    * Seperated examples in example table
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    * Fixed test_examples to skip some files.
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    
    ---------
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    Co-authored-by: Yoav Katz <katz@il.ibm.com>

--- prepare/formats/models/llama3.py
+++ prepare/formats/models/llama3.py
@@ -2,25 +2,43 @@ from unitxt.catalog import add_to_catalog
 from unitxt.formats import SystemFormat
 
 # see: https://llama.meta.com/docs/model-cards-and-prompt-formats/meta-llama-3/
+# According to: https://huggingface.co/blog/llama3#how-to-prompt-llama-3
+# The Instruct versions use the following conversation structure:
 # <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+#
 # {{ system_prompt }}<|eot_id|><|start_header_id|>user<|end_header_id|>
-# {{ user_message }}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+#
+# {{ user_msg_1 }}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+#
+# {{ model_answer_1 }}<|eot_id|>
+
+format = SystemFormat(
+    demo_format="<|start_header_id|>user<|end_header_id|>\n\n"
+    "{source}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+    "{target_prefix}{target}<|eot_id|>",
+    model_input_format="<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+    + "{system_prompt}{instruction}"
+    + "<|eot_id|>{demos}<|start_header_id|>user<|end_header_id|>\n\n"
+    "{source}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n{target_prefix}",
+)
+
+add_to_catalog(
+    format,
+    "formats.llama3_instruct",
+    overwrite=True,
+)
 
 format = SystemFormat(
     demo_format="{source}\n\n{target_prefix}{target}\n\n",
-    model_input_format="<|begin_of_text|><|eot_id|><|start_header_id|>user<|end_header_id|>\n\n"
-    "{instruction}\\N{demos}{source}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
-    "{target_prefix}",
+    model_input_format="<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+    "{system_prompt}{instruction}"
+    "<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n"
+    "{demos}"
+    "{source}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n{target_prefix}",
 )
 
-add_to_catalog(format, "formats.llama3_chat", overwrite=True)
-
-format = SystemFormat(
-    demo_format="{source}\n\n{target_prefix}{target}\n\n",
-    model_input_format="<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n"
-    "{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n"
-    "{instruction}{demos}{source}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n"
-    "{target_prefix}",
+add_to_catalog(
+    format,
+    "formats.llama3_instruct_all_demos_in_one_turn",
+    overwrite=True,
 )
-
-add_to_catalog(format, "formats.llama3_chat_with_system_prompt", overwrite=True)
