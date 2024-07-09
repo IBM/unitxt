c93ce43f0d4066c413a11d0a23d274c6d799327e
Yoav Katz <katz@il.ibm.com>
2024-07-09 12:23:10 +0300

    Remove confidence interval calculation for meteor metric by default
    added a new metric with interval calculations
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/metrics/meteor.py
+++ prepare/metrics/meteor.py
@@ -2,7 +2,16 @@ from unitxt import add_to_catalog
 from unitxt.metrics import HuggingfaceMetric
 
 metric = HuggingfaceMetric(
-    hf_metric_name="meteor", main_score="meteor", prediction_type="str"
+    hf_metric_name="meteor",
+    main_score="meteor",
+    prediction_type="str",
+    n_resamples=None,
 )
 
 add_to_catalog(metric, "metrics.meteor", overwrite=True)
+
+metric = HuggingfaceMetric(
+    hf_metric_name="meteor", main_score="meteor", prediction_type="str"
+)
+
+add_to_catalog(metric, "metrics.meteor_with_confidence_intervals", overwrite=True)
