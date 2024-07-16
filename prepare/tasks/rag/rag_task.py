2019dea27d90d518190ee22dca3832fbbebe93fe
Assaf Toledo <assaf.toledo@ibm.com>
2024-07-16 18:20:24 +0300

    adding tasks to catalog

--- prepare/tasks/rag/rag_task.py
+++ prepare/tasks/rag/rag_task.py
@@ -3,73 +3,45 @@ from unitxt.blocks import (
 )
 from unitxt.catalog import add_to_catalog
 
-from prepare.tasks.rag.rag_constants import (
-    TaskRagCorporaConstants,
-    TaskRagCorporaInputConstants,
-    TaskRagEndToEndConstants,
-    TaskRagEndToEndInputConstants,
-    TaskRagEndToEndReferenceConstants,
-    TaskRagResponseGenConstants,
+add_to_catalog(
+    Task(
+        inputs={
+            "question": "str",
+            "question_id": "Any",
+            "metadata_field": "str",
+        },
+        outputs={
+            "reference_answers": "list[str]",
+            "reference_contexts": "list[str]",
+            "reference_context_ids": "list[str|int]",
+            "is_answerable_label": "bool",
+        },
+        metrics=[
+            "metrics.rag.answer_correctness",
+            "metrics.rag.faithfulness",
+            "metrics.rag.answer_reward",
+            "metrics.rag.context_correctness",
+            "metrics.rag.context_relevance",
+        ],
+        prediction_type="dict",
+        augmentable_inputs=["question"],
+    ),
+    name="tasks.rag.end_to_end",
+    overwrite=True,
 )
 
-rag_response_generation_task = Task(
-    inputs={
-        TaskRagResponseGenConstants.CONTEXTS: "List[str]",
-        TaskRagResponseGenConstants.CONTEXT_IDS: "List[int]",
-        TaskRagResponseGenConstants.QUESTION: "str",
-    },
-    outputs={TaskRagResponseGenConstants.REFERENCE_ANSWERS: "List[str]"},
-    metrics=[
-        TaskRagResponseGenConstants.METRICS_ANSWER_CORRECTNESS,
-        TaskRagResponseGenConstants.METRICS_ANSWER_FAITHFULNESS,
-        TaskRagResponseGenConstants.METRICS_ANSWER_CORRECTNESS_BERT,
-    ],
-    augmentable_inputs=[
-        TaskRagResponseGenConstants.CONTEXTS,
-        TaskRagResponseGenConstants.QUESTION,
-    ],
+
+add_to_catalog(
+    Task(
+        inputs={
+            "document_id": "str",
+            "title": "str",
+            "passages": "List[str]",
+            "metadata_field": "str",
+        },
+        outputs=[],
+        metrics=["metrics.rouge"],
+    ),
+    name="tasks.rag.corpora",
+    overwrite=True,
 )
-
-rag_end_to_end_task = Task(
-    inputs={
-        TaskRagEndToEndInputConstants.QUESTION: "str",
-        TaskRagEndToEndInputConstants.QUESTION_ID: "Any",
-        TaskRagEndToEndInputConstants.METADATA_FIELD: "str",
-    },
-    outputs={
-        TaskRagEndToEndReferenceConstants.REFERENCE_ANSWERS: "list[str]",
-        TaskRagEndToEndReferenceConstants.REFERENCE_CONTEXTS: "list[str]",
-        TaskRagEndToEndReferenceConstants.REFERENCE_CONTEXT_IDS: "list[str|int]",
-        TaskRagEndToEndReferenceConstants.IS_ANSWERABLE_LABEL: "bool",
-    },
-    metrics=[
-        TaskRagEndToEndConstants.METRICS_RAG_END_TO_END_ROUGE,
-        TaskRagEndToEndConstants.METRICS_RAG_END_TO_END_ANSWER_CORRECTNESS,
-        TaskRagEndToEndConstants.METRICS_RAG_END_TO_END_ANSWER_FAITHFULNESS,
-        TaskRagEndToEndConstants.METRICS_RAG_END_TO_END_ANSWER_REWARD,
-        TaskRagEndToEndConstants.METRICS_RAG_END_TO_END_CONTEXT_CORRECTNESS,
-        TaskRagEndToEndConstants.METRICS_RAG_END_TO_END_CONTEXT_RELEVANCE,
-    ],
-    prediction_type="dict",
-    augmentable_inputs=[TaskRagEndToEndInputConstants.QUESTION],
-)
-
-rag_corpora_task = Task(
-    inputs={
-        TaskRagCorporaInputConstants.DOCUMENT_ID: "str",
-        TaskRagCorporaInputConstants.TITLE: "str",
-        TaskRagCorporaInputConstants.PASSAGES: "List[str]",
-        TaskRagCorporaInputConstants.METADATA_FIELD: "str",
-    },
-    outputs=[],
-    metrics=["metrics.rouge"],
-)
-
-artifact_name = {
-    rag_response_generation_task: TaskRagResponseGenConstants.TASKS_RAG_RESPONSE_GENERATION,
-    rag_end_to_end_task: TaskRagEndToEndConstants.TASKS_RAG_END_TO_END,
-    rag_corpora_task: TaskRagCorporaConstants.TASKS_RAG_CORPORA,
-}
-
-for artifact, name in artifact_name.items():
-    add_to_catalog(artifact=artifact, name=name)
