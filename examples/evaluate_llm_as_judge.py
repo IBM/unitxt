5f16d0f480c369a0eecb30083b3ee74957ff6120
Yoav Katz <68273864+yoavkatz@users.noreply.github.com>
2024-07-17 09:09:08 +0300

    Update evaluate_llm_as_judge.py

--- examples/evaluate_llm_as_judge.py
+++ examples/evaluate_llm_as_judge.py
@@ -9,16 +9,16 @@ logger = get_logger()
 # This example demonstrates how to evaluate the quality of LLM as judge
 # on a task by using the gold references of a dataset.
 
-# It checks two llama3 based judges - one based on a 8b model and one on a 70b model on
+# It checks two llama3 based judges - one based on a 8b model and one on a 70b model on a
 # summarization dataset.
 #
-# The results indicate that the 8b model gives a higher score to a wrong prediction vs the correct
-# prediction in 20% of the examples, and gives a truncated corrected prediction higher score than
-# the correct prediction 35% of examples.  This means it is not so good as judge for this task.
+# The results indicate that the 8b model gives a higher score to a wrong prediction over the correct
+# prediction in 20% of the examples, and gives a truncated corrected prediction a higher score than
+# the correct prediction in 35% of examples.  This means it is not so good as a judge for this task.
 #
 # On the other hand the 70b model is better.  It always gives a higher score for the correct prediction,
-# and in only 5% of the cases gives the the truncated prediction a higher score.
-# Note that even the 70b, gives relatively low average score for correct predictions (0.395 +/ 0.17)
+# and in only 5% of the cases it gives the truncated prediction a higher score.
+# Note that even the 70b model gives relatively low average score for correct predictions (0.395 +/ 0.17)
 
 # List of metrics to evaluate
 metrics_to_check = [
@@ -27,7 +27,7 @@ metrics_to_check = [
 ]
 
 for metric_to_check in metrics_to_check:
-    # The dataset used to evaluate the metrics based on it's gold answers
+    # The dataset used to evaluate the metrics based on its gold answers
     dataset = load_dataset(
         card="cards.xsum",
         template="templates.summarization.abstractive.formal",
@@ -38,7 +38,7 @@ for metric_to_check in metrics_to_check:
 
     # Prepare three sets of predictions :
     # 1. the correct predictions taken from the gold answer
-    # 2. wrong predictions (where the prediction is the gold answer of the previous question)
+    # 2. wrong predictions (where the prediction is the gold answer of another question)
     # 3. truncated predictions taken as the first half of the gold answer
     correct_predictions = test_dataset["target"]
     wrong_predictions = [correct_predictions[-1]]
@@ -69,10 +69,11 @@ for metric_to_check in metrics_to_check:
         for i in range(len(truncated_predictions))
     ]
 
-    # Print the scores of the metric on each type of prediction
-    # The score of correct predictions should be close to 1 with low std
-    # The score of wrong predictions should be close to 0 with low std
+    # Print the scores of the metric on each type of prediction.
+    # The score of correct predictions should be close to 1 with low standard deviation
+    # The score of wrong predictions should be close to 0 with low standard deviation
     # The score of the truncated prediction, should be between the values.
+    # Also prints the precent of examples the wrong / truncated prediction get a higher score than the correct prediction.
 
     logger.info(f"Meta evaluation of metric: {metric_to_check}")
     logger.info(f"Scores of correct predictions: {correct_prediction_scores}")
