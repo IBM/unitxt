5955dfad07184eba87510974167c6a4b2b09eb5a
ofirarviv <ofir.arviv@ibm.com>
2024-07-21 12:28:28 +0300

    update to new params usage

--- prepare/metrics/llm_as_judge/pairwise_rating/llama_3_ibm_genai_arena_hard_template.py
+++ prepare/metrics/llm_as_judge/pairwise_rating/llama_3_ibm_genai_arena_hard_template.py
@@ -1,7 +1,6 @@
 from unitxt import add_to_catalog
 from unitxt.inference import (
     IbmGenAiInferenceEngine,
-    IbmGenAiInferenceEngineParams,
 )
 from unitxt.llm_as_judge import LLMAsJudge
 
@@ -14,10 +13,9 @@ templates = [
 for template in templates:
     task = "pairwise_comparative_rating.single_turn"
 
-    gen_params = IbmGenAiInferenceEngineParams(max_new_tokens=2048, random_seed=42)
     for model_id in model_list:
         inference_model = IbmGenAiInferenceEngine(
-            model_name=model_id, parameters=gen_params
+            model_name=model_id, max_new_tokens=2048, random_seed=42
         )
         model_label = model_id.split("/")[1].replace("-", "_").replace(".", ",").lower()
         model_label = f"{model_label}_ibm_genai"
