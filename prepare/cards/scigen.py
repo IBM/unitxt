a5e69027177ab6a8aec4e309514fa8892eb7c716
Rajmohan <rajmohanc1@in.ibm.com>
2024-07-22 17:58:57 +0530

    added llama3 llm_as_judge metric for table2text task

--- prepare/cards/scigen.py
+++ prepare/cards/scigen.py
@@ -23,9 +23,9 @@ card = TaskCard(
         Set({"type_of_output": "Text"}),
         RenameFields(field_to_field={"text": "output"}),
     ],
-    task="tasks.generation",
+    task="tasks.generation[metrics=[metrics.llm_as_judge.rating.llama_3_70b_instruct_ibm_genai_template_table2text_single_turn_with_reference]]",
     templates="templates.generation.all",
 )
 
-test_card(card)
+test_card(card, strict=False)
 add_to_catalog(card, "cards.scigen", overwrite=True)
