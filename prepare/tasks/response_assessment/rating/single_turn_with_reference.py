879e181a61f4cc22db0a25f2e3bc594b756f7ec7
Yoav Katz <katz@il.ibm.com>
2024-07-29 12:59:54 +0300

    Added prediction type to llm as jusdge to avoid warning
    Clarified the sandalone llm as judge example
    
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/tasks/response_assessment/rating/single_turn_with_reference.py
+++ prepare/tasks/response_assessment/rating/single_turn_with_reference.py
@@ -6,6 +6,7 @@ add_to_catalog(
         input_fields={"question": str, "answer": str, "reference_answer": str},
         reference_fields={"rating": float},
         metrics=["metrics.spearman"],
+        prediction_type=float,
     ),
     "tasks.response_assessment.rating.single_turn_with_reference",
     overwrite=True,
