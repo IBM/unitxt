66376fc05fb5c4bb403b8a56cb5177dcffb42357
Yotam Perlitz <y.perlitz@ibm.com>
2024-08-12 22:07:36 +0200

    add finqa template to catalog
    Signed-off-by: Yotam Perlitz <y.perlitz@ibm.com>

--- prepare/cards/fin_qa.py
+++ prepare/cards/fin_qa.py
@@ -2,7 +2,7 @@ from unitxt.blocks import (
     LoadHF,
     SerializeTableAsIndexedRowMajor,
     TaskCard,
-    TemplatesList,
+    TemplatesDict,
 )
 from unitxt.catalog import add_to_catalog
 from unitxt.operators import CopyFields, FilterByExpression
@@ -10,6 +10,35 @@ from unitxt.struct_data_operators import MapTableListsToStdTableJSON
 from unitxt.task import Task
 from unitxt.templates import InputOutputTemplate
 from unitxt.test_utils.card import test_card
+
+add_to_catalog(
+    InputOutputTemplate(
+        input_format="""Presented with a financial report consisting of textual contents and a structured table, given a question, generate the reasoning program in the domain specific language (DSL) that will be executed to get the answer. \nThe DSL consists of mathematical operations and table operations as executable programs. The program consists of a sequence of operations. Each operation takes a list of arguments. \nThere are 6 mathematical operations: add, subtract, multiply, divide, greater, exp, and 4 table aggregation operations table-max, table-min, table-sum, table-average, that apply aggregation operations on table rows. The mathematical operations take arguments of either numbers from the given reports, or a numerical result from a previous step.\nThe table operations take arguments of table row names. We use the special token #n to denote the result from the nth step. \nFor example, in the example "divide(9413, 20.01), divide(8249, 9.48), subtract(#0, #1)", the program consists of 3 steps; The first and the second division steps take arguments from the table and the text, respectively, then the third step subtracts the results from the two previous steps.
+                Definitions of all operations:
+                [["Name", "Arguments", "Output", "Description"],
+                ["add", "number1, number2", "number", "add two numbers: number1 + number2"],
+                ["subtract", "number1, number2", "number", "subtract two numbers: number1 - number2"],
+                ["multiply", "number1, number2", "number", "multiply two numbers: number1 * number2"],
+                ["divide", "number1, number2", "number", "multiply two numbers: number1 / number2"],
+                ["exp", "number1, number2", "number", "exponential: number1 ^ number2"],
+                ["greater", "number1, number2", "bool", "comparison: number1 > number2"],
+                ["table-sum", "table header", "number", "the summation of one table row"],
+                ["table-average", "table header", "number", "the average of one table row"],
+                ["table-max", "table header", "number", "the maximum number of one table row"],
+                ["table-min", "table header", "number", "the minimum number of one table row"]]
+                Answer with only the program, without any additional explanation.
+                Pre-table text: {pre_text}
+                Table: {serialized_table}
+                Post-table text: {post_text}
+                Question: {question}
+                Program:
+                    """,
+        output_format="{program_re}",
+        postprocessors=[],
+    ),
+    "templates.finqa.default",
+    overwrite=True,
+)
 
 card = TaskCard(
     loader=LoadHF(path="ibm/finqa", streaming=False),
@@ -34,33 +63,8 @@ card = TaskCard(
         metrics=["metrics.fin_qa_metric"],
         augmentable_inputs=["pre_text", "serialized_table", "post_text", "question"],
     ),
-    templates=TemplatesList(
-        [
-            InputOutputTemplate(
-                input_format="""Presented with a financial report consisting of textual contents and a structured table, given a question, generate the reasoning program in the domain specific language (DSL) that will be executed to get the answer. \nThe DSL consists of mathematical operations and table operations as executable programs. The program consists of a sequence of operations. Each operation takes a list of arguments. \nThere are 6 mathematical operations: add, subtract, multiply, divide, greater, exp, and 4 table aggregation operations table-max, table-min, table-sum, table-average, that apply aggregation operations on table rows. The mathematical operations take arguments of either numbers from the given reports, or a numerical result from a previous step.\nThe table operations take arguments of table row names. We use the special token #n to denote the result from the nth step. \nFor example, in the example "divide(9413, 20.01), divide(8249, 9.48), subtract(#0, #1)", the program consists of 3 steps; The first and the second division steps take arguments from the table and the text, respectively, then the third step subtracts the results from the two previous steps.
-                Definitions of all operations:
-                [["Name", "Arguments", "Output", "Description"],
-                ["add", "number1, number2", "number", "add two numbers: number1 + number2"],
-                ["subtract", "number1, number2", "number", "subtract two numbers: number1 - number2"],
-                ["multiply", "number1, number2", "number", "multiply two numbers: number1 * number2"],
-                ["divide", "number1, number2", "number", "multiply two numbers: number1 / number2"],
-                ["exp", "number1, number2", "number", "exponential: number1 ^ number2"],
-                ["greater", "number1, number2", "bool", "comparison: number1 > number2"],
-                ["table-sum", "table header", "number", "the summation of one table row"],
-                ["table-average", "table header", "number", "the average of one table row"],
-                ["table-max", "table header", "number", "the maximum number of one table row"],
-                ["table-min", "table header", "number", "the minimum number of one table row"]]
-                Answer with only the program, without any additional explanation.
-                Pre-table text: {pre_text}
-                Table: {serialized_table}
-                Post-table text: {post_text}
-                Question: {question}
-                Program:
-                    """,
-                output_format="{program_re}",
-                postprocessors=[],
-            ),
-        ]
+    templates=TemplatesDict(
+        {"default": "templates.finqa.default"},
     ),
     __description__=(
         "FINQA is an expert-annotated QA dataset that aims to tackle numerical reasoning over real-world "
