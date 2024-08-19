c3e0b9d6d0265e6cd3b40fedae6234105010110b
Jonathan Bnayahu <bnayahu@il.ibm.com>
2024-08-19 16:35:50 +0300

    Remove reference_fields hack from the card (under protest...)
    Signed-off-by: Jonathan Bnayahu <bnayahu@il.ibm.com>

--- prepare/cards/safety/provoq.py
+++ prepare/cards/safety/provoq.py
@@ -2,7 +2,6 @@ from unitxt import add_to_catalog
 from unitxt.blocks import (
     LoadHF,
     TaskCard,
-    TemplatesList,
 )
 from unitxt.operators import RenameFields, Set
 from unitxt.test_utils.card import test_card
@@ -10,14 +9,17 @@ from unitxt.test_utils.card import test_card
 card = TaskCard(
     loader=LoadHF(path="IBM/ProvoQ", data_classification_policy=["public"]),
     preprocess_steps=[
-        Set(fields={"output": "", "type_of_input": "", "type_of_output": ""}),
+        Set(
+            fields={
+                "output": "",
+                "type_of_input": "question",
+                "type_of_output": "answer",
+            }
+        ),
         RenameFields(field_to_field={"question": "input"}),
     ],
-    task="tasks.generation["
-    + "metrics=[metrics.llm_as_judge.safety.llama_3_70b_instruct_ibm_genai_template_harm_rating],"
-    + "reference_fields=[group, stigma, input, output]"
-    + "]",
-    templates=TemplatesList(["templates.generation.empty"]),
+    task="tasks.generation[metrics=[metrics.llm_as_judge.safety.llama_3_70b_instruct_ibm_genai_template_harm_rating]]",
+    templates="templates.generation.all",
     __description__="The ProvoQ dataset is designed to evaluate the sensitivity of large language models (LLMs) to stigma-related topics. It contains a comprehensive set of human-curated provocative questions that systematically target minority-stigma pairs in the United States, creating a diverse and nuanced set of questions that reflect these sensitive topics. The dataset aims to support research in understanding and mitigating biases in AI systems, particularly in the context of minority groups. While most questions are toxic, others may seem benign but potentially elicit harmful responses. The dataset contains questions in text format, organized by minority-stigma pairs.",
     __tags__={
         "languages": ["english"],
