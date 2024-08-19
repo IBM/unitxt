f4af8229667c3be04b9bfc2cbceb3d76930e48df
Jonathan Bnayahu <bnayahu@il.ibm.com>
2024-08-19 20:44:44 +0300

    Switched to a new task
    Signed-off-by: Jonathan Bnayahu <bnayahu@il.ibm.com>

--- prepare/cards/safety/provoq.py
+++ prepare/cards/safety/provoq.py
@@ -3,7 +3,8 @@ from unitxt.blocks import (
     LoadHF,
     TaskCard,
 )
-from unitxt.operators import RenameFields, Set
+from unitxt.operators import ListFieldValues, RenameFields, Set
+from unitxt.string_operators import Join
 from unitxt.test_utils.card import test_card
 
 card = TaskCard(
@@ -17,8 +18,10 @@ card = TaskCard(
             }
         ),
         RenameFields(field_to_field={"question": "input"}),
+        ListFieldValues(fields=["group", "stigma"], to_field="context"),
+        Join(field="context", by=" # "),
     ],
-    task="tasks.generation[metrics=[metrics.llm_as_judge.safety.llama_3_70b_instruct_ibm_genai_template_harm_rating]]",
+    task="tasks.generation.with_context[metrics=[metrics.llm_as_judge.safety.llama_3_70b_instruct_ibm_genai_template_harm_rating]]",
     templates="templates.generation.all",
     __description__="The ProvoQ dataset is designed to evaluate the sensitivity of large language models (LLMs) to stigma-related topics. It contains a comprehensive set of human-curated provocative questions that systematically target minority-stigma pairs in the United States, creating a diverse and nuanced set of questions that reflect these sensitive topics. The dataset aims to support research in understanding and mitigating biases in AI systems, particularly in the context of minority groups. While most questions are toxic, others may seem benign but potentially elicit harmful responses. The dataset contains questions in text format, organized by minority-stigma pairs.",
     __tags__={
