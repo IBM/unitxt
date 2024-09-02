33180fb8f0ca6a9039c99a4952d2f5611fc41dc1
elronbandel <elronbandel@gmail.com>
2024-09-02 13:04:23 +0300

    Add support for Literal, TypedDict and NewType for unitxt type checking
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- tests/library/test_utils.py
+++ tests/library/test_utils.py
@@ -1,4 +1,8 @@
-from unitxt.utils import is_module_available, is_package_installed
+from unitxt.utils import (
+    is_module_available,
+    is_package_installed,
+    remove_numeric_and_strings,
+)
 
 from tests.utils import UnitxtTestCase
 
@@ -15,3 +19,37 @@ class TestUtils(UnitxtTestCase):
 
     def test_is_module_available_false(self):
         self.assertFalse(is_module_available("some_non_existent_module"))
+
+    def test_remove_numeric_and_strings(self):
+        test_cases = [
+            ("This is a string with numbers 1234", "This is a string with numbers "),
+            (
+                "This string contains a float 123.45 in it",
+                "This string contains a float  in it",
+            ),
+            (
+                "This string contains a 'quoted string' here",
+                "This string contains a  here",
+            ),
+            (
+                'This string contains a "double quoted string" here',
+                "This string contains a  here",
+            ),
+            (
+                '''This string contains a """triple quoted string""" here''',
+                "This string contains a  here",
+            ),
+            (
+                '''Here are some numbers 1234 and floats 123.45, and strings 'single' "double" """triple""" ''',
+                "Here are some numbers  and floats , and strings    ",
+            ),
+            (
+                "This string contains no numbers or quoted strings",
+                "This string contains no numbers or quoted strings",
+            ),
+        ]
+
+        for i, (input_str, expected_output) in enumerate(test_cases, 1):
+            with self.subTest(i=i):
+                result = remove_numeric_and_strings(input_str)
+                self.assertEqual(result, expected_output)
