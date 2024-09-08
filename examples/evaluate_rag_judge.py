b87c291fa204eeccff34b507399e9124435239f7
lilacheden <lilach.edel@gmail.com>
2024-09-08 21:50:59 +0300

    add test examples add Signed-off-by: lilacheden <lilach.edel@gmail.com>

--- examples/evaluate_rag_judge.py
+++ examples/evaluate_rag_judge.py
@@ -2,14 +2,14 @@ import pandas as pd
 from unitxt.eval_utils import evaluate
 
 test_examples = [
-    {
-        "question": "What foundation models are available in watsonx.ai ?",
-        "answer": "Watsonx.ai supports a variety of foundation models",
-        "contexts": [
-            "Supported foundation models available with watsonx.ai. Watsonx.ai offers numerous foundation models."
-        ],
-        "ground_truths": ["Many Large Language Models are supported by Watsonx.ai"],
-    },
+    # {
+    #     "question": "What foundation models are available in watsonx.ai ?",
+    #     "answer": "Watsonx.ai supports a variety of foundation models",
+    #     "contexts": [
+    #         "Supported foundation models available with watsonx.ai. Watsonx.ai offers numerous foundation models."
+    #     ],
+    #     "ground_truths": ["Many Large Language Models are supported by Watsonx.ai"],
+    # },
     {
         "question": "What foundation models are available in watsonx.ai ?",
         "answer": "Watsonx.ai supports no foundation models",
@@ -26,19 +26,43 @@ test_examples = [
         ],
         "ground_truths": ["Many Large Language Models are supported by Watsonx.ai"],
     },
+    {
+        "question": "What foundation models are available in watsonx.ai ?",
+        "answer": "Meta.ai supports a variety of foundation models",
+        "contexts": [
+            "Supported foundation models available with Meta. Meta AI offers numerous foundation models."
+        ],
+        "ground_truths": ["Many Large Language Models are supported by Watsonx.ai"],
+    },
+    {
+        "question": "What foundation models are available in watsonx.ai ?",
+        "answer": "Watsonx.ai supports a variety of foundation models, the most prominent are Llama, Mixtral and Granite.",
+        "contexts": [
+            "Supported foundation models available with Meta. Meta AI offers numerous foundation models."
+        ],
+        "ground_truths": ["Many Large Language Models are supported by Watsonx.ai"],
+    },
+    {
+        "question": "What foundation models are available in watsonx.ai ?",
+        "answer": "never gonna give you up, never gonna let you down.",
+        "contexts": [
+            "Supported foundation models available with Meta. Meta AI offers numerous foundation models."
+        ],
+        "ground_truths": ["Many Large Language Models are supported by Watsonx.ai"],
+    },
 ]
 metrics_base_path = "metrics.rag"
 metrics_base_names = [
-    "context_relevance.context_relevance_q_c",
+    # "context_relevance.context_relevance_q_c",
     "answer_relevance.answer_relevance_q_a",
-    "answer_correctness.answer_correctness_q_c_a_gt",
+    # "answer_correctness.answer_correctness_q_c_a_gt",
     "answer_correctness.answer_correctness_q_a_gt",
-    "correctness_holistic.correctness_holistic_q_c_a",
-    "faithfulness.faithfulness_c_a",
-    "faithfulness.faithfulness_q_c_a",
+    # "correctness_holistic.correctness_holistic_q_c_a",
+    # "faithfulness.faithfulness_c_a",
+    # "faithfulness.faithfulness_q_c_a",
 ]
 model_names = ["llama_3_1_70b_instruct", "mixtral_8x7b_instruct_v01", "gpt_4_turbo"]
-use_armorm = False
+use_armorm = True
 
 
 if __name__ == "__main__":
@@ -53,11 +77,12 @@ if __name__ == "__main__":
     if use_armorm:
         metric_names.extend(
             [
-                "metrics.rag.correctness_holistic.correctness_holistic_q_c_a_ArmoRM",
+                # "metrics.rag.correctness_holistic.correctness_holistic_q_c_a_ArmoRM",
                 "metrics.rag.answer_relevance.answer_relevance_q_a_ArmoRM",
             ]
         )
 
     result, _ = evaluate(df, metric_names)
     result = result.round(2)
+    #    print(result)
     result.to_csv("dataset_out.csv")
