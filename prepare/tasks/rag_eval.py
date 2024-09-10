2277cc31ed7e8140889fc9e90531137dc41fef46
lilacheden <lilach.edel@gmail.com>
2024-09-10 16:35:07 +0300

    update rag_eval tasks (inputs -> input_fields Signed-off-by: lilacheden <lilach.edel@gmail.com>

--- prepare/tasks/rag_eval.py
+++ prepare/tasks/rag_eval.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 from unitxt import add_to_catalog
 from unitxt.task import Task
 
@@ -15,17 +17,17 @@ rag_classification_metrics = {
 }
 
 field_to_type = {
-    "answer": "str",
-    "ground_truths": "List[str]",
-    "question": "str",
-    "choices": "List[str]",
-    "contexts": "List[str]",
-    "number_val": "Union[float, int]",
-    "textual_label": "List[str]",
-    "is_correct": "List[str]",
-    "is_faithful": "List[str]",
-    "is_context_relevant": "List[str]",
-    "is_relevant": "List[str]",
+    "answer": str,
+    "ground_truths": List[str],
+    "question": str,
+    "choices": List[str],
+    "contexts": List[str],
+    "number_val": Union[float, int],
+    "textual_label": List[str],
+    "is_correct": List[str],
+    "is_faithful": List[str],
+    "is_context_relevant": List[str],
+    "is_relevant": List[str],
 }
 
 
@@ -36,7 +38,7 @@ def convert_to_dict_of_type(field_list):
 for binary_val in rag_classification_metrics:
     add_to_catalog(
         Task(
-            inputs=convert_to_dict_of_type(
+            input_fields=convert_to_dict_of_type(
                 ["answer", "contexts", "question", "choices"]
             ),
             outputs=convert_to_dict_of_type(["is_faithful", "number_val"]),
@@ -49,7 +51,7 @@ for binary_val in rag_classification_metrics:
 
     add_to_catalog(
         Task(
-            inputs=convert_to_dict_of_type(["contexts", "question", "choices"]),
+            input_fields=convert_to_dict_of_type(["contexts", "question", "choices"]),
             outputs=convert_to_dict_of_type(["is_context_relevant", "number_val"]),
             metrics=rag_classification_metrics[binary_val],
             prediction_type="float",
@@ -60,7 +62,7 @@ for binary_val in rag_classification_metrics:
 
     add_to_catalog(
         Task(
-            inputs=convert_to_dict_of_type(["answer", "question", "choices"]),
+            input_fields=convert_to_dict_of_type(["answer", "question", "choices"]),
             outputs=convert_to_dict_of_type(["is_relevant", "number_val"]),
             metrics=rag_classification_metrics[binary_val],
             prediction_type="float",
@@ -71,7 +73,7 @@ for binary_val in rag_classification_metrics:
 
     add_to_catalog(
         Task(
-            inputs=convert_to_dict_of_type(
+            input_fields=convert_to_dict_of_type(
                 ["answer", "contexts", "question", "choices"]
             ),
             outputs=convert_to_dict_of_type(["is_correct", "number_val"]),
@@ -84,7 +86,7 @@ for binary_val in rag_classification_metrics:
 
     add_to_catalog(
         Task(
-            inputs=convert_to_dict_of_type(
+            input_fields=convert_to_dict_of_type(
                 ["answer", "question", "choices", "ground_truths", "contexts"]
             ),
             outputs=convert_to_dict_of_type(["is_correct", "number_val"]),
