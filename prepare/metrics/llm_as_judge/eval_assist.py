edc839b885a81045e43fd7577030fdab26be70fc
Tejaswini Pedapati <teju.great@gmail.com>
2024-09-18 14:33:43 -0400

    Fixed registering eval assist to catalog. Eval assist now generates assessment and summary for hardcoded prompts
    Signed-off-by: Martín Santillán Cooper <msantillancooper@ibm.com>

--- prepare/metrics/llm_as_judge/eval_assist.py
+++ prepare/metrics/llm_as_judge/eval_assist.py
@@ -1,11 +1,11 @@
 import json
 
-from unitxt import add_to_local_catalog
-from unitxt.inference import HFPipelineBasedInferenceEngine
-from unitxt.metrics import (
+from unitxt import add_to_catalog
+from unitxt.inference import IbmGenAiInferenceEngine, IbmGenAiInferenceEngineParamsMixin
+from unitxt.evalassist_llm_as_judge import (
     EvalAssistLLMAsJudge
 )
-from unitxt.templates import InputOutputTemplate, TemplatesDict
+import os
 
 config_filepath = "prepare/metrics/llm_as_judge/eval_assist.json"
 
@@ -13,29 +13,16 @@ with open(config_filepath) as file:
     config = json.load(file)
 print("config is ", config)
 
-# judge_correctness_template = InputOutputTemplate(
-#     instruction="Please act as an impartial judge and evaluate if the assistant's answer is correct."
-#     ' Answer "[[10]]" if the answer is accurate, and "[[0]]" if the answer is wrong. '
-#     'Please use the exact format of the verdict as "[[rate]]". '
-#     "You can explain your answer after the verdict"
-#     ".\n\n",
-#     input_format="[User's input]\n{question}\n" "[Assistant's Answer]\n{answer}\n",
-#     output_format="[[{rating}]]",
-#     postprocessors=[
-#         r"processors.extract_mt_bench_rating_judgment",
-#     ],
-# )
+os.environ["GENAI_KEY"] = "pak-cAcH7ExLy-3jXXCICeCr-jQD3xK8grc3B32vczLXa9E"
+params = IbmGenAiInferenceEngineParamsMixin(max_new_tokens=1024, random_seed=42)
 
-# platform = "hf"
-# model_name = "google/flan-t5-base"
-# inference_model = HFPipelineBasedInferenceEngine(
-#     model_name=model_name, max_new_tokens=256
-# )
-
-eval_assist_metric = EvalAssistLLMAsJudge()#inference_model=inference_model, template=judge_correctness_template)
-
-add_to_local_catalog(
-    eval_assist_metric,
-    "metrics.llm_as_judge.eval_assist.direct",
-    overwrite=True
-)
+for model_name, inference_engine in [("mixtral", IbmGenAiInferenceEngine(model_name="mistralai/mixtral-8x7b-instruct-v01", parameters=params)),
+                    ("granite", IbmGenAiInferenceEngine(model_name="ibm/granite-20b-code-instruct", parameters=params)),
+                    ("llama_8b", IbmGenAiInferenceEngine(model_name="meta-llama/llama-3-8b-instruct", parameters=params)),
+                    ("llama_70b", IbmGenAiInferenceEngine(model_name="meta-llama/llama-3-70b-instruct", parameters=params))]:
+    eval_assist_metric = EvalAssistLLMAsJudge(inference_model=inference_engine)
+    add_to_catalog(
+        eval_assist_metric,
+        f"metrics.llm_as_judge.eval_assist.direct.{model_name}",
+        overwrite=True
+    )
