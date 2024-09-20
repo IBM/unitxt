f2b15849f55ba7a0da2543ecd5055ac33b6882bc
Yotam-Perlitz <y.perlitz@ibm.com>
2024-09-20 09:42:16 +0200

    make example more likely to succeed
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

--- examples/run_generic_inference_engine.py
+++ examples/run_generic_inference_engine.py
@@ -3,13 +3,16 @@ from unitxt.inference import GenericInferenceEngine
 
 if __name__ == "__main__":
     generic_engine = GenericInferenceEngine(
-        default="engines.ibm_gen_ai.llama_3_8b_instruct"
+        default="engines.ibm_wml.llama_3_70b_instruct"
     )
     recipe = "card=cards.almost_evil,template=templates.qa.open.simple,demos_pool_size=0,num_demos=0"
     instances = [
-        {"question": "How many days there are in a week", "answers": ["7"]},
         {
-            "question": "If a ate an apple in the morning, and one in the evening, how many apples did I eat?",
+            "question": "How many days there are in a week, answer only with numerals",
+            "answers": ["7"],
+        },
+        {
+            "question": "If a ate an apple in the morning, and one in the evening, what is the number of apples I have eaten?, answer only with numerals",
             "answers": ["2"],
         },
     ]
