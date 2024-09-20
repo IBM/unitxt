e91085f402eaeda32427e46742c779bc764c0ef9
Yotam-Perlitz <y.perlitz@ibm.com>
2024-09-20 12:06:58 +0200

    add wml based llmaj
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

--- prepare/metrics/llm_as_judge/pairwise_rating/llama_3_ibm_genai_arena_hard_template.py
+++ prepare/metrics/llm_as_judge/pairwise_rating/llama_3_ibm_genai_arena_hard_template.py
@@ -1,6 +1,8 @@
 from unitxt import add_to_catalog
 from unitxt.inference import (
+    GenericInferenceEngine,
     IbmGenAiInferenceEngine,
+    WMLInferenceEngine,
 )
 from unitxt.llm_as_judge import LLMAsJudge
 
@@ -10,27 +12,49 @@ templates = [
     "templates.response_assessment.pairwise_comparative_rating.arena_hard",
     "templates.response_assessment.pairwise_comparative_rating.arena_hard_with_shuffling",
 ]
+
+inference_engines = [
+    ("ibm_wml", WMLInferenceEngine),
+    ("ibm_genai", IbmGenAiInferenceEngine),
+    ("generic", GenericInferenceEngine),
+]
+
+
 for template in templates:
     task = "pairwise_comparative_rating.single_turn"
 
     for model_id in model_list:
-        inference_model = IbmGenAiInferenceEngine(
-            model_name=model_id, max_new_tokens=2048, random_seed=42
-        )
-        model_label = model_id.split("/")[1].replace("-", "_").replace(".", ",").lower()
-        model_label = f"{model_label}_ibm_genai"
-        template_label = template.split(".")[-1]
-        metric_label = f"{model_label}_template_{template_label}"
-        metric = LLMAsJudge(
-            inference_model=inference_model,
-            template=template,
-            task=task,
-            format=format,
-            main_score=metric_label,
-        )
+        for inference_engine_name, inference_engine in inference_engines:
+            if (
+                inference_engine_name == "ibm_wml"
+                and model_id == "meta-llama/llama-3-8b-instruct"
+            ):
+                continue  # currently not supported
 
-        add_to_catalog(
-            metric,
-            f"metrics.llm_as_judge.pairwise_comparative_rating.{model_label}_template_{template_label}",
-            overwrite=True,
-        )
+            # if inference engine is generic, these configurations will be defined when it is saved to the catalog
+            if inference_engine_name != "generic":
+                inference_model = inference_engine(
+                    model_name=model_id, max_new_tokens=2048, random_seed=42
+                )
+            else:
+                inference_model = inference_engine()
+
+            model_label = (
+                model_id.split("/")[1].replace("-", "_").replace(".", ",").lower()
+            )
+            model_label = f"{model_label}_{inference_engine_name}"
+            template_label = template.split(".")[-1]
+            metric_label = f"{model_label}_template_{template_label}"
+            metric = LLMAsJudge(
+                inference_model=inference_model,
+                template=template,
+                task=task,
+                format=format,
+                main_score=metric_label,
+            )
+
+            add_to_catalog(
+                metric,
+                f"metrics.llm_as_judge.pairwise_comparative_rating.{model_label}_template_{template_label}",
+                overwrite=True,
+            )
