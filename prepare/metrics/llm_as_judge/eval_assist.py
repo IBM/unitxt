06dbb617c37837feee828f048b8a6555bbf3c947
Swapnaja <Swapnaja.Achintalwar@ibm.com>
2024-09-27 18:44:33 +0530

    Update EvalAssistLLMasJudge
    Signed-off-by: Martín Santillán Cooper <msantillancooper@ibm.com>

--- prepare/metrics/llm_as_judge/eval_assist.py
+++ prepare/metrics/llm_as_judge/eval_assist.py
@@ -1,26 +1,36 @@
-import json
 
+import os
 from unitxt import add_to_catalog
 from unitxt.inference import IbmGenAiInferenceEngine, IbmGenAiInferenceEngineParamsMixin
 from unitxt.evalassist_llm_as_judge import (
     EvalAssistLLMAsJudge
 )
-import os
+from templates_eval_assist import template_dict
+from rubrics_eval_assist import rubrics
 
-config_filepath = "prepare/metrics/llm_as_judge/eval_assist.json"
+# Register all the predefined rubrics
+for rubric_name, rubric_obj in rubrics.items():
+    add_to_catalog(
+        rubric_obj,
+        f"metrics.llm_as_judge.eval_assist.direct.rubrics.{rubric_name}",
+        overwrite=True
+    )
 
-with open(config_filepath) as file:
-    config = json.load(file)
-print("config is ", config)
-
-os.environ["GENAI_KEY"] = "pak-cAcH7ExLy-3jXXCICeCr-jQD3xK8grc3B32vczLXa9E"
+# os.environ["GENAI_KEY"] = ""
 params = IbmGenAiInferenceEngineParamsMixin(max_new_tokens=1024, random_seed=42)
 
+# Register the metrics for all four models
 for model_name, inference_engine in [("mixtral", IbmGenAiInferenceEngine(model_name="mistralai/mixtral-8x7b-instruct-v01", parameters=params)),
                     ("granite", IbmGenAiInferenceEngine(model_name="ibm/granite-20b-code-instruct", parameters=params)),
                     ("llama_8b", IbmGenAiInferenceEngine(model_name="meta-llama/llama-3-8b-instruct", parameters=params)),
-                    ("llama_70b", IbmGenAiInferenceEngine(model_name="meta-llama/llama-3-70b-instruct", parameters=params))]:
-    eval_assist_metric = EvalAssistLLMAsJudge(inference_model=inference_engine)
+                    ("llama_70b", IbmGenAiInferenceEngine(model_name="meta-llama/llama-3-70b-instruct", parameters=params)),
+                    ("prometheus", IbmGenAiInferenceEngine(model_name="kaist-ai/prometheus-8x7b-v2", parameters=params))]:
+    eval_assist_metric = EvalAssistLLMAsJudge(inference_model=inference_engine, 
+                                            #   rubric=rubrics["temperature"],
+                                              assessment_template=template_dict[model_name]["assessment"],
+                                              summ_template=template_dict[model_name]["summarization"],
+                                              answer_template=template_dict[model_name]["answer"])
+    
     add_to_catalog(
         eval_assist_metric,
         f"metrics.llm_as_judge.eval_assist.direct.{model_name}",
