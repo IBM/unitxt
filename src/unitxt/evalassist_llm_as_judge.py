a48c53043198edead5a862b5f038e99e1136f969
Swapnaja <Swapnaja.Achintalwar@ibm.com>
2024-09-27 18:44:33 +0530

    Update EvalAssistLLMasJudge
    Signed-off-by: Martín Santillán Cooper <msantillancooper@ibm.com>

--- src/unitxt/evalassist_llm_as_judge.py
+++ src/unitxt/evalassist_llm_as_judge.py
@@ -1,98 +1,121 @@
 from .metrics import BulkInstanceMetric
 from .inference import InferenceEngine
-from .templates import Template, InputOutputTemplate
+from .templates import Template
 from .task import Task
+from .artifact import Artifact
 from .api import infer
 
-from typing import List,Dict
+from typing import List,Dict, Literal
 
 class RubricOption:
     def __init__(self, option: str, description: str):
         self.option = option
         self.description = description
 
+class Rubric(Artifact):
+    name: str
+    criteria: str
+    options: List[Dict[Literal["option", "description"], str]]
+
 class EvalAssistLLMAsJudge(BulkInstanceMetric):
     inference_model: InferenceEngine
-    # template: Template
+    rubric: Rubric = None
+    assessment_template : Template = None
+    summ_template : Template = None
+    answer_template : Template = None
+
     reduction_map = {"mean": ["score"]}
     main_score = "score"
-    print("------------inside eval assist llm as a judge-------------")
+
+    assessment_task =  Task(
+            input_fields={"context_variables": str, "response": str, "criteria": str, "options": str},
+            reference_fields={},
+            prediction_type=str,
+            metrics=[])
+    
+    assessment_task_prometheus =  Task(
+            input_fields={"score_instructions" : str, "context_variables": str,
+                        "response": str, "criteria": str, "score_rubric" : str},
+            reference_fields={},
+            prediction_type=str,
+            metrics=[])
+    
+    summ_task =  Task(
+            input_fields={"assessment": str},
+            reference_fields={},
+            prediction_type=str,
+            metrics=[])
    
+    def set_rubric(self, rubrics):
+        rubric_options = [RubricOption(rub_option["option"], rub_option["description"]) for rub_option in rubrics.options]
+        options_as_str = "Choose an answer:\n" + "\n".join([f"- \"{o.option}\"{f' if {o.description}' if o.description != '' else ''}" for o in rubric_options])
+        criteria = rubrics.criteria
+
+        if self.inference_model.model_name == "kaist-ai/prometheus-8x7b-v2":
+            # NOTE : EvalAssist has rubric_options : list(list(RubricOption))
+            # score_options = [[o.option for o in criteria_option_list] for criteria_option_list in rubric_options]
+            # score_rubric = ["".join([f"Score {o.option}: {o.description}\n" for o in criteria_option_list]) for criteria_option_list in rubric_options]
+            score_options = [o.option for o in rubric_options]
+            score_rubric = ["".join([f"Score {o.option}: {o.description}\n" for o in rubric_options])]
+            return criteria, score_options, score_rubric
+        return options_as_str, criteria
+        
     def prepare(self):
-        print("------------inside prepare of eval assist llm as a judge-------------")
-        self.assessment_template = """<s> [INST]
-You are presented with a response generated subject to a context.
-The context includes information relevant to the nature or generation of the response.
-You will assess the quality of the response subject to an evaluation criteria.
-
-###Context:
-{context_variables}
-
-###Response:
-{response}
-
-###Evaluation criteria:
-{criteria}
-{options}
-
-Briefly assess the quality of the response subject to the evaluation criteria.
-Focus on the evaluation criteria during assessment, do not provide a general assessment.
-[/INST]
-Assessment:
-"""
-
-        self.summarization_prompt = """<s> [INST]
-Summarize the following assessment while maintaining the pertinent details.
-Assessment: {assessment}
-[/INST]
-Assessment Summary:
-"""
-        self.answer_prompt = """</s>
-[INST]
-Now consider the evaluation criteria and choose a final answer.
-Validate the answer against the assessment.
-###Evaluation criteria:
-{{criteria}}
-{{options}}
-[/INST]
-Answer:"""
-
-        self.rubric_options = [RubricOption("Yes", "The temperature reading is provided in both Fahrenheit and Celsius."),
-                            RubricOption("No", "The temperature reading is provided either in Fahrenheit or Celsius, but not both."),
-                             RubricOption("Pass", "There is no numerical temperature reading in the response.")]
-        self.options_as_str = "Choose an answer:\n" + "\n".join([f"- \"{o.option}\"{f' if {o.description}' if o.description != '' else ''}" for o in self.rubric_options])
-        self.criteria = "In the response, if there is a numerical temperature present, is it denominated in both Fahrenheit and Celsius?"
+        # TODO: if we have predefined rubrics --> prepare should create it only once
+        # if self.rubric:
+        #     self.set_rubric()
+            
         super().prepare()
 
     def compute(
         self, references: List[List[str]], predictions: List[str], task_data: List[Dict[str,str]]
     ) -> dict:
-        instances = [{
-                    "context_variables": input_instance['question'],
-                    "response": prediction,
-                    "criteria": criterion,
-                    "options": option
-                }
-                for input_instance, prediction, criterion, option in zip(
-                    task_data, predictions, [self.criteria] * len(predictions), [self.options_as_str] * len(predictions))]
+        if self.rubric is None:
+           # Get it from the task data
+           rubric_dict = task_data[0]["rubric"]
+           rubric = Rubric(**rubric_dict)
+        else:
+            rubric = self.rubric
 
-        assessment_template = InputOutputTemplate(input_format=self.assessment_template)
-        summ_template = InputOutputTemplate(input_format=self.summarization_prompt)
-        assessment_task =  Task(
-            input_fields={"context_variables": str, "response": str, "criteria": str, "options": str},
-            reference_fields={},
-            prediction_type=str,
-            metrics=[])
-        assessment_outputs = infer(instances, task=assessment_task, engine=self.inference_model, template=assessment_template)
-        print("ass_output ", assessment_outputs)
+        if self.inference_model.model_name == "kaist-ai/prometheus-8x7b-v2":
+            criteria, score_options, score_rubric = self.set_rubric(rubric)
 
-        assessment_instances = [{"assessment": assessment_output} for assessment_output in assessment_outputs]
-        summ_task =  Task(
-            input_fields={"assessment": str},
-            reference_fields={},
-            prediction_type=str,
-            metrics=[])
-        summ_output = infer(assessment_instances, task=summ_task, engine=self.inference_model, template=summ_template)
-        print("summ_output ", summ_output)
+            # Assessment Stage
+            instances = [{
+                       "score_instructions" : " or ".join(score_options_list),
+                        "context_variables": input_instance['question'],
+                        "response": prediction,
+                        "criteria": criterion,
+                        "score_rubric" : "".join(score_rubric*len(predictions))
+                    }
+                    for input_instance, prediction, criterion, score_options_list in zip(
+                        task_data, predictions, [criteria] * len(predictions), [score_options] * len(predictions))]
+            
+            assessment_outputs = infer(instances, task=self.assessment_task_prometheus, engine=self.inference_model, template=self.assessment_template)
+            print("ass_output prometheus", assessment_outputs)
 
-        return [{"score": 0.8, "assessment": assessment_outputs[i], "summary": summ_output[i]} for i in range(len(predictions))]
+            feedbacks = [out.split("[RESULT]")[0].strip() for out in assessment_outputs]
+            return [{"score": 0.8, "assessment": assessment_outputs[i], "feedbacks": feedbacks[i]} for i in range(len(predictions))]
+        else:
+            options_as_str, criteria = self.set_rubric(rubric)
+            # Assessment Stage
+            instances = [{
+                        "context_variables": input_instance['question'],
+                        "response": prediction,
+                        "criteria": criterion,
+                        "options": option
+                    }
+                    for input_instance, prediction, criterion, option in zip(
+                        task_data, predictions, [criteria] * len(predictions), [options_as_str] * len(predictions))]
+            
+            assessment_outputs = infer(instances, task=self.assessment_task, engine=self.inference_model, template=self.assessment_template)
+            print("ass_output ", assessment_outputs)
+            
+            # Summarisation Stage
+            assessment_instances = [{"assessment": assessment_output} for assessment_output in assessment_outputs]
+            summ_output = infer(assessment_instances, task=self.summ_task, engine=self.inference_model, template=self.summ_template)
+            
+            print("summ_output ", summ_output)
+
+            return [{"score": 0.8, "assessment": assessment_outputs[i], "summary": summ_output[i]} for i in range(len(predictions))]
+    
