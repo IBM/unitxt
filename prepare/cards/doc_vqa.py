d0d376db288435bd2c7280c2f38928afb818dd33
alfassy <amit.alfassy@gmail.com>
2024-09-29 06:38:58 -0700

    Adding of ANLS metric to doc_vqa and info_vqa datasets (#1241)
    sdf
    
    sdf
    
    Co-authored-by: Elron Bandel <elronbandel@gmail.com>

--- prepare/cards/doc_vqa.py
+++ prepare/cards/doc_vqa.py
@@ -17,7 +17,7 @@ for language in ["en", "fr"]:
             ToImage(field="image", to_field="context"),
             Set(fields={"context_type": "image"}),
         ],
-        task="tasks.qa.with_context.abstractive",
+        task="tasks.qa.with_context.abstractive[metrics=[metrics.anls]]",
         templates="templates.qa.with_context.all",
         __tags__={
             "license": "apache-2.0",
