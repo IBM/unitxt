462d82dee8abf327c1951ae4942d6f19b10a3b8b
Shachar Don-Yehiya <shachar.don-yehiya@ibm.com>
2024-10-07 06:13:19 -0400

    add BlueBench as a benchmark to the catalog
    Signed-off-by: Shachar Don-Yehiya <shachar.don-yehiya@ibm.com>

---
+++ prepare/benchmarks/bluebench.py
@@ -0,0 +1,26 @@
+import os
+
+from unitxt.benchmark import Benchmark
+from unitxt.catalog import add_to_catalog
+
+BLUEBENCH_PATH = "src/unitxt/catalog/recipes/bluebench"
+bluebench_scenarios = {}
+
+bluebench_directory = os.fsencode(BLUEBENCH_PATH)
+for file in os.listdir(bluebench_directory):
+    scenario_name = os.fsdecode(file)
+    bluebench_scenarios[scenario_name] = {}
+    for subscenario in os.listdir(f"{BLUEBENCH_PATH}/{scenario_name}"):
+        subscenario_name = os.fsdecode(subscenario)
+        subscenario_name = (
+            subscenario_name[: -len(".json")]
+            if subscenario_name.endswith(".json")
+            else subscenario_name
+        )
+        bluebench_scenarios[scenario_name][
+            subscenario_name
+        ] = f"recipes.bluebench.{scenario_name}.{subscenario_name}"
+    bluebench_scenarios[scenario_name] = Benchmark(bluebench_scenarios[scenario_name])
+
+benchmark = Benchmark(bluebench_scenarios)
+add_to_catalog(benchmark, "benchmarks.bluebench", overwrite=True)
