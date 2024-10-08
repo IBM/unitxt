67ca077d029bd088cb888ff4c87f911863e6ce2d
Swapnaja <Swapnaja.Achintalwar@ibm.com>
2024-10-08 16:00:50 +0530

    add pairwise criteria in preprocess_step

--- examples/evaluate_eval_assist.py
+++ examples/evaluate_eval_assist.py
@@ -23,18 +23,13 @@ rubric = "metrics.llm_as_judge.eval_assist.direct.rubrics.conciseness"
 metric = f"metrics.llm_as_judge.eval_assist.direct.mixtral[rubric={rubric}]"
 
 card = TaskCard(
-    # Load the data from the dictionary.  Data can be  also loaded from HF, CSV files, COS and other sources
-    # with different loaders.
     loader=LoadFromDictionary(data=data),
-    # Define the QA task input and output and metrics.
     task=Task(
         input_fields={"question": str},
         reference_fields={},
         prediction_type=str,
         metrics = [metric]
     ),
-    # Create a simple template that formats the input.
-    # Add lowercase normalization as a post processor.
     templates=TemplatesDict(
         {
             "simple": InputOutputTemplate(
