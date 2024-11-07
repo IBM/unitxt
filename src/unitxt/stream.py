03d85aab8e60d5b0b87059f0a192c00521329ed6
Elron Bandel <elronbandel@gmail.com>
2024-11-07 17:39:06 +0200

    Allow turning single stream to dataset (#1335)
    Signed-off-by: Elron Bandel <elron.bandel@ibm.com>

--- src/unitxt/stream.py
+++ src/unitxt/stream.py
@@ -33,6 +33,22 @@ class Stream(Dataclass):
     def set_copying(self, copying: bool):
         pass
 
+    def to_dataset(self, disable_cache=True, cache_dir=None, features=None):
+        with tempfile.TemporaryDirectory() as dir_to_be_deleted:
+            cache_dir = dir_to_be_deleted if disable_cache else cache_dir
+            return Dataset.from_generator(
+                self.__iter__,
+                keep_in_memory=disable_cache,
+                cache_dir=cache_dir,
+                features=features,
+                num_proc=20,
+            )
+
+    def to_iterable_dataset(
+        self,
+    ):
+        return IterableDataset.from_generator(self.__iter__)
+
 
 class ListStream(Stream):
     instances_list: List[Dict[str, Any]]
@@ -229,26 +245,18 @@ class MultiStream(dict):
             cache_dir = dir_to_be_deleted if disable_cache else cache_dir
             return DatasetDict(
                 {
-                    key: Dataset.from_generator(
-                        self.get_generator,
-                        keep_in_memory=disable_cache,
+                    key: value.to_dataset(
+                        disable_cache=disable_cache,
                         cache_dir=cache_dir,
-                        gen_kwargs={"key": key},
                         features=features,
                     )
-                    for key in self.keys()
+                    for key, value in self.items()
                 }
             )
 
     def to_iterable_dataset(self) -> IterableDatasetDict:
         return IterableDatasetDict(
-            {
-                key: IterableDataset.from_generator(
-                    self.get_generator,
-                    gen_kwargs={"key": key},
-                )
-                for key in self.keys()
-            }
+            {key: value.to_iterable_dataset() for key, value in self.items()}
         )
 
     def __setitem__(self, key, value):
