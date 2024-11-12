dbb42aa48d4346cf290b259d3b1a8604ad644365
dafnapension <dafnashein@yahoo.com>
2024-11-12 22:38:03 +0200

    is 12 the problem?
    Signed-off-by: dafnapension <dafnashein@yahoo.com>

--- tests/catalog/test_preparation.py
+++ tests/catalog/test_preparation.py
@@ -29,10 +29,9 @@ logger.critical(
 )
 # the following should be any of modulo num_par: 0,1,2,3,4,5,6,7,8,.. num_par-1
 modulo = 0
-if num_par > 1:
-    all_preparation_files = [
-        file for i, file in enumerate(all_preparation_files) if i % num_par == modulo
-    ]
+all_preparation_files = [
+    file for i, file in enumerate(all_preparation_files) if i % num_par == modulo
+]
 
 
 class TestCatalogPreparation(UnitxtCatalogPreparationTestCase):
