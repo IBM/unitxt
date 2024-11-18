f202c3a7947d5a3033bd6c92e50bcfc3b9a8fdd7
elronbandel <elronbandel@gmail.com>
2024-11-18 15:10:44 +0200

    Fix grammar
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- prepare/system_prompts/general/be_concise.py
+++ prepare/system_prompts/general/be_concise.py
@@ -2,6 +2,6 @@ from unitxt.catalog import add_to_catalog
 from unitxt.system_prompts import TextualSystemPrompt
 
 system_prompt = TextualSystemPrompt(
-    "be concise. at every point give the shortest acceptable answer."
+    "Be concise. At every point give the shortest acceptable answer."
 )
 add_to_catalog(system_prompt, "system_prompts.general.be_concise", overwrite=True)
