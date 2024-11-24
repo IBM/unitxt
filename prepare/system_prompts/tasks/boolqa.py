da967c9c4975c6778d10581848372d04f72c4943
Yoav Katz <katz@il.ibm.com>
2024-11-24 14:42:30 +0200

    Added example of deprecation of an asset
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/system_prompts/tasks/boolqa.py
+++ prepare/system_prompts/tasks/boolqa.py
@@ -5,7 +5,8 @@ system_prompt = TextualSystemPrompt(
     "You are an agent in charge of answering a boolean (yes/no) question. The system presents "
     "you with a passage and a question. Read the passage carefully, and then answer yes or no. "
     "Think about your answer, and make sure it makes sense. Do not explain the answer. "
-    "Only say yes or no."
+    "Only say yes or no.",
+    __deprecated_msg__="This legacy system prompt reflects a task specific instructions, which best is handled by the instruction field of the template.",
 )
 
 add_to_catalog(system_prompt, "system_prompts.boolqa", overwrite=True)
