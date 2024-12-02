adf1cc1f59c701393a1ad146a9103431fd9d2a9a
Martín Santillán Cooper <martinsantillancooper@gmail.com>
2024-12-02 11:47:07 -0300

    Many changes, prepares to the integration with EvalAssist
    Signed-off-by: Martín Santillán Cooper <msantillancooper@ibm.com>

--- examples/evaluate_eval_assist_direct_user_criteria.py
+++ examples/evaluate_eval_assist_direct_user_criteria.py
@@ -1,9 +1,10 @@
 from unitxt import get_logger
 from unitxt.api import evaluate, load_dataset
 from unitxt.blocks import Task, TaskCard
+from unitxt.eval_assist_constants import OptionSelectionStrategyEnum
 from unitxt.loaders import LoadFromDictionary
-from unitxt.text_utils import print_dict
 from unitxt.operators import Set
+from unitxt.text_utils import print_dict
 
 logger = get_logger()
 
@@ -13,28 +14,22 @@ criteria_json = {
     "options": [
         {
             "name": "Yes",
-            "description": "The temperature reading is provided in both Fahrenheit and Celsius."
+            "description": "The temperature reading is provided in both Fahrenheit and Celsius.",
         },
         {
             "name": "No",
-            "description": "The temperature reading is provided either in Fahrenheit or Celsius, but not both."
+            "description": "The temperature reading is provided either in Fahrenheit or Celsius, but not both.",
         },
         {
             "name": "Pass",
-            "description": "There is no numerical temperature reading in the response."
-        }
+            "description": "There is no numerical temperature reading in the response.",
+        },
     ],
-    "option_map": {
-        "Yes": 1.0,
-        "No": 0.5,
-        "Pass": 0.0
-    }
+    "option_map": {"Yes": 1.0, "No": 0.5, "Pass": 0.0},
 }
 
 data = {
     "test": [
-        {"context": {"Question": "How is the weather?"}},
-        {"context": {"Question": "How is the weather?"}},
         {"context": {"Question": "How is the weather?"}},
     ]
 }
@@ -46,16 +41,17 @@ card = TaskCard(
         input_fields={"context": dict, "criteria": dict},
         reference_fields={},
         prediction_type=str,
-        metrics = ["metrics.llm_as_judge.eval_assist.direct_assessment.llama3_1_70b"],
-    )
+        metrics=[
+            "metrics.llm_as_judge.eval_assist.direct_assessment.rits.llama3_1_70b"
+            f"[option_selection_strategy={OptionSelectionStrategyEnum.PARSE_OUTPUT_TEXT.name}]"
+        ],
+    ),
 )
 
 test_dataset = load_dataset(card=card, template="templates.empty")["test"]
 
 predictions = [
     """On most days, the weather is warm and humid, with temperatures often soaring into the high 80s and low 90s Fahrenheit (around 31-34°C). The dense foliage of the jungle acts as a natural air conditioner, keeping the temperature relatively stable and comfortable for the inhabitants.""",
-    """On most days, the weather is warm and humid, with temperatures often soaring into the high 80s and low 90s Fahrenheit. The dense foliage of the jungle acts as a natural air conditioner, keeping the temperature relatively stable and comfortable for the inhabitants.""",
-    """On most days, the weather is warm and humid. The dense foliage of the jungle acts as a natural air conditioner, keeping the temperature relatively stable and comfortable for the inhabitants."""
 ]
 
 evaluated_dataset = evaluate(predictions=predictions, data=test_dataset)
@@ -71,4 +67,3 @@ for instance in evaluated_dataset:
             "score",
         ],
     )
-    
--- prepare/metrics/llm_as_judge/eval_assist.py
+++ prepare/metrics/llm_as_judge/eval_assist.py
@@ -1,104 +1,124 @@
-from typing import Optional, Union
-from unitxt import add_to_catalog
+from unitxt import add_to_catalog, get_logger
+from unitxt.eval_assist_constants import (
+    DIRECT_ASSESSMENT_CRITERIAS,
+    EVALUATOR_TO_MODEL_ID,
+    EVALUATORS_METADATA,
+    INFERENCE_ENGINE_NAME_TO_CLASS,
+    PAIRWISE_COMPARISON_CRITERIAS,
+    PROVIDER_TO_STRATEGY,
+    EvaluatorNameEnum,
+    EvaluatorTypeEnum,
+    ModelProviderEnum,
+    OptionSelectionStrategyEnum,
+)
 from unitxt.eval_assist_llm_as_judge_direct import EvalAssistLLMAsJudgeDirect
 from unitxt.eval_assist_llm_as_judge_pairwise import EvalAssistLLMAsJudgePairwise
-from unitxt.eval_assist_constants import DIRECT_ASSESSMENT_CRITERIAS, EVALUATOR_TO_MODEL_ID, EVALUATORS_METADATA, PAIRWISE_COMPARISON_CRITERIAS, EvaluatorMetadata, EvaluatorNameEnum, EvaluatorTypeEnum, INFERENCE_ENGINE_NAME_TO_CLASS
-from templates_eval_assist_direct import direct_assessment_template_dict
-from templates_eval_assist_pairwise import pairwise_comparison_template_dict
+from unitxt.eval_assist_utils import get_evaluator_metadata, rename_model_if_required
+from unitxt.inference import MockInferenceEngine, RITSInferenceEngine
 
+logger = get_logger()
 
-
-def get_evaluator_metadata(name: EvaluatorNameEnum) -> EvaluatorMetadata: #, evaluator_type: EvaluatorTypeEnum) -> EvaluatorMetadata:
-    evaluator_search = [e for e in EVALUATORS_METADATA if e.name == name] #and e.evaluator_type == evaluator_type]
-    if len(evaluator_search) == 0:
-        # raise ValueError(f'A {evaluator_type} evaluator with id {name} does not exist.')
-        raise ValueError(f'An evaluator with id {name} does not exist.')
-    if len(evaluator_search) > 1:
-        # raise ValueError(f'A {evaluator_type} evaluator with id {name} matched several models.')
-        raise ValueError(f'An evaluator with id {name} matched several models.')
-    return evaluator_search[0]
 
 def get_evaluator(
-        name: EvaluatorNameEnum,
-        evaluator_type: EvaluatorTypeEnum,
-        credentials: Optional[dict[str,str]] = None,
-        provider: Optional[str] = None,
-        inference_engine_params: Optional[dict[str,any]] = None) ->  EvalAssistLLMAsJudgeDirect | EvalAssistLLMAsJudgePairwise:
-    
-    evaluator_metadata = get_evaluator_metadata(name) #, evaluator_type)
+    name: EvaluatorNameEnum,
+    evaluator_type: EvaluatorTypeEnum,
+    provider: ModelProviderEnum,
+) -> EvalAssistLLMAsJudgeDirect | EvalAssistLLMAsJudgePairwise:
+    evaluator_metadata = get_evaluator_metadata(name)
 
-    selected_provider = provider
-    if selected_provider is None:
-        if len(evaluator_metadata.providers) > 1:
-            raise Exception(f'Evaluator {evaluator_metadata.name} has more than one available model providers ({evaluator_metadata.providers}). Use the provider parameter to select one of them.')
-        selected_provider = evaluator_metadata.providers[0]
+    inference_params = {"max_tokens": 1024, "seed": 42}
+    model_name = rename_model_if_required(EVALUATOR_TO_MODEL_ID[name], provider)
+    if provider != ModelProviderEnum.WATSONX:
+        model_name = f"watsonx/{model_name}"
+    elif provider in [ModelProviderEnum.OPENAI, ModelProviderEnum.RITS]:
+        model_name = f"openai/{model_name}"
 
-    params = {"model_name": EVALUATOR_TO_MODEL_ID[evaluator_metadata.name]}
     params = {
-        **params,
-        **inference_engine_params
+        "model": model_name,
+        **inference_params,
     }
-    inference_engine = INFERENCE_ENGINE_NAME_TO_CLASS[selected_provider](**params)
 
-    model_family = evaluator_metadata.model_family
-    if evaluator_type == EvaluatorTypeEnum.DIRECT_ASSESSMENT:
-        evaluator = EvalAssistLLMAsJudgeDirect(
-            inference_engine=inference_engine,
-            assessment_template=direct_assessment_template_dict[model_family]["assessment"],
-            summarization_template=direct_assessment_template_dict[model_family]["summarization"],
-            option_selection_template=direct_assessment_template_dict[model_family]["answer"],
-            option_selection_strategy=evaluator_metadata.option_selection_strategy.name,
-            evaluator_name = evaluator_metadata.name.name,
-            model_family=evaluator_metadata.model_family.name
-        )
-    else:
-        evaluator = EvalAssistLLMAsJudgePairwise(
-            inference_engine=inference_engine,
-            assessment_template=pairwise_comparison_template_dict[model_family]["assessment"],
-            summarization_template=pairwise_comparison_template_dict[model_family]["summarization"],
-            option_selection_template=pairwise_comparison_template_dict[model_family]["answer"],
-            option_selection_strategy=evaluator_metadata.option_selection_strategy.name,
-            evaluator_name = evaluator_metadata.name.name
-        )
-    return evaluator
+    if provider == ModelProviderEnum.RITS:
+        params['api_base'] = RITSInferenceEngine.get_base_url_from_model_name(model_name) + '/v1'
 
-print("Registering criterias...")
+    inference_engine = INFERENCE_ENGINE_NAME_TO_CLASS[provider](**params)
+
+    params = {
+        "inference_engine": inference_engine,
+        "option_selection_strategy": PROVIDER_TO_STRATEGY[provider].name,
+        "evaluator_name": evaluator_metadata.name.name,
+        "model_family": evaluator_metadata.model_family.name,
+    }
+
+    evaluator_klass = (
+        EvalAssistLLMAsJudgeDirect
+        if evaluator_type == EvaluatorTypeEnum.DIRECT_ASSESSMENT
+        else EvalAssistLLMAsJudgePairwise
+    )
+
+    return evaluator_klass(**params)
+
+
+logger.debug("Registering criterias...")
 # Register all the predefined criterisa
 for criteria in DIRECT_ASSESSMENT_CRITERIAS:
     add_to_catalog(
         criteria,
         f"metrics.llm_as_judge.eval_assist.direct_assessment.criterias.{criteria.name}",
-        overwrite=True
+        overwrite=True,
     )
 
 for criteria in PAIRWISE_COMPARISON_CRITERIAS:
     add_to_catalog(
         criteria,
         f"metrics.llm_as_judge.eval_assist.pairwise_comparison.criterias.{criteria.name}",
-        overwrite=True
+        overwrite=True,
     )
-    
-print("Registering evaluators...")
+
+logger.debug("Registering evaluators...")
 for evaluator_metadata in EVALUATORS_METADATA:
     for provider in evaluator_metadata.providers:
-        for evaluator_type in [EvaluatorTypeEnum.DIRECT_ASSESSMENT, EvaluatorTypeEnum.PAIRWISE_COMPARISON]:
-            evalutor = get_evaluator(
+        for evaluator_type in [
+            EvaluatorTypeEnum.DIRECT_ASSESSMENT,
+            EvaluatorTypeEnum.PAIRWISE_COMPARISON,
+        ]:
+            evaluator = get_evaluator(
                 name=evaluator_metadata.name,
                 evaluator_type=evaluator_type,
                 provider=provider,
-                inference_engine_params={
-                    "max_new_tokens": 1024,
-                    "random_seed": 42
-                } if evaluator_metadata.name != EvaluatorNameEnum.GPT4 else {
-                    "max_tokens": 1024,
-                    "seed": 42
-                }
             )
 
-            metric_name = evaluator_metadata.name.value.lower().replace('-', '_').replace('.','_')
-            add_to_catalog(
-                evalutor,
-                f"metrics.llm_as_judge.eval_assist.{evaluator_type.value}.{metric_name}",
-                overwrite=True
+            metric_name = (
+                evaluator_metadata.name.value.lower()
+                .replace("-", "_")
+                .replace(".", "_")
+                .replace(" ", "_")
             )
-        
+            add_to_catalog(
+                evaluator,
+                f"metrics.llm_as_judge.eval_assist.{evaluator_type.value}.{provider.value.lower()}.{metric_name}",
+                overwrite=True,
+            )
+
+for evaluator_type in [
+    EvaluatorTypeEnum.DIRECT_ASSESSMENT,
+    EvaluatorTypeEnum.PAIRWISE_COMPARISON,
+]:
+    evaluator_klass = (
+        EvalAssistLLMAsJudgeDirect
+        if evaluator_type == EvaluatorTypeEnum.DIRECT_ASSESSMENT
+        else EvalAssistLLMAsJudgePairwise
+    )
+
+    e = evaluator_klass(
+        inference_engine=MockInferenceEngine(model_name="mock"),
+        option_selection_strategy=OptionSelectionStrategyEnum.PARSE_OUTPUT_TEXT.name,
+        evaluator_name="",
+        model_family="",
+    )
+
+    add_to_catalog(
+        e,
+        f"metrics.llm_as_judge.eval_assist.{evaluator_type.value}",
+        overwrite=True,
+    )
