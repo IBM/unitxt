46b8068dec00877603140002a6a47c8af1f5f7e9
Yoav Katz <katz@il.ibm.com>
2024-12-05 15:52:42 +0200

    Suggested python based approach for user criteria
    Signed-off-by: Yoav Katz <katz@il.ibm.com>
    Signed-off-by: Martín Santillán Cooper <msantillancooper@ibm.com>

---
+++ examples/evaluate_eval_assist_direct_user_criteria_new.py
@@ -0,0 +1,78 @@
+from unitxt import get_logger
+from unitxt.api import evaluate, load_dataset
+from unitxt.blocks import Task, TaskCard
+from unitxt.eval_assist_constants import (
+    CriteriaOption,
+    CriteriaWithOptions,
+)
+from unitxt.eval_assist_llm_as_judge_direct import EvalAssistLLMAsJudgeDirect
+from unitxt.inference import LiteLLMInferenceEngine
+from unitxt.loaders import LoadFromDictionary
+from unitxt.text_utils import print_dict
+
+logger = get_logger()
+
+criteria = CriteriaWithOptions(
+    name="Temperature",
+    description="In the response, if there is a numerical temperature present, is it denominated in both Fahrenheit and Celsius?",
+    options=[
+        CriteriaOption(
+            name="Yes",
+            description="The temperature reading is provided in both Fahrenheit and Celsius.",
+        ),
+        CriteriaOption(
+            name="No",
+            description="The temperature reading is provided either in Fahrenheit or Celsius, but not both.",
+        ),
+        CriteriaOption(
+            name="Pass",
+            description="There is no numerical temperature reading in the response.",
+        ),
+    ],
+    option_map={"Yes": 1.0, "No": 0.5, "Pass": 0.0},
+)
+
+data = {
+    "test": [
+        {"context": {"Question": "How is the weather?"}},
+    ]
+}
+
+metric = EvalAssistLLMAsJudgeDirect(
+    inference_engine=LiteLLMInferenceEngine(
+        model="watsonx/meta-llama/llama-3-1-70b-instruct", max_tokens=1024
+    ),
+    option_selection_strategy="PARSE_OUTPUT_TEXT",
+    evaluator_name="LLAMA3_1_70B",
+    criteria_or_criterias=criteria,
+)
+card = TaskCard(
+    loader=LoadFromDictionary(data=data, data_classification_policy=["public"]),
+    preprocess_steps=[],
+    task=Task(
+        input_fields={"context": dict},
+        reference_fields={},
+        prediction_type=str,
+        metrics=[metric],
+    ),
+)
+
+test_dataset = load_dataset(card=card, template="templates.empty")["test"]
+
+predictions = [
+    """On most days, the weather is warm and humid, with temperatures often soaring into the high 80s and low 90s Fahrenheit (around 31-34°C). The dense foliage of the jungle acts as a natural air conditioner, keeping the temperature relatively stable and comfortable for the inhabitants.""",
+]
+
+evaluated_dataset = evaluate(predictions=predictions, data=test_dataset)
+
+for instance in evaluated_dataset:
+    print_dict(
+        instance,
+        keys_to_print=[
+            "source",
+            "prediction",
+            # "processed_prediction",
+            # "references",
+            "score",
+        ],
+    )
