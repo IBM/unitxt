a18534282242d2c3a811fe22ce3da93976bf931b
Yotam-Perlitz <y.perlitz@ibm.com>
2024-12-06 20:52:48 +0100

    add basic template
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

---
+++ prepare/templates/text2sql/templates.py
@@ -0,0 +1,29 @@
+from unitxt import add_to_catalog
+from unitxt.blocks import TemplatesList
+from unitxt.text2sql.templates import Text2SQLInputOutputTemplate
+
+add_to_catalog(
+    Text2SQLInputOutputTemplate(
+        input_format="Question:\nYou are given the following SQL schema\n\n```sql\n{schema_text}```\n\n{question}\n\n\n",
+        instruction="",
+        num_samples=0,
+        use_schema_linking=False,
+        use_oracle_knowledge=False,
+        db_type="sqlite",
+        target_prefix="Answer:\n```sql\n",
+        output_format="{label}",
+        postprocessors=["processors.text2sql.get_sql"],
+    ),
+    "templates.text2sql.you_are_given",
+    overwrite=True,
+)
+
+add_to_catalog(
+    TemplatesList(
+        [
+            "templates.text2sql.you_are_given",
+        ]
+    ),
+    "templates.text2sql.all",
+    overwrite=True,
+)
