94f10c432aa403c7b4d0bed89334ddbfc52d90f1
Yotam-Perlitz <y.perlitz@ibm.com>
2024-12-06 20:14:25 +0100

    add text2sql task
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

---
+++ prepare/tasks/text2sql.py
@@ -0,0 +1,13 @@
+from unitxt.blocks import Task
+from unitxt.catalog import add_to_catalog
+from unitxt.text2sql.metrics import ExecutionAccuracy  # noqa
+
+add_to_catalog(
+    Task(
+        input_fields={"id": str, "utterance": str, "db_id": str, "dbms": str},
+        reference_fields={"query": str},
+        prediction_type=str,
+        metrics=["metrics.text2sql.execution_accuracy"],
+    ),
+    "tasks.text2sql",
+)
