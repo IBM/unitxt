cecca37c04e216b8dfeb1e5e53759e3bad58f65c
Yoav Katz <katz@il.ibm.com>
2024-12-11 12:47:48 +0200

    Fixed typo in template
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/templates/qa/with_context.py
+++ prepare/templates/qa/with_context.py
@@ -79,7 +79,7 @@ add_to_catalog(
 
 add_to_catalog(
     MultiReferenceTemplate(
-        instruction="Answer the question directly based on the information provided in the {context_type}. Respond with the exact phrase within the {content_type} that provides the answer.",
+        instruction="Answer the question directly based on the information provided in the {context_type}. Respond with the exact phrase within the {context_type} that provides the answer.",
         input_format="{context_type}:\n{context}\nQuestion:\n{question}",
         output_format="{answer}",
         target_prefix="Answer:\n",
