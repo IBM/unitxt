8d44f679ccf60296567aa1437914b61da1714b9a
ofirarviv <ofir.arviv@ibm.com>
2024-12-15 17:14:12 +0200

    fixes

--- prepare/formats/empty.py
+++ prepare/formats/empty.py
@@ -7,11 +7,13 @@ format = SystemFormat(
 )
 
 default_format = SystemFormat()
+
 assert (
     format.demo_format == default_format.demo_format
 ), f"{format.demo_format} != {default_format.demo_format}"
 assert (
     format.model_input_format == default_format.model_input_format
 ), f"{format.model_input_format} != {default_format.model_input_format}"
+
 
 add_to_catalog(format, "formats.empty", overwrite=True)
