5d9112f919a0af3e7c485e36d5f0e47fc896ef80
Yotam-Perlitz <y.perlitz@ibm.com>
2024-12-18 20:01:50 +0100

    adjust templates to using serializer
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

--- prepare/templates/text2sql/templates.py
+++ prepare/templates/text2sql/templates.py
@@ -1,27 +1,9 @@
 from unitxt import add_to_catalog
 from unitxt.blocks import TemplatesList
-from unitxt.text2sql.templates import Text2SQLInputOutputTemplate
+from unitxt.templates import InputOutputTemplate
 
 add_to_catalog(
-    Text2SQLInputOutputTemplate(
-        input_format="You are given the following SQL schema\n\n```sql\n{schema_text}```\n\nand question:\n\n{utterance}\n\n",
-        instruction="You are a Text2SQL generation model, in your answer, only have SQL code",
-        num_samples=0,
-        # use_schema_linking=False,
-        use_oracle_knowledge=False,
-        db_type="sqlite",
-        # target_prefix="Answer:\n```sql\n",
-        # target_prefix="SELECT ",
-        target_prefix="```sql\nSELECT ",
-        output_format="{query}",
-        postprocessors=["processors.text2sql.get_sql"],
-    ),
-    "templates.text2sql.you_are_given",
-    overwrite=True,
-)
-
-add_to_catalog(
-    Text2SQLInputOutputTemplate(
+    InputOutputTemplate(
         input_format="""You are given the following question:
 
 {utterance}
@@ -29,7 +11,34 @@ add_to_catalog(
 An SQL schema
 
 ```sql
-{schema_text}
+{schema}
+```
+
+Answer the following question:
+
+{utterance}
+
+""",
+        instruction="You are a Text2SQL generation model, in your answer, only have SQL code",
+        target_prefix="```sql\nSELECT ",
+        output_format="{query}",
+        postprocessors=["processors.text2sql.get_sql"],
+        # serializer="serializers.text2sql.schema",
+    ),
+    "templates.text2sql.you_are_given",
+    overwrite=True,
+)
+
+add_to_catalog(
+    InputOutputTemplate(
+        input_format="""You are given the following question:
+
+{utterance}
+
+An SQL schema
+
+```sql
+{schema}
 ```
 
 And hint:
@@ -42,12 +51,6 @@ Answer the following question:
 
 """,
         instruction="You are a Text2SQL generation model, in your answer, only have SQL code",
-        num_samples=0,
-        # use_schema_linking=True,
-        use_oracle_knowledge=True,
-        db_type="sqlite",
-        # target_prefix="Answer:\n```sql\n",
-        # target_prefix="SELECT ",
         target_prefix="```sql\nSELECT ",
         output_format="{query}",
         postprocessors=["processors.text2sql.get_sql"],
@@ -56,11 +59,12 @@ Answer the following question:
     overwrite=True,
 )
 
+
 add_to_catalog(
     TemplatesList(
         [
-            "templates.text2sql.you_are_given",
             "templates.text2sql.you_are_given_with_hint",
+            "templates.text2sql.you_are_given",
         ]
     ),
     "templates.text2sql.all",
