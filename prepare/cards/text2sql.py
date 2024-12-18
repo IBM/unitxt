82e1fe8282fef2ef026229048731f76ec014d0d5
Yotam-Perlitz <y.perlitz@ibm.com>
2024-12-18 20:00:08 +0100

    update card to use serializers
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

--- prepare/cards/text2sql.py
+++ prepare/cards/text2sql.py
@@ -1,8 +1,7 @@
 from unitxt import add_to_catalog
-from unitxt.blocks import Rename, Set, TaskCard
+from unitxt.blocks import Copy, Rename, Set, TaskCard
 from unitxt.loaders import LoadHF
 from unitxt.text2sql.metrics import ExecutionAccuracy  # noqa
-from unitxt.text2sql.templates import Text2SQLInputOutputTemplate  # noqa
 
 # add_to_catalog(
 #     Task(
@@ -20,26 +19,26 @@ from unitxt.text2sql.templates import Text2SQLInputOutputTemplate  # noqa
 # print(df.columns.tolist())
 # ['question_id', 'db_id', 'question', 'evidence', 'SQL', 'difficulty']
 
-card = TaskCard(
-    loader=LoadHF(path="heegyu/bird-sql-mini-dev", split="validation"),
-    preprocess_steps=[
-        Rename(
-            field_to_field={
-                "question_id": "id",
-                "question": "utterance",
-                "SQL": "query",
-            }
-        ),
-        Set(fields={"dbms": "sqlite"}),
-    ],
-    task="tasks.text2sql",
-    templates="templates.text2sql.all",
-)
-add_to_catalog(
-    card,
-    "cards.text2sql.bird_mini_dev",
-    overwrite=True,
-)
+# card = TaskCard(
+#     loader=LoadHF(path="heegyu/bird-sql-mini-dev", split="validation"),
+#     preprocess_steps=[
+#         Rename(
+#             field_to_field={
+#                 "question_id": "id",
+#                 "question": "utterance",
+#                 "SQL": "query",
+#             }
+#         ),
+#         Set(fields={"dbms": "sqlite"}),
+#     ],
+#     task="tasks.text2sql",
+#     templates="templates.text2sql.all",
+# )
+# add_to_catalog(
+#     card,
+#     "cards.text2sql.bird_mini_dev",
+#     overwrite=True,
+# )
 
 card = TaskCard(
     loader=LoadHF(path="premai-io/birdbench", split="validation"),
@@ -49,15 +48,36 @@ card = TaskCard(
                 "question_id": "id",
                 "question": "utterance",
                 "SQL": "query",
+                "db_id": "db_id",
             }
         ),
-        Set(fields={"dbms": "sqlite"}),
+        Set(
+            fields={
+                "dbms": "sqlite",
+                "db_type": "sqlite",
+                "use_oracle_knowledge": True,
+                "num_table_rows_to_add": 0,
+            }
+        ),
+        Copy(field="db_id", to_field="schema/db_id"),
+        Copy(field="db_type", to_field="schema/db_type"),
+        Copy(field="num_table_rows_to_add", to_field="schema/num_table_rows_to_add"),
     ],
     task="tasks.text2sql",
     templates="templates.text2sql.all",
 )
+
+# test_card(
+#     card,
+# )
+
 add_to_catalog(
     card,
     "cards.text2sql.bird",
     overwrite=True,
 )
+
+# from unitxt import evaluate, load_dataset
+
+# ds = load_dataset("card=cards.text2sql.bird,template_card_index=0,loader_limit=10")
+# scores = evaluate(predictions=ds["validation"]["target"], data=ds["validation"])
