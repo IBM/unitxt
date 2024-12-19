3e23e4c898cd198d4f1d1e244aca07f833c2beac
Yotam-Perlitz <y.perlitz@ibm.com>
2024-12-19 12:27:35 +0100

    edit template
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

--- prepare/templates/text2sql/templates.py
+++ prepare/templates/text2sql/templates.py
@@ -1,74 +1,43 @@
+import sys
+
 from unitxt import add_to_catalog
 from unitxt.blocks import TemplatesList
 from unitxt.templates import InputOutputTemplate
 
-add_to_catalog(
-    InputOutputTemplate(
-        input_format="""You are given the following question:
+sys.setrecursionlimit(99999)
 
-{utterance}
 
-An SQL schema
-
-```sql
-{schema}
-```
-
-Answer the following question:
-
-{utterance}
-
-""",
-        instruction="You are a Text2SQL generation model, in your answer, only have SQL code.\n"
-        "Start your query with 'SELECT' and end it with ';'\n\n",
-        target_prefix="",
-        output_format="{query}",
-        postprocessors=["processors.text2sql.get_sql"],
-        # serializer="serializers.text2sql.schema",
+template_details = [
+    (
+        "templates.text2sql.you_are_given",
+        "You are given the following question:\n\n{utterance}\n\nAn SQL schema\n\n```sql\n\n{schema}\n```\n\nAnswer the following question:\n\n{utterance}\n\n",
+        "You are a Text2SQL generation model, in your answer, only have SQL code.\nStart your query with 'SELECT' and end it with ';'\n\n",
     ),
-    "templates.text2sql.you_are_given",
-    overwrite=True,
-)
-
-add_to_catalog(
-    InputOutputTemplate(
-        input_format="""You are given the following question:
-
-{utterance}
-
-An SQL schema
-
-```sql
-{schema}
-```
-
-And hint:
-
-{evidence}
-
-Answer the following question:
-
-{utterance}
-
-""",
-        instruction="You are a Text2SQL generation model, in your answer, only have SQL code.\n"
-        "Make sure you start your query with 'SELECT' and end it with ';'\n\n",
-        target_prefix="",
-        output_format="{query}",
-        postprocessors=["processors.text2sql.get_sql"],
+    (
+        "templates.text2sql.you_are_given_with_hint",
+        "You are given the following question:\n\n{utterance}\n\nAn SQL schema\n\n```sql\n\n{schema}\n```\n\nAnd hint:\n\n{evidence}\n\nAnswer the following question:\n\n{utterance}\n\n",
+        "You are a Text2SQL generation model, in your answer, only have SQL code.\nMake sure you start your query with 'SELECT' and end it with ';'\n\n",
     ),
-    "templates.text2sql.you_are_given_with_hint",
-    overwrite=True,
-)
+]
+
+template_names = []
+for name, input_format, instruction in template_details:
+    add_to_catalog(
+        InputOutputTemplate(
+            input_format=input_format,
+            instruction=instruction,
+            target_prefix="",
+            output_format="{query}",
+            postprocessors=["processors.text2sql.get_sql"],
+        ),
+        name,
+        overwrite=True,
+    )
+    template_names.append(template_names)
 
 
 add_to_catalog(
-    TemplatesList(
-        [
-            "templates.text2sql.you_are_given_with_hint",
-            "templates.text2sql.you_are_given",
-        ]
-    ),
+    TemplatesList(template_names),
     "templates.text2sql.all",
     overwrite=True,
 )
