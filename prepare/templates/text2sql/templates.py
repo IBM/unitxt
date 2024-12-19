ce38e3a621992f7fed12ea5cfee12d499370cd34
Yotam-Perlitz <y.perlitz@ibm.com>
2024-12-19 13:38:09 +0100

    add option with ticks
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

--- prepare/templates/text2sql/templates.py
+++ prepare/templates/text2sql/templates.py
@@ -9,24 +9,38 @@ sys.setrecursionlimit(99999)
 
 template_details = [
     (
+        "templates.text2sql.you_are_given_with_sql_prefix",
+        "You are given the following question:\n\n{utterance}\n\nAn SQL schema\n\n```sql\n\n{schema}\n```\n\nAnswer the following question:\n\n{utterance}\n\n",
+        "You are a Text2SQL generation model, in your answer, only have SQL code.\nStart your query with 'SELECT' and end it with ';'\n\n",
+        "```sql\nSELECT ",
+    ),
+    (
+        "templates.text2sql.you_are_given_with_hint_with_sql_prefix",
+        "You are given the following question:\n\n{utterance}\n\nAn SQL schema\n\n```sql\n\n{schema}\n```\n\nAnd hint:\n\n{evidence}\n\nAnswer the following question:\n\n{utterance}\n\n",
+        "You are a Text2SQL generation model, in your answer, only have SQL code.\nMake sure you start your query with 'SELECT' and end it with ';'\n\n",
+        "```sql\nSELECT ",
+    ),
+    (
         "templates.text2sql.you_are_given",
         "You are given the following question:\n\n{utterance}\n\nAn SQL schema\n\n```sql\n\n{schema}\n```\n\nAnswer the following question:\n\n{utterance}\n\n",
         "You are a Text2SQL generation model, in your answer, only have SQL code.\nStart your query with 'SELECT' and end it with ';'\n\n",
+        "",
     ),
     (
-        "templates.text2sql.you_are_given_with_hint",
+        "templates.text2sql.you_are_given_with_hint_with_sql_prefix",
         "You are given the following question:\n\n{utterance}\n\nAn SQL schema\n\n```sql\n\n{schema}\n```\n\nAnd hint:\n\n{evidence}\n\nAnswer the following question:\n\n{utterance}\n\n",
         "You are a Text2SQL generation model, in your answer, only have SQL code.\nMake sure you start your query with 'SELECT' and end it with ';'\n\n",
+        "",
     ),
 ]
 
 template_names = []
-for name, input_format, instruction in template_details:
+for name, input_format, instruction, target_prefix in template_details:
     add_to_catalog(
         InputOutputTemplate(
             input_format=input_format,
             instruction=instruction,
-            target_prefix="",
+            target_prefix=target_prefix,
             output_format="{query}",
             postprocessors=["processors.text2sql.get_sql"],
         ),
