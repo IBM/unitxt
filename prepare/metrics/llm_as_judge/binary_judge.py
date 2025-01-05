86f73c530e30deb5033d18053f8a9cfc0a71db9b
lilacheden <lilach.edel@gmail.com>
2025-01-05 14:48:49 +0200

    add rag judges that use the new templates Signed-off-by: lilacheden <lilach.edel@gmail.com>

--- prepare/metrics/llm_as_judge/binary_judge.py
+++ prepare/metrics/llm_as_judge/binary_judge.py
@@ -1,4 +1,5 @@
 from unitxt import add_to_catalog
+from unitxt.artifact import UnitxtArtifactNotFoundError, fetch_artifact
 from unitxt.inference import GenericInferenceEngine
 from unitxt.llm_as_judge import (
     TaskBasedLLMasJudge,
@@ -11,10 +12,7 @@ metric_type_to_template_dict = {
     },
     "context_relevance": {"q_c_ares": "judge_context_relevance_ares"},
     "correctness_holistic": {"q_c_a": "judge_correctness_simple"},
-    "answer_correctness": {
-        "q_a_gt_loose": "judge_loose_match_no_context",
-        "q_a_gt_strict": "judge_simplified_format",
-    },
+    "answer_correctness": {"q_a_gt_loose": "judge_loose_match_no_context"},
     "answer_relevance": {"q_a": "judge_answer_relevance"},
 }
 
@@ -32,18 +30,31 @@ def get_prediction_field(metric_type):
 for metric_type, template_dict in metric_type_to_template_dict.items():
     for template_short_name, template_name in template_dict.items():
         task_name = f"tasks.rag_eval.{metric_type}.binary"
-
-        for use_logprobs in [True, False]:
+        for logprobs_label in [
+            "",
+            "_logprobs",
+            "_numeric",
+            "_verbal_good_bad",
+            "_verbal",
+        ]:
+            use_logprobs = logprobs_label == "_logprobs"
+            template = (
+                f"templates.rag_eval.{metric_type}.{template_name}{logprobs_label}"
+            )
+            try:
+                t = fetch_artifact(template)[0]
+            except UnitxtArtifactNotFoundError:
+                continue
             for inf_label, inference_model in inference_models.items():
                 if (
                     use_logprobs and inf_label == generic_engine_label
                 ):  # engine GenericInferenceEngine does not support logprobs
                     continue
-                logprobs_label = "_logprobs" if use_logprobs else ""
+
                 metric_label = f"{metric_type}_{template_short_name}{logprobs_label}"
                 metric = TaskBasedLLMasJudge(
                     inference_model=inference_model,
-                    template=f"templates.rag_eval.{metric_type}.{template_name}{logprobs_label}",
+                    template=template,
                     task=task_name,
                     format=None,
                     main_score=metric_label,
