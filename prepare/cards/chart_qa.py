380303702489cb998b293164453641eed5b39083
Amit Alfassy <amit.alfassy@gmail.com>
2025-01-05 15:38:47 +0200

    Fixing stuff :)

--- prepare/cards/chart_qa.py
+++ prepare/cards/chart_qa.py
@@ -4,49 +4,21 @@ from unitxt.collections_operators import Wrap
 from unitxt.image_operators import ToImage
 from unitxt.templates import MultiReferenceTemplate
 from unitxt.test_utils.card import test_card
-
-# card = TaskCard(
-#     loader=LoadHF(path="HuggingFaceM4/ChartQA"),
-#     preprocess_steps=[
-#         RenameSplits(mapper={"train": "train", "val": "validation", "test": "test"}),
-#         Rename(field="label", to_field="answers"),
-#         Rename(field="query", to_field="question"),
-#         ToImage(field="image", to_field="context"),
-#         Set(fields={"context_type": "image"}),
-#     ],
-#     task="tasks.qa.with_context.abstractive[metrics=[metrics.relaxed_correctness]]",
-#     templates="templates.qa.with_context.all",
-#     __tags__={
-#         "license": "GPL-3.0",
-#         "multilinguality": "monolingual",
-#         "modalities": ["image", "text"],
-#         "size_categories": "10K<n<100K",
-#         "task_categories": "question-answering",
-#         "task_ids": "extractive-qa",
-#     },
-#     __description__=(
-#         "ChartQA: A Benchmark for Question Answering about Charts with Visual and Logical Reasoning."
-#     ),
-# )
-#
-# test_card(card)
-# add_to_catalog(card, "cards.chart_qa", overwrite=True)
+from unitxt.splitters import RenameSplits
+from unitxt.operators import Rename
 
 
 card = TaskCard(
-    loader=LoadHF(path="lmms-lab/ChartQA"),
+    loader=LoadHF(path="HuggingFaceM4/ChartQA"),
     preprocess_steps=[
-        Wrap(field="answer", inside="list", to_field="answers"),
+        RenameSplits(mapper={"train": "train", "val": "validation", "test": "test"}),
+        Rename(field="label", to_field="answers"),
+        Rename(field="query", to_field="question"),
         ToImage(field="image", to_field="context"),
         Set(fields={"context_type": "image"}),
     ],
-    task="tasks.qa.with_context.with_type[metrics=[metrics.relaxed_correctness]]",
+    task="tasks.qa.with_context",
     templates="templates.qa.with_context.all",
-    default_template=MultiReferenceTemplate(
-        input_format="{context}\n{question}\nAnswer the question using a single word.",
-        references_field="answers",
-        __description__="lmms-evals default template for chartqa.",
-    ),
     __tags__={
         "license": "GPL-3.0",
         "multilinguality": "monolingual",
@@ -61,4 +33,35 @@ card = TaskCard(
 )
 
 test_card(card)
-add_to_catalog(card, "cards.chart_qa_lmms_eval", overwrite=True)
+add_to_catalog(card, "cards.chart_qa", overwrite=True)
+
+
+# card = TaskCard(
+#     loader=LoadHF(path="lmms-lab/ChartQA"),
+#     preprocess_steps=[
+#         Wrap(field="answer", inside="list", to_field="answers"),
+#         ToImage(field="image", to_field="context"),
+#         Set(fields={"context_type": "image"}),
+#     ],
+#     task="tasks.qa.with_context.with_type[metrics=[metrics.relaxed_correctness]]",
+#     templates="templates.qa.with_context.all",
+#     default_template=MultiReferenceTemplate(
+#         input_format="{context}\n{question}\nAnswer the question using a single word.",
+#         references_field="answers",
+#         __description__="lmms-evals default template for chartqa.",
+#     ),
+#     __tags__={
+#         "license": "GPL-3.0",
+#         "multilinguality": "monolingual",
+#         "modalities": ["image", "text"],
+#         "size_categories": "10K<n<100K",
+#         "task_categories": "question-answering",
+#         "task_ids": "extractive-qa",
+#     },
+#     __description__=(
+#         "ChartQA: A Benchmark for Question Answering about Charts with Visual and Logical Reasoning."
+#     ),
+# )
+#
+# test_card(card)
+# add_to_catalog(card, "cards.chart_qa_lmms_eval", overwrite=True)
--- prepare/metrics/relaxed_correctness.py
+++ prepare/metrics/relaxed_correctness.py
@@ -1,43 +1,44 @@
 from unitxt import add_to_catalog
 from unitxt.metrics import RelaxedCorrectness
+from unitxt.test_utils.metrics import test_metric
 
 # from cvar_pyutils.debugging_tools import set_remote_debugger
 # set_remote_debugger('9.61.188.58', 55557)
-metric = RelaxedCorrectness()
+metric = RelaxedCorrectness(n_resamples=None)
 
-# predictions = ["10", "30"]
-# references = [["14"], ["30"]]
-#
-# # how to create a metric which isn't updated in every sample when using UNITXT?
-# instance_targets = [
-#     {
-#         "relaxed_overall": 0.0,
-#         "relaxed_human_split": 0.0,
-#         "score": 0.0,
-#         "score_name": "relaxed_overall",
-#     },
-#     {
-#         "relaxed_overall": 1.0,
-#         "relaxed_augmented_split": 1.0,
-#         "score": 1.0,
-#         "score_name": "relaxed_overall",
-#     },
-# ]
-#
-# global_target = {
-#     "relaxed_overall": 0.5,
-#     "relaxed_human_split": 0.0,
-#     "relaxed_augmented_split": 1.0,
-#     "score": 0.5,
-#     "score_name": "relaxed_overall",
-#     "num_of_instances": 2,
-# }
-# outputs = test_metric(
-#     metric=metric,
-#     predictions=predictions,
-#     references=references,
-#     instance_targets=instance_targets,
-#     global_target=global_target,
-#     task_data=[{"type": "human_test"}, {"type": "augmented_test"}],
-# )
+predictions = ["10", "30"]
+references = [["14"], ["30"]]
+
+# how to create a metric which isn't updated in every sample when using UNITXT?
+instance_targets = [
+    {
+        "relaxed_overall": 0.0,
+        "relaxed_human_split": 0.0,
+        "score": 0.0,
+        "score_name": "relaxed_overall",
+    },
+    {
+        "relaxed_overall": 1.0,
+        "relaxed_augmented_split": 1.0,
+        "score": 1.0,
+        "score_name": "relaxed_overall",
+    },
+]
+
+global_target = {
+    "relaxed_overall": 0.5,
+    "relaxed_human_split": 0.0,
+    "relaxed_augmented_split": 1.0,
+    "score": 0.5,
+    "score_name": "relaxed_overall",
+    "num_of_instances": 2,
+}
+outputs = test_metric(
+    metric=metric,
+    predictions=predictions,
+    references=references,
+    instance_targets=instance_targets,
+    global_target=global_target,
+    task_data=[{"type": "human_test"}, {"type": "augmented_test"}],
+)
 add_to_catalog(metric, "metrics.relaxed_correctness", overwrite=True)
