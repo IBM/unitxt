f7e13ebe237822ce2b07d97c6d5685c0ac1e4194
elronbandel <elronbandel@gmail.com>
2025-01-07 14:08:49 +0200

    Refactor AttaQ card: remove deprecated card and add new card structure with artifact link
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- prepare/cards/attaq.py
+++ prepare/cards/attaq.py
@@ -5,6 +5,7 @@ from unitxt.blocks import (
     Task,
     TaskCard,
 )
+from unitxt.catalog import add_link_to_catalog
 from unitxt.operators import Shuffle
 from unitxt.splitters import RenameSplits
 from unitxt.test_utils.card import test_card
@@ -27,4 +28,10 @@ card = TaskCard(
 )
 
 test_card(card, strict=False, demos_taken_from="test", num_demos=0)
-add_to_catalog(card, "cards.atta_q", overwrite=True)
+add_to_catalog(card, "cards.attaq", overwrite=True)
+
+add_link_to_catalog(
+    artifact_linked_to="cards.attaq",
+    name="cards.atta_q",
+    overwrite=True,
+)
