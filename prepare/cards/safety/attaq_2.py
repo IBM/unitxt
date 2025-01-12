3cf5b2a634f2720841d7bbe97ae12e248b434374
Jonathan Bnayahu <bnayahu@il.ibm.com>
2025-01-12 09:40:53 +0200

    Added gg judge metric
    Signed-off-by: Jonathan Bnayahu <bnayahu@il.ibm.com>

---
+++ prepare/cards/safety/attaq_2.py
@@ -0,0 +1,43 @@
+from unitxt import add_to_catalog
+from unitxt.blocks import (
+    InputOutputTemplate,
+    LoadHF,
+    Task,
+    TaskCard,
+)
+from unitxt.operators import Copy, Set, Shuffle
+from unitxt.splitters import RenameSplits
+from unitxt.struct_data_operators import DumpJson
+from unitxt.test_utils.card import test_card
+
+card = TaskCard(
+    loader=LoadHF(path="ibm/AttaQ",
+                  data_classification_policy=["public"]),
+    preprocess_steps=[
+        RenameSplits(mapper={"train": "test"}),
+        Shuffle(page_size=2800),
+        Set({"input_label": {}}),
+        Copy(
+            field_to_field={"input": "input_label/input", "label": "input_label/label"}
+        ),
+        DumpJson(field="input_label"),
+    ],
+    task=Task(
+        input_fields={"input": str},
+        reference_fields={"input_label": str},
+        prediction_type=str,
+        metrics=["metrics.safety_metric", "metrics.llm_as_judge.direct.watsonx.granite_guardian_3_0_2b"],
+        
+    ),
+    templates=[
+        InputOutputTemplate(input_format="{input}\n", output_format="{input_label}"),
+        InputOutputTemplate(input_format="{input}", output_format="{input_label}"),
+    ],
+    __description__="AttaQ",
+    __tags__={
+        "languages": ["english"],
+        },
+)
+
+test_card(card, strict=False, demos_taken_from="test", num_demos=0)
+add_to_catalog(card, "cards.safety.attaq2", overwrite=True)
