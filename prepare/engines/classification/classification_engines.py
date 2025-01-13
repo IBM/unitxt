b5c6592274f75d82442e8dde105ea96fe22fc264
lilacheden <lilach.edel@gmail.com>
2025-01-13 22:09:35 +0200

    update classification engines: remove gen_ai, use crossprovider, add watsonx Signed-off-by: lilacheden <lilach.edel@gmail.com>

--- prepare/engines/classification/classification_engines.py
+++ prepare/engines/classification/classification_engines.py
@@ -1,56 +1,45 @@
 from unitxt import add_to_catalog
 from unitxt.inference import (
-    AzureOpenAIInferenceEngine,
-    IbmGenAiInferenceEngine,
-    RITSInferenceEngine,
+    CrossProviderInferenceEngine,
     WMLInferenceEngineGeneration,
 )
 
+model_names_to_provider = {
+    "llama-3-3-70b-instruct": ["watsonx", "rits"],
+    "llama-3-1-70b-instruct": ["watsonx", "rits"],
+    "gpt-4o": ["open-ai", "azure"],
+    "gpt-4-turbo": ["open-ai", "azure"],
+    "mistralai/mixtral-8x7b-instruct-v01": ["ibm_wml"],
+    "meta-llama/llama-3-3-70b-instruct": ["ibm_wml"],
+    "meta-llama/llama-3-1-70b-instruct": ["ibm_wml"],
+    "meta-llama/llama-3-405b-instruct": ["ibm_wml"],
+    "llama-3-1-405b-instruct-fp8": ["rits"],
+}
 
-def get_inference_engine(model_name, framework_name):
-    if framework_name == "ibm_wml":
+
+def get_inference_engine(model_name, provider):
+    if provider == "ibm_wml":
         return WMLInferenceEngineGeneration(
             model_name=model_name,
             max_new_tokens=5,
             random_seed=42,
             decoding_method="greedy",
         )
-    if framework_name == "ibm_gen_ai":
-        return IbmGenAiInferenceEngine(
-            model_name=model_name,
-            max_new_tokens=5,
-            random_seed=42,
-            decoding_method="greedy",
-        )
-    if framework_name == "openai":
-        return AzureOpenAIInferenceEngine(
-            model_name=model_name,
-            logprobs=True,
-            max_tokens=5,
-            temperature=0.0,
-            top_logprobs=5,
-        )
-    if framework_name == "rits":
-        return RITSInferenceEngine(
-            model_name=model_name, logprobs=True, max_tokens=5, temperature=0.0
-        )
-    raise ValueError("Unsupported framework name " + framework_name)
+
+    return CrossProviderInferenceEngine(
+        model=model_name,
+        logprobs=True,
+        max_tokens=5,
+        temperature=0.0,
+        top_logprobs=5,
+        provider=provider,
+    )
 
 
-model_names_to_infer_framework = {
-    "meta-llama/llama-3-1-70b-instruct": ["ibm_wml", "rits", "ibm_gen_ai"],
-    "meta-llama/llama-3-3-70b-instruct": ["ibm_wml", "rits"],
-    "gpt-4-turbo-2024-04-09": ["openai"],
-    "gpt-4o-2024-08-06": ["openai"],
-    "mistralai/mixtral-8x7b-instruct-v01": ["ibm_wml", "ibm_gen_ai", "rits"],
-    "meta-llama/llama-3-1-405b-instruct-fp8": ["ibm_gen_ai", "rits"],
-    "meta-llama/llama-3-405b-instruct": ["ibm_wml"],
-}
-
-for judge_model_name, infer_frameworks in model_names_to_infer_framework.items():
+for judge_model_name, infer_frameworks in model_names_to_provider.items():
     for infer_framework in infer_frameworks:
         inference_engine = get_inference_engine(judge_model_name, infer_framework)
-        inference_engine_label = inference_engine.get_engine_id()
+        inference_engine_label = inference_engine.get_engine_id().replace("-", "_")
 
         add_to_catalog(
             inference_engine,
