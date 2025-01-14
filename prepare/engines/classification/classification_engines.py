114c519582c18e543ecde3f4ecfdc717e77d6e50
Yoav Katz <katz@il.ibm.com>
2025-01-14 14:44:39 +0200

    Removed GA
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/engines/classification/classification_engines.py
+++ prepare/engines/classification/classification_engines.py
@@ -31,12 +31,12 @@ def get_inference_engine(model_name, framework_name):
 
 
 model_names_to_infer_framework = {
-    "meta-llama/llama-3-1-70b-instruct": ["ibm_wml", "rits", "ibm_gen_ai"],
+    "meta-llama/llama-3-1-70b-instruct": ["ibm_wml", "rits"],
     "meta-llama/llama-3-3-70b-instruct": ["ibm_wml", "rits"],
     "gpt-4-turbo-2024-04-09": ["openai"],
     "gpt-4o-2024-08-06": ["openai"],
-    "mistralai/mixtral-8x7b-instruct-v01": ["ibm_wml", "ibm_gen_ai", "rits"],
-    "meta-llama/llama-3-1-405b-instruct-fp8": ["ibm_gen_ai", "rits"],
+    "mistralai/mixtral-8x7b-instruct-v01": ["ibm_wml", "rits"],
+    "meta-llama/llama-3-1-405b-instruct-fp8": ["rits"],
     "meta-llama/llama-3-405b-instruct": ["ibm_wml"],
 }
 
