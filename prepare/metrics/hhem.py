e96bb97644dff5f58c40eeb206b49738454617a2
lilacheden <lilach.edel@gmail.com>
2025-01-19 08:12:36 +0200

    remove duplicatte preparation Signed-off-by: lilacheden <lilach.edel@gmail.com>

--- prepare/metrics/hhem.py
+++ prepare/metrics/hhem.py
@@ -1,6 +1,5 @@
 from unitxt import add_to_catalog
-from unitxt.metrics import FaithfulnessHHEM, MetricPipeline
-from unitxt.operators import Copy
+from unitxt.metrics import FaithfulnessHHEM
 from unitxt.test_utils.metrics import test_metric
 
 pairs = [
@@ -12,21 +11,6 @@ pairs = [
 predictions = [p[1] for p in pairs]
 task_data = [{"contexts": [p[0]]} for p in pairs]
 
-## This metric pipeline supports two usecases:
-## 1. Regular unitxt flow: predictions are taken from model prediction and contexts appears in the task data
-## 2. Running on external rag output: each instance contains field "answer" and field "contexts"
-metric = MetricPipeline(
-    main_score="hhem_score",
-    preprocess_steps=[
-        Copy(
-            field_to_field={"task_data/contexts": "references", "answer": "prediction"},
-            not_exist_do_nothing=True,
-        ),
-        Copy(field_to_field={"contexts": "references"}, not_exist_do_nothing=True),
-    ],
-    metric=FaithfulnessHHEM(),
-    __description__="Vectara's halucination detection model, HHEM2.1, compares contexts and generated answer to determine faithfulness.",
-)
 instance_targets = [
     {"score": 0.01, "score_name": "hhem_score", "hhem_score": 0.01},
     {"score": 0.65, "score_name": "hhem_score", "hhem_score": 0.65},
@@ -42,18 +26,6 @@ global_target = {
     "hhem_score_ci_low": 0.05,
     "hhem_score_ci_high": 0.65,
 }
-
-
-outputs = test_metric(
-    metric=metric,
-    predictions=predictions,
-    references=[[""]] * len(instance_targets),
-    task_data=task_data,
-    instance_targets=instance_targets,
-    global_target=global_target,
-)
-add_to_catalog(metric, "metrics.rag.faithfulness.vectara_hhem_2_1", overwrite=True)
-
 
 references = [[p[0]] for p in pairs]
 metric = FaithfulnessHHEM()
