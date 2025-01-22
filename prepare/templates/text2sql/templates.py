b0098d8d3b2a8e69c279d156926a44febc41edd9
Yotam-Perlitz <y.perlitz@ibm.com>
2025-01-22 12:32:38 +0100

    fix template bug
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

--- prepare/templates/text2sql/templates.py
+++ prepare/templates/text2sql/templates.py
@@ -22,7 +22,7 @@ template_details = [
         "",
     ),
     (
-        "templates.text2sql.you_are_given_with_hint_with_sql_prefix",
+        "templates.text2sql.you_are_given_with_hint",
         "You are given the following question:\n\n{utterance}\n\nAn SQL schema\n\n```sql\n\n{db}\n```\n\nAnd hint:\n\n{hint}\n\nAnswer the following question:\n\n{utterance}\n\n",
         "You are a Text2SQL generation model, in your answer, only have SQL code.\nMake sure you start your query with 'SELECT' and end it with ';'\n\n",
         "",
