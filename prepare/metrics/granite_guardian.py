2e59332f95070e5e2c33177dbf920b21d3402a14
Martín Santillán Cooper <msantillancooper@ibm.com>
2025-02-05 12:03:08 -0300

    Adapt catalog
    Signed-off-by: Martín Santillán Cooper <msantillancooper@ibm.com>

--- prepare/metrics/granite_guardian.py
+++ prepare/metrics/granite_guardian.py
@@ -1,25 +1,8 @@
 from unitxt import add_to_catalog
-from unitxt.metrics import GraniteGuardianWMLMetric, MetricPipeline
-from unitxt.operators import Set
+from unitxt.metrics import GraniteGuardianWMLMetric
 
 for risk_type, risk_names in GraniteGuardianWMLMetric.available_risks.items():
     for risk_name in risk_names:
         metric_name = f"""granite_guardian.{risk_type.value}.{risk_name}"""
-        metric = GraniteGuardianWMLMetric(
-            main_score=metric_name, risk_name=risk_name, risk_type=risk_type.name
-        )
-
-        metric_pipeline = MetricPipeline(
-            main_score=metric_name,
-            metric=metric,
-            preprocess_steps=[
-                Set(
-                    fields={
-                        "prediction": 0.0,  # these are not used by the metric
-                        "references": [0.0],
-                    }
-                ),
-            ],
-        )
-
-        add_to_catalog(metric_pipeline, name=f"metrics.{metric_name}", overwrite=True)
+        metric = GraniteGuardianWMLMetric(risk_name=risk_name, risk_type=risk_type.name)
+        add_to_catalog(metric, name=f"metrics.{metric_name}", overwrite=True)
