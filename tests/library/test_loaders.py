cb63864485696eb6d3467f218512d042a30572bc
elronbandel <elronbandel@gmail.com>
2025-02-09 15:09:47 +0200

    test: add unit test for LoadCSV error handling on file not found
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- tests/library/test_loaders.py
+++ tests/library/test_loaders.py
@@ -4,6 +4,7 @@ import tempfile
 from unittest.mock import patch
 
 import pandas as pd
+from unitxt.error_utils import UnitxtError
 from unitxt.loaders import (
     LoadCSV,
     LoadFromDictionary,
@@ -76,6 +77,10 @@ class TestLoaders(UnitxtTestCase):
                     dfs[file].iterrows(), ms[file]
                 ):
                     self.assertEqual(saved_instance[1].to_dict(), loaded_instance)
+
+    def test_failed_load_csv(self):
+        with self.assertRaises(UnitxtError):
+            list(LoadCSV(files={"test": "not_exist.csv"})()["test"])
 
     def test_load_csv_with_pandas_args(self):
         # Using a context for the temporary directory
