d9d9a9d4793f83809b369be075a7d6a4567e8f68
Elron Bandel <elronbandel@gmail.com>
2025-02-23 11:27:35 +0200

    Enable offline mode for hugginface by using local pre-downloaded metrics, datasets and models (#1603)
    * Enable local caching with UNITXT_LOCAL_CACHE
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    * Add cache_dir parameter to model and tokenizer loading for improved performance
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    * Update line numbers and generated timestamp in secrets baseline
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    * Update line number and generated timestamp in secrets baseline
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    * Fix metrics.py
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    * Use huggingface prefix
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    * Clarify documentation for offline Hugging Face model, metrics, and datasets paths
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    * Rename hf_offline_loaders_path to hf_offline_models_path for clarity
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    ---------
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- src/unitxt/string_operators.py
+++ src/unitxt/string_operators.py
@@ -1,3 +1,4 @@
+import os
 import re
 from typing import (
     Any,
@@ -7,7 +8,9 @@ from typing import (
 )
 
 from .operators import FieldOperator, InstanceOperator
+from .settings_utils import get_settings
 
+settings = get_settings()
 
 class Split(FieldOperator):
     by: str
@@ -30,8 +33,10 @@ class TokensSplit(FieldOperator):
     def prepare(self):
         super().prepare()
         from transformers import AutoTokenizer
-
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model)
+        path = self.model
+        if settings.hf_offline_models_path is not None:
+            path = os.path.join(settings.hf_offline_models_path, path)
+        self.tokenizer = AutoTokenizer.from_pretrained(path)
 
     def process_value(self, value: str) -> List[str]:
         return self.tokenizer.tokenize(value)
@@ -48,8 +53,10 @@ class TokensSlice(FieldOperator):
     def prepare(self):
         super().prepare()
         from transformers import AutoTokenizer
-
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model)
+        path = self.model
+        if settings.hf_offline_models_path is not None:
+            path = os.path.join(settings.hf_offline_models_path, path)
+        self.tokenizer = AutoTokenizer.from_pretrained(path)
 
     def process_value(self, value: str) -> str:
         encoded = self.tokenizer.encode(value)
