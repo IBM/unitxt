f5c2cee32c7ffdf3b051dcca256f2b9396ee9c7b
elronbandel <elronbandel@gmail.com>
2025-02-26 17:24:49 +0200

    Another try
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- src/unitxt/loaders.py
+++ src/unitxt/loaders.py
@@ -307,9 +307,8 @@ class LoadHF(LazyLoader):
             self.__class__._loader_cache.max_size = settings.loader_cache_size
             if not disable_memory_caching:
                 self.__class__._loader_cache[dataset_id] = dataset
-        if "instruction" in dataset[0]:
-
-            raise ValueError("There might be a problem")
+        if "instruction" in dict(next(iter(dataset))):
+            raise requests.HTTPError("There might be a problem")
         return self.__class__._loader_cache[dataset_id]
 
     def _maybe_set_classification_policy(self):
