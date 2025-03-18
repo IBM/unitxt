e75e6252d7701e101cd00c9058ec43de30e11455
Yoav Katz <katz@il.ibm.com>
2025-03-18 10:58:47 +0200

    Ensure temperature is 0 in extraction task

--- examples/key_value_extraction_evaluation.py
+++ examples/key_value_extraction_evaluation.py
@@ -57,7 +57,7 @@ dataset = create_dataset(
 )
 
 model = CrossProviderInferenceEngine(
-    model="llama-3-2-90b-vision-instruct", provider="watsonx"
+    model="llama-3-2-90b-vision-instruct", provider="watsonx", temperature=0
 )
 
 predictions = model(dataset)
