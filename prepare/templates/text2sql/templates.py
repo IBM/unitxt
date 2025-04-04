7941c51a8a3dfbc3b11ba9d2e111a7e627250763
Yotam Perlitz <y.perlitz@ibm.com>
2025-04-04 13:39:56 +0200

    add system-less template
    Signed-off-by: Yotam Perlitz <y.perlitz@ibm.com>

--- prepare/templates/text2sql/templates.py
+++ prepare/templates/text2sql/templates.py
@@ -4,6 +4,12 @@ from unitxt.templates import InputOutputTemplate
 
 template_details = [
     (
+        "templates.text2sql.you_are_given_no_system",
+        "You are given the following SQL schema\n\n```sql\n{db}```\n\n{utterance}\n",
+        "",  # system should be the one comes with the model
+        "```sql\nSELECT ",
+    ),
+    (
         "templates.text2sql.you_are_given_with_sql_prefix",
         "You are given the following question:\n\n{utterance}\n\nAn SQL schema\n\n```sql\n\n{db}\n```\n\nAnswer the following question:\n\n{utterance}\n\n",
         "You are a Text2SQL generation model, in your answer, only have SQL code.\nStart your query with 'SELECT' and end it with ';'\n\n",
