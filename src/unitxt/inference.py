3806ba84bba354823e15019b46a65a9a00677c50
ofirarviv <ofir.arviv@ibm.com>
2025-05-08 12:18:07 +0300

    update

--- src/unitxt/inference.py
+++ src/unitxt/inference.py
@@ -730,9 +730,10 @@ class HFAutoModelInferenceEngine(HFInferenceEngineBase):
         return self.processor(
             data,
             return_tensors="pt",
-            # padding=self.padding,
-            # truncation=self.truncation,
-            # padding_side=self.padding_side,
+            padding=self.padding,
+            truncation=self.truncation,
+            padding_side=self.padding_side,
+            add_special_tokens=False
         ).to(self.device or self.device_map)
 
     def _infer_fn(
