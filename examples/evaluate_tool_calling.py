1195a607fb13dee39f5bd88661a517d5f5982b42
Yoav Katz <katz@il.ibm.com>
2025-05-08 16:39:04 +0300

    Simplify output

--- examples/evaluate_tool_calling.py
+++ examples/evaluate_tool_calling.py
@@ -10,16 +10,17 @@ dataset = load_dataset(
     card="cards.bfcl.simple_v3",
     split="test",
     format="formats.chat_api",
+    max_test_instances=10
 )
 
-model = CrossProviderInferenceEngine(model="granite-3-3-8b-instruct", provider="watsonx")
+model = CrossProviderInferenceEngine(model="granite-3-3-8b-instruct", provider="rits")
 
 
 predictions = model(dataset)
 results = evaluate(predictions=predictions, data=dataset)
 
+print("Instance Results:")
+print(results.instance_scores)
+
 print("Global Results:")
 print(results.global_scores.summary)
-
-print("Instance Results:")
-print(results.instance_scores.summary)
