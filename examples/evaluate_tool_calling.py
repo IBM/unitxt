dbeb7ef7003149fefce084c9b6f2a26d2af5be32
Yoav Katz <katz@il.ibm.com>
2025-05-13 10:46:07 +0300

    Changed example to use explicit inputs.

--- examples/evaluate_tool_calling.py
+++ examples/evaluate_tool_calling.py
@@ -1,19 +1,96 @@
 from unitxt import get_logger
-from unitxt.api import evaluate, load_dataset
+from unitxt.api import create_dataset, evaluate
 from unitxt.inference import (
     CrossProviderInferenceEngine,
 )
 
 logger = get_logger()
 
-dataset = load_dataset(
-    card="cards.bfcl.simple_v3",
-    split="test",
-    format="formats.chat_api",
-    max_test_instances=10
-)
+sum_tool = {
+    "name": "add_numbers",
+    "description": "Add two numbers together and return the result.",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "num1": {
+          "type": "number",
+          "description": "The first number to add."
+        },
+        "num2": {
+          "type": "number",
+          "description": "The second number to add."
+        }
+      },
+      "required": [
+        "num1",
+        "num2"
+      ]
+    }
+}
 
-model = CrossProviderInferenceEngine(model="granite-3-3-8b-instruct", provider="watsonx")
+subtract_tool = {
+    "name": "subtract_numbers",
+    "description": "Subtracts one number from another from the other.",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "num1": {
+          "type": "number",
+          "description": "Number to subtract from"
+        },
+        "num2": {
+          "type": "number",
+          "description": "Number to subtract"
+        }
+      },
+      "required": [
+        "num1",
+        "num2"
+      ]
+    }
+}
+
+data = [
+    {
+        "query": "What is the sum of  1212382312231231 and 834672468234768234?",
+        "tools" : [sum_tool,subtract_tool],
+        "reference_calls" : [ {
+                 "name": "add_numbers",
+                  "arguments": {
+                      "num1": 1212382312231231,
+                      "num2": 834672468234768234
+                 }
+             },
+             {
+                 "name": "add_numbers",
+                  "arguments": {
+                      "num1": 834672468234768234,
+                      "num2": 1212382312231231
+                 }
+             }
+     ]
+    },
+     {
+        "query": "Subtract  1212382312231231 from 834672468234768234?",
+        "tools" : [sum_tool,subtract_tool],
+        "reference_calls" : [
+             {
+                 "name": "subtract_numbers",
+                  "arguments": {
+                      "num1": 834672468234768234,
+                      "num2": 1212382312231231
+                 }
+             }
+     ]
+    }
+]
+
+dataset = create_dataset(task="tasks.tool_calling.supervised",
+                         test_set=data,
+                         split="test",
+                         format="formats.chat_api",max_test_instances=10)
+
+model = CrossProviderInferenceEngine(model="granite-3-3-8b-instruct", provider="watsonx",temperature=0)
 
 
 predictions = model(dataset)
