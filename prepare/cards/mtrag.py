85c07cfe09391b8a87e6141c95ff195fe39e305d
Elron Bandel <elronbandel@gmail.com>
2025-05-19 10:40:18 +0300

    Add LoadJsonFile loader and tests (#1801)
    * Add LoadJsonFile loader and tests
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    * Update artifact
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>
    
    ---------
    
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- prepare/cards/mtrag.py
+++ prepare/cards/mtrag.py
@@ -5,7 +5,7 @@ from unitxt.blocks import (
     TaskCard,
 )
 from unitxt.collections_operators import Dictify, Wrap
-from unitxt.loaders import LoadCSV
+from unitxt.loaders import LoadJsonFile
 from unitxt.operators import (
     Cast,
     Copy,
@@ -17,11 +17,10 @@ from unitxt.templates import InputOutputTemplate
 from unitxt.test_utils.card import test_card
 
 card = TaskCard(
-    loader=LoadCSV(
+    loader=LoadJsonFile(
         files={
             "test": "https://raw.githubusercontent.com/IBM/mt-rag-benchmark/refs/heads/main/human/generation_tasks/reference+RAG.jsonl"
         },
-        file_type="json",
         lines=True,
         data_classification_policy=["public"],
     ),
@@ -97,12 +96,11 @@ for subset in ["clapnq", "cloud", "fiqa", "govt"]:
         subset_operators.append(Set(fields={"title": ""}))
 
     card = TaskCard(
-        loader=LoadCSV(
+        loader=LoadJsonFile(
             files={
                 "test": f"https://github.com/IBM/mt-rag-benchmark/raw/refs/heads/main/corpora/{subset}.jsonl.zip"
             },
             compression="zip",
-            file_type="json",
             lines=True,
             data_classification_policy=["public"],
         ),
