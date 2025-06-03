8ee6a64652bd261665ce217b092b5cc3951c5edb
matano <matano@il.ibm.com>
2025-06-03 09:20:53 +0300

    remove Metric import which causes a circular import

--- src/unitxt/eval_utils.py
+++ src/unitxt/eval_utils.py
@@ -4,7 +4,6 @@ from typing import List, Optional, Union
 import pandas as pd
 
 from .artifact import verbosed_fetch_artifact
-from .metric import Metric
 from .metric_utils import get_remote_metrics_endpoint, get_remote_metrics_names
 from .operator import SequentialOperator
 from .stream import MultiStream
