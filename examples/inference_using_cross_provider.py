99138f402e2111f46b39683b57ca049dfbbe7f91
Yoav Katz <katz@il.ibm.com>
2025-06-09 17:00:02 +0300

    Improved example.
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- examples/inference_using_cross_provider.py
+++ examples/inference_using_cross_provider.py
@@ -2,15 +2,25 @@ from unitxt.inference import CrossProviderInferenceEngine
 from unitxt.text_utils import print_dict
 
 if __name__ == "__main__":
-    model = CrossProviderInferenceEngine(
-        model="granite-3-3-8b-instruct", provider="watsonx", temperature=0
-    )
+    for provider in ["watsonx", "rits", "watsonx-sdk"]:
+        print()
+        print("------------------------------------------------ ")
+        print("PROVIDER:", provider)
+        model = CrossProviderInferenceEngine(
+            model="granite-3-3-8b-instruct", provider=provider, temperature=0
+        )
 
-    # Loading dataset:
-    test_data = [{"source": [{"content": "Hello, how are you?", "role": "user"}]}]
+        # Loading dataset:
+        test_data = [
+            {
+                "source": [{"content": "Hello, how are you?", "role": "user"}],
+                "data_classification_policy": ["public"],
+            }
+        ]
 
-    # Performing inference:
-    predictions = model(test_data)
-    for inp, prediction in zip(test_data, predictions):
-        result = {**inp, "prediction": prediction}
-        print_dict(result, keys_to_print=["source", "prediction"])
+        # Performing inference:
+        predictions = model(test_data)
+        for inp, prediction in zip(test_data, predictions):
+            result = {**inp, "prediction": prediction}
+
+            print_dict(result, keys_to_print=["source", "prediction"])
