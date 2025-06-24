33a5917655784e85670e07c87102528f62cab5e6
Jonathan Bnayahu <bnayahu@il.ibm.com>
2025-06-24 13:08:24 +0300

    Utility to summarize evaluation results created by the Unitxt CLI
    Signed-off-by: Jonathan Bnayahu <bnayahu@il.ibm.com>

---
+++ utils/summarize_cli_results.py
@@ -0,0 +1,55 @@
+import json
+import os
+import sys
+
+import pandas as pd
+from unitxt.logging_utils import get_logger
+
+logger = get_logger()
+
+
+def extract_scores(directory):
+    data = []
+
+    for filename in sorted(os.listdir(directory)):
+        if filename.endswith("evaluation_results.json"):
+            file_path = os.path.join(directory, filename)
+            try:
+                with open(file_path, encoding="utf-8") as f:
+                    content = json.load(f)
+
+                    env_info = content.get("environment_info", {})
+                    timestamp = env_info.get("timestamp_utc", "N/A")
+                    model = (
+                        env_info.get("environment_info", {})
+                        .get("parsed_arguments", {})
+                        .get("model", "N/A")
+                    )
+                    results = content.get("results", {})
+
+                    row = {}
+                    row["Model"] = model
+                    row["Average"] = results.get("score", "N/A")
+                    row["Timestamp"] = timestamp
+
+                    for key in results.keys():
+                        if isinstance(results[key], dict):
+                            score = results[key].get("score", "N/A")
+                            row[key] = score
+
+                    data.append(row)
+            except Exception as e:
+                logger.error(f"Error parsing results file {filename}: {e}.")
+
+    return pd.DataFrame(data)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        logger.error("Usage: python summarize_cli_results.py <results-directory>")
+        sys.exit(1)
+
+    directory = sys.argv[1]
+    df = extract_scores(directory)
+
+    logger.log(df.to_markdown(index=False))
