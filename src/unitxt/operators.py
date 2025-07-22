53be0aff1024b9f1166c24d2c5b5defa1e9cadea
Yoav Katz <katz@il.ibm.com>
2025-07-22 09:47:20 +0300

    Reverted unrelated change
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- src/unitxt/operators.py
+++ src/unitxt/operators.py
@@ -522,10 +522,6 @@ class InstanceFieldOperator(InstanceOperator):
     def process(
         self, instance: Dict[str, Any], stream_name: Optional[str] = None
     ) -> Dict[str, Any]:
-        def _limited_repr(obj, limit=196):
-            r = repr(obj)
-            return r if len(r) <= limit else r[: limit - 3] + "..."
-
         self.verify_field_definition()
         for from_field, to_field in self._field_to_field:
             with error_context(self, field=from_field, action="Read Field"):
@@ -544,7 +540,6 @@ class InstanceFieldOperator(InstanceOperator):
                 self,
                 field=from_field,
                 action="Process Field",
-                value=_limited_repr(old_value),
             ):
                 if self.process_every_value:
                     new_value = [
