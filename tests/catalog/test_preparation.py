7a96f135a01e9e19a5b66ec397a04005783d18b2
dafnapension <dafnashein@yahoo.com>
2025-07-24 11:24:41 +0300

    modulo 12 == 3
    Signed-off-by: dafnapension <dafnashein@yahoo.com>

--- tests/catalog/test_preparation.py
+++ tests/catalog/test_preparation.py
@@ -32,7 +32,7 @@ logger.critical(
     f"Over all, {len(all_preparation_files)} files will now be tested over {num_par} parallel processes."
 )
 # the following should be any of modulo num_par: 0,1,2,3,4,5,6,7,8,.. num_par-1
-modulo = 2
+modulo = 3
 all_preparation_files = [
     file for i, file in enumerate(all_preparation_files) if i % num_par == modulo
 ]
