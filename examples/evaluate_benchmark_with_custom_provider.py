62351ca4b3aca03ba5f7f5d4458bfecde9a28981
elronbandel <elronbandel@gmail.com>
2025-08-06 17:34:01 +0300

    Update some examples
    Signed-off-by: elronbandel <elronbandel@gmail.com>

--- examples/evaluate_benchmark_with_custom_provider.py
+++ examples/evaluate_benchmark_with_custom_provider.py
@@ -1,14 +1,17 @@
 from unitxt import evaluate, load_dataset
 from unitxt.inference import CrossProviderInferenceEngine
 
-data = load_dataset(
-    "benchmarks.glue[max_samples_per_subset=5, format=formats.chat_api, system_prompt=system_prompts.general.be_concise]",
+dataset = load_dataset(
+    "benchmarks.glue",
+    format="formats.chat_api",
+    system_prompt="system_prompts.general.be_concise",
+    max_samples_per_subset=5,
     split="test",
     use_cache=True,
 )
 
 model = CrossProviderInferenceEngine(
-    model="llama-3-8b-instruct", temperature=0.0, top_p=1.0, provider="watsonx"
+    model="llama-3-2-3b-instruct", temperature=0.0, top_p=1.0, provider="watsonx"
 )
 """
 We are using a CrossProviderInferenceEngine inference engine that supply api access to provider such as:
@@ -18,12 +21,12 @@ For the arguments these inference engines can receive, please refer to the class
 about the the open ai api arguments the CrossProviderInferenceEngine follows.
 """
 
-predictions = model(data)
+predictions = model(dataset)
 
-results = evaluate(predictions=predictions, data=data)
+results = evaluate(predictions=predictions, data=dataset)
 
 print("Global Results:")
 print(results.global_scores.summary)
 
-print("Instance Results:")
-print(results.instance_scores.summary)
+print("Subsets Results:")
+print(results.subsets_scores.summary)
