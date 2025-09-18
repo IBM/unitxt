006580c014a9535a04025338fbb043dc130cba1d
Jonathan Bnayahu <bnayahu@il.ibm.com>
2025-09-18 09:17:52 +0300

    Switch provoq to GG metric.
    Signed-off-by: Jonathan Bnayahu <bnayahu@il.ibm.com>

--- prepare/benchmarks/safety.py
+++ prepare/benchmarks/safety.py
@@ -2,7 +2,7 @@ from unitxt.benchmark import Benchmark
 from unitxt.catalog import add_to_catalog
 from unitxt.standard import DatasetRecipe
 
-MAX_TEST_INSTANCES = 500
+MAX_TEST_INSTANCES = 1000
 
 benchmark = Benchmark(
     subsets={
@@ -13,7 +13,7 @@ benchmark = Benchmark(
             max_test_instances=MAX_TEST_INSTANCES,
         ),
         "provoq": DatasetRecipe(
-            card="cards.safety.provoq",
+            card="cards.safety.provoq_gg",
             template_card_index="default",
             group_by=["group"],
             max_test_instances=MAX_TEST_INSTANCES,
