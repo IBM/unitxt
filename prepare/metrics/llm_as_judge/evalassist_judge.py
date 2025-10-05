5929882809b5790f5f04d73c0de0088bdf03915b
Yoav Katz <katz@il.ibm.com>
2025-10-05 18:27:13 +0300

    Added RITS version of new eval assist judges
    Signed-off-by: Yoav Katz <katz@il.ibm.com>

--- prepare/metrics/llm_as_judge/evalassist_judge.py
+++ prepare/metrics/llm_as_judge/evalassist_judge.py
@@ -2,16 +2,23 @@ from unitxt import add_to_catalog
 from unitxt.evalassist_judge import EvalAssistLLMJudgeDirect
 from unitxt.inference import CrossProviderInferenceEngine
 
-eval_assist_judge = EvalAssistLLMJudgeDirect(
-    inference_engine=CrossProviderInferenceEngine(
-        model="llama-3-3-70b-instruct",
-        max_tokens=1024,
-        temperature=0.0,
-    ),
-)
+for provider in ["watsonx", "rits"]:
+    for model in ["llama-3-3-70b-instruct"]:
+        eval_assist_judge = EvalAssistLLMJudgeDirect(
+            inference_engine=CrossProviderInferenceEngine(
+                provider=provider,
+                model=model,
+                max_tokens=1024,
+                temperature=0.0,
+            )
+        )
+        if model == "llama-3-3-70b-instruct":
+            catalog_model = "llama3_3_70b"
+        else:
+            raise ValueError(f"Model {model} not supported")
 
-add_to_catalog(
-    eval_assist_judge,
-    "metrics.llm_as_judge.evalassist.direct.watsonx.llama3_3_70b",
-    overwrite=True,
-)
+        add_to_catalog(
+            eval_assist_judge,
+            f"metrics.llm_as_judge.evalassist.direct.{provider}.{catalog_model}",
+            overwrite=True,
+        )
